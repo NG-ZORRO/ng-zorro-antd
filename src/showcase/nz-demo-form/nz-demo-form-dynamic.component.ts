8ef45eb2407408fce8b83e8314eaf9cf4ac21424
thegatheringstorm <tgs@tgs.blue>
2017-08-31 16:13:46 +0800

    docs(form): fix an incorrect effect in nz-demo-form-dynamic (#186)
    close #163

--- src/showcase/nz-demo-form/nz-demo-form-dynamic.component.ts
+++ src/showcase/nz-demo-form/nz-demo-form-dynamic.component.ts
@@ -9,36 +9,44 @@ import {
 @Component({
   selector: 'nz-demo-form-dynamic',
   template: `
-    <form nz-form [formGroup]="validateForm" (ngSubmit)="_submitForm()">
-      <div nz-form-item nz-row *ngFor="let control of controlArray;let i = index">
-        <div nz-form-label nz-col [nzSpan]="4" *ngIf="i==0">
-          <label [attr.for]="'passenger'+control">Passengers</label>
-        </div>
-        <div nz-form-control nz-col [nzSpan]="20" [nzOffset]="i==0?0:4" [nzValidateStatus]="getFormControl('passenger'+control)">
-          <nz-input style="width: 60%; margin-right:8px;" [nzSize]="'large'" [nzPlaceHolder]="'placeholder'" [formControlName]="'passenger'+control" [nzId]="'passenger'+control"></nz-input>
-          <i class="anticon anticon-minus-circle-o dynamic-delete-button" (click)="removeField(control,$event)"></i>
-          <div nz-form-explain *ngIf="getFormControl('passenger'+control)?.dirty&&getFormControl('passenger'+control)?.hasError('required')">Please input passenger's name or delete this field.</div>
+  <form nz-form [formGroup]="validateForm" (ngSubmit)="_submitForm()">
+    <div nz-form-item nz-row *ngFor="let control of controlArray;let i = index">
+      <div nz-form-label nz-col [nzSpan]="4" *ngIf="i==0">
+        <label [attr.for]="control.controlInstance">Passengers</label>
+      </div>
+      <div nz-form-control nz-col [nzSpan]="20" [nzOffset]="i==0?0:4" [nzValidateStatus]="getFormControl(control.controlInstance)">
+        <nz-input
+          style="width: 60%; margin-right:8px;"
+          [nzSize]="'large'"
+          [nzPlaceHolder]="'placeholder'"
+          [formControlName]="control.controlInstance"
+          [nzId]="control.id">
+        </nz-input>
+        <i class="anticon anticon-minus-circle-o dynamic-delete-button" (click)="removeField(control,$event)"></i>
+        <div nz-form-explain
+          *ngIf="getFormControl(control.controlInstance)?.dirty&&getFormControl(control.controlInstance)?.hasError('required')">
+          Please input passenger's name or delete this field.
         </div>
       </div>
-      <div nz-form-item nz-row>
-        <div nz-form-control nz-col [nzSpan]="20" [nzOffset]="4">
-          <button nz-button [nzType]="'dashed'" [nzSize]="'large'" style="width:60%" (click)="addField($event)">
-            <i class="anticon anticon-plus"></i>
-            <span> Add field</span>
-          </button>
-        </div>
+    </div>
+    <div nz-form-item nz-row>
+      <div nz-form-control nz-col [nzSpan]="20" [nzOffset]="4">
+        <button nz-button [nzType]="'dashed'" [nzSize]="'large'" style="width:60%" (click)="addField($event)">
+          <i class="anticon anticon-plus"></i>
+          <span> Add field</span>
+        </button>
       </div>
-      <div nz-form-item nz-row>
-        <div nz-form-control nz-col [nzSpan]="20" [nzOffset]="4">
-          <button nz-button [nzType]="'primary'" [nzSize]="'large'">Submit</button>
-        </div>
+    </div>
+    <div nz-form-item nz-row>
+      <div nz-form-control nz-col [nzSpan]="20" [nzOffset]="4">
+        <button nz-button [nzType]="'primary'" [nzSize]="'large'">Submit</button>
       </div>
-
-    </form>
+    </div>
+  </form>
   `,
 
   styles: [
-      `
+    `
       .dynamic-delete-button {
         cursor: pointer;
         position: relative;
@@ -53,32 +61,39 @@ import {
 export class NzDemoFormDynamicComponent implements OnInit {
   validateForm: FormGroup;
   controlArray = [];
-  index = 0;
 
   addField(e?: MouseEvent) {
     if (e) {
       e.preventDefault();
     }
-    this.controlArray.push(this.index);
-    this.validateForm.addControl(`passenger${this.index}`, new FormControl(null, Validators.required));
-    this.index++;
+    const id = (this.controlArray.length > 0) ? this.controlArray[this.controlArray.length - 1].id + 1 : 0;
+
+    const control = {
+      id,
+      controlInstance: `passenger${id}`
+    };
+    const index = this.controlArray.push(control);
+    console.log(this.controlArray[this.controlArray.length - 1]);
+    this.validateForm.addControl(this.controlArray[index - 1].controlInstance, new FormControl(null, Validators.required));
   }
 
   removeField(i, e: MouseEvent) {
     e.preventDefault();
     if (this.controlArray.length > 1) {
-      this.controlArray.splice(i, 1);
-      this.validateForm.removeControl(`passenger${this.index}`);
+      const index = this.controlArray.indexOf(i);
+      this.controlArray.splice(index, 1);
+      console.log(this.controlArray);
+      this.validateForm.removeControl(i.controlInstance);
     }
   }
 
   getFormControl(name) {
-    return this.validateForm.controls[ name ];
+    return this.validateForm.controls[name];
   }
 
   _submitForm() {
     for (const i in this.validateForm.controls) {
-      this.validateForm.controls[ i ].markAsDirty();
+      this.validateForm.controls[i].markAsDirty();
     }
     console.log(this.validateForm.value);
   }
@@ -91,4 +106,3 @@ export class NzDemoFormDynamicComponent implements OnInit {
     this.addField();
   }
 }
-
