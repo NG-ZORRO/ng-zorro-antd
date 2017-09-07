2f5bafa368cfa38fda0c51b55b278029e8899502
执衡 <yadong.xyd@alibaba-inc.com>
2017-09-07 20:15:38 +0800

    feat(module:grid): support embedded col option close #101

--- src/showcase/nz-demo-grid/nz-demo-grid.module.ts
+++ src/showcase/nz-demo-grid/nz-demo-grid.module.ts
@@ -11,6 +11,7 @@ import { NzDemoGridFlexComponent } from './nz-demo-grid-flex.component';
 import { NzDemoGridFlexAlignComponent } from './nz-demo-grid-flex-align.component';
 import { NzDemoGridFlexOrderComponent } from './nz-demo-grid-flex-order.component';
 import { NzDemoGridResponsiveComponent } from './nz-demo-grid-responsive.component';
+import { NzDemoGridResponsiveMoreComponent } from './nz-demo-grid-responsive-more.component';
 import { NzDemoGridComponent } from './nz-demo-grid.component';
 import { NzCodeBoxModule } from '../share/nz-codebox/nz-codebox.module';
 
@@ -20,7 +21,7 @@ import { NzDemoGridRoutingModule } from './nz-demo-grid.routing.module';
 
 @NgModule({
   imports     : [ NzDemoGridRoutingModule, CommonModule, NzCodeBoxModule, NgZorroAntdModule, FormsModule ],
-  declarations: [ NzDemoGridGutterConfigComponent, NzDemoGridFlexOrderComponent, NzDemoGridBasicComponent, NzDemoGridGutterComponent, NzDemoGridOffsetComponent, NzDemoGridSortComponent, NzDemoGridFlexComponent, NzDemoGridFlexAlignComponent, NzDemoGridResponsiveComponent, NzDemoGridComponent ]
+  declarations: [ NzDemoGridResponsiveMoreComponent, NzDemoGridGutterConfigComponent, NzDemoGridFlexOrderComponent, NzDemoGridBasicComponent, NzDemoGridGutterComponent, NzDemoGridOffsetComponent, NzDemoGridSortComponent, NzDemoGridFlexComponent, NzDemoGridFlexAlignComponent, NzDemoGridResponsiveComponent, NzDemoGridComponent ]
 })
 
 export class NzDemoGridModule {
