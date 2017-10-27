639498152fe6503b68aa3677d387fdc037f7fff4
Hsuan Lee <HsuanGM@gmail.com>
2017-10-27 17:10:14 +0800

    fix(module:tabs): repeat rendering the TabBarExtra when used nested tabs

--- src/showcase/nz-demo-tabs/nz-demo-tabs-extra.component.ts
+++ src/showcase/nz-demo-tabs/nz-demo-tabs-extra.component.ts
@@ -3,14 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'nz-demo-tabs-extra',
   template: `
-    <nz-tabset>
+    <nz-tabset [nzTabBarExtraTemplate]="tabBarExtraContent">
       <nz-tab *ngFor="let tab of tabs">
         <ng-template #nzTabHeading>
          Tab {{tab.index}}
         </ng-template>
         <span>Content of tab {{tab.index}}</span>
       </nz-tab>
-      <ng-template #nzTabBarExtraContent>
+      <ng-template #tabBarExtraContent>
         <button nz-button>Extra Action</button>
       </ng-template>
     </nz-tabset>`,
