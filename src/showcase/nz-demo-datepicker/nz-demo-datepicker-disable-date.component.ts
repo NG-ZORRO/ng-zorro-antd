611f1fdf1ebfceb4c04784f648390c3a2d77cbb9
Hsuan Lee <HsuanGM@gmail.com>
2017-11-04 13:02:32 +0800

    feat(module:datepicker): support [nzDisabledDate] property in month mode (#451)
    close #442

--- src/showcase/nz-demo-datepicker/nz-demo-datepicker-disable-date.component.ts
+++ src/showcase/nz-demo-datepicker/nz-demo-datepicker-disable-date.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'nz-demo-datepicker-disable-date',
   template: `
-    <nz-datepicker [(ngModel)]="_date" [nzPlaceHolder]="'Select date'" [nzDisabledDate]="_disabledDate"></nz-datepicker>`,
+    <nz-datepicker [(ngModel)]="_date" [nzPlaceHolder]="'Select date'" [nzDisabledDate]="_disabledDate"></nz-datepicker>
+    <nz-datepicker [(ngModel)]="_moment" [nzPlaceHolder]="'Select month'" [nzMode]="'month'" [nzDisabledDate]="_disabledMonth" [nzFormat]="'YYYY-MM'"></nz-datepicker>
+  `,
   styles  : []
 })
 export class NzDemoDatePickerDisableDateComponent implements OnInit {
   _date = null;
+  _moment = null;
   _disabledDate = function (current) {
     return current && current.getTime() > Date.now();
+  };
+
+  _disabledMonth = function (current) {
+    return current && moment(current).day(0).valueOf() > moment().valueOf();
   };
 
   constructor() {
