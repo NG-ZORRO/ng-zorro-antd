4a4e3937568021335879c575b280278b49ff21d3
卡色 <cipchk@qq.com>
2017-11-16 16:15:49 +0800

    fix(module:popover,steps): fix popover always show title placeholder & steps nzDescription property support TemplateRef (#556)
    * fix(module:popover): fix always show title placeholder
    
    * feat(module:steps): nzDescription property support TemplateRef
    
    close #555 close #523

--- src/showcase/nz-demo-steps/nz-demo-steps-basic.component.ts
+++ src/showcase/nz-demo-steps/nz-demo-steps-basic.component.ts
@@ -5,7 +5,12 @@ import { Component, OnInit } from '@angular/core';
   template: `
     <nz-steps [(nzCurrent)]="current">
       <nz-step [nzTitle]="'Finished'" [nzDescription]="'This is a description.'"></nz-step>
-      <nz-step [nzTitle]="'In Progress'" [nzDescription]="'This is a description.'"></nz-step>
+      <nz-step [nzTitle]="'In Progress'" [nzDescription]="nzDescription">
+        <ng-template #nzDescription>
+          <p>This is a description.</p>
+          <nz-progress [ngModel]="30" [nzStrokeWidth]="5"></nz-progress>
+        </ng-template>
+      </nz-step>
       <nz-step [nzTitle]="'Waiting'" [nzDescription]="'This is a description.'"></nz-step>
     </nz-steps>
   `
