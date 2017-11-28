07e192c2e4b18dfeb3637d6caca6b6aea249a268
执衡 <yadong.xyd@alibaba-inc.com>
2017-11-28 22:22:14 +0800

    fix(module:steps) support dynamic steps change close #628

--- src/components/steps/nz-steps.module.ts
+++ src/components/steps/nz-steps.module.ts
@@ -3,13 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { NzStepsComponent } from './nz-steps.component';
 import { NzStepComponent } from './nz-step.component';
-import { NzStepConnectService } from './nz-step-connect.service';
 
 @NgModule({
   imports     : [ CommonModule ],
   exports     : [ NzStepsComponent, NzStepComponent ],
   declarations: [ NzStepsComponent, NzStepComponent ],
-  providers   : [ NzStepConnectService ]
 })
 export class NzStepsModule {
 }
