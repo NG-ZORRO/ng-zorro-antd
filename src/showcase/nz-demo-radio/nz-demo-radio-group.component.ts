15c968cfd6617f43fa80ac146e34f66f87320704
卡色 <cipchk@qq.com>
2017-12-30 13:56:23 +0800

    feat(module:radio): use nz-radio components alone (#825)

--- src/showcase/nz-demo-radio/nz-demo-radio-group.component.ts
+++ src/showcase/nz-demo-radio/nz-demo-radio-group.component.ts
@@ -3,6 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'nz-demo-radio-group',
   template: `
+    <div>
+      <label nz-radio [(ngModel)]="singleValue">
+        <span>独立使用 {{ singleValue }}</span>
+      </label>
+    </div>
     <nz-radio-group [(ngModel)]="radioValue">
       <label nz-radio [nzValue]="'A'">
         <span>A</span>
@@ -21,4 +26,5 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NzDemoRadioGroupComponent {
   radioValue = 'A';
+  singleValue = true;
 }
