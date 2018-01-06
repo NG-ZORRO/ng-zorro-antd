addc0913d0580f25142c09a3639da6ac7e52d74d
执衡 <yadong.xyd@alibaba-inc.com>
2018-01-06 10:37:12 +0800

    fix(module:radio): fix radio button click bug close #854

--- src/components/radio/nz-radio-button.component.ts
+++ src/components/radio/nz-radio-button.component.ts
@@ -24,8 +24,8 @@ import { NzRadioComponent } from './nz-radio.component';
   ]
 })
 export class NzRadioButtonComponent extends NzRadioComponent implements OnInit {
-  private _radioButtonDisabled = false;
-  private _radioButtonChecked = false;
+  _checked = false;
+  _disabled = false;
   _prefixCls = 'ant-radio-button';
   _innerPrefixCls = `${this._prefixCls}-inner`;
   _inputPrefixCls = `${this._prefixCls}-input`;
@@ -33,22 +33,22 @@ export class NzRadioButtonComponent extends NzRadioComponent implements OnInit {
   @Input()
   @HostBinding('class.ant-radio-button-wrapper-disabled')
   set nzDisabled(value: boolean) {
-    this._radioButtonDisabled = toBoolean(value);
+    this._disabled = toBoolean(value);
     this.setClassMap();
   }
 
   get nzDisabled(): boolean {
-    return this._radioButtonDisabled;
+    return this._disabled;
   }
 
   @Input()
   @HostBinding('class.ant-radio-button-wrapper-checked')
   set nzChecked(value: boolean) {
-    this._radioButtonChecked = toBoolean(value);
+    this._checked = toBoolean(value);
     this.setClassMap();
   }
 
   get nzChecked(): boolean {
-    return this._radioButtonChecked;
+    return this._checked;
   }
 }
