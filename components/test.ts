7043d95cb8f0f2bfce580ce0c12e1b3a1eef69d6
cipchk <cipchk@qq.com>
2018-01-17 19:46:16 +0800

    chore: doc site work simplification

--- components/test.ts
+++ components/test.ts
@@ -26,6 +26,7 @@ getTestBed().initTestEnvironment(
 );
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
+// const context = require.context('./', true, /nz-affix\.component\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
 // Finally, start Karma to run the tests.
