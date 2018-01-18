cffac94e61bc3fab689bc868329dce6ef186ae8f
Hsuan Lee <HsuanGM@gmail.com>
2018-01-18 16:43:44 +0800

    feat(module:input): add nzOnSearch property (#876)
    close #813

--- src/showcase/nz-demo-input/nz-demo-input-search.component.ts
+++ src/showcase/nz-demo-input/nz-demo-input-search.component.ts
@@ -3,11 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'nz-demo-input-search',
   template: `
-    <nz-input [nzType]="'search'" [nzPlaceHolder]="'input search text'" [(ngModel)]="_value" style="width: 200px;"></nz-input>
+    <nz-input [nzType]="'search'" [nzPlaceHolder]="'input search text'" [(ngModel)]="_value" style="width: 200px;" (nzOnSearch)="onSearch($event)"></nz-input>
   `,
 
   styles: []
 })
 export class NzDemoInputSearchComponent {
-  _value: string;
+  _value = '';
+
+  onSearch(event: string): void {
+    console.log(event);
+  }
 }
