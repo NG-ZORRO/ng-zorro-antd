7842733fe653865a9ade4e716f6bf060e0d3dc5e
执衡 <yadong.xyd@alibaba-inc.com>
2018-01-19 13:01:13 +0800

    fix(ci): fix travic ci error

--- src/showcase/share/nz-highlight/nz-highlight.component.ts
+++ src/showcase/share/nz-highlight/nz-highlight.component.ts
@@ -1,34 +1,29 @@
-import { Input, Component, OnInit, ViewEncapsulation, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
-import * as HighLight from 'highlight.js';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import * as Prism from 'prismjs';
 
 @Component({
-  selector     : 'nz-highlight',
-  encapsulation: ViewEncapsulation.None,
-  template     : `
-    <pre [ngClass]="'language-'+nzLanguage"><code #code [innerText]="nzCode"></code></pre>
+  selector: 'nz-highlight',
+  template: `
+    <pre [ngClass]="'language-'+nzLanguage"><code [innerHTML]="nzCode"></code></pre>
   `
 })
-export class NzHighlightComponent implements OnInit, AfterViewInit {
+export class NzHighlightComponent implements OnInit {
   _code;
   @ViewChild('code') codeElement: ElementRef;
   @Input() nzLanguage: string;
 
   @Input()
-  get nzCode() {
+  get nzCode(): string {
     return this._code || '';
   }
 
-  set nzCode(value) {
-    this._code = value.replace('../../../index.showcase', 'ng-zorro-antd');
-  }
-
-  ngAfterViewInit() {
-    (<any>HighLight).highlightBlock(this.codeElement.nativeElement);
+  set nzCode(value: string) {
+    this._code = (Prism as any).highlight(value, (Prism.languages as any).javascript);
   }
 
   constructor() {
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
   }
 }
