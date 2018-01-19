b172f3ef22b207de4bb97b46555b14a37a5643c9
执衡 <yadong.xyd@alibaba-inc.com>
2018-01-19 18:41:10 +0800

    refactor(doc): change highlight.js to prismjs

--- components/button/demo/size.ts
+++ components/button/demo/size.ts
@@ -4,27 +4,37 @@ import { Component } from '@angular/core';
   selector: 'nz-demo-button-size',
   template: `
     <nz-radio-group [(ngModel)]="size">
-      <label nz-radio-button [nzValue]="'large'"><span>Large</span></label>
-      <label nz-radio-button [nzValue]="'default'"><span>Default</span></label>
-      <label nz-radio-button [nzValue]="'small'"><span>Small</span></label>
+      <label nz-radio-button nzValue="large"><span>Large</span></label>
+      <label nz-radio-button nzValue="default"><span>Default</span></label>
+      <label nz-radio-button nzValue="small"><span>Small</span></label>
     </nz-radio-group>
     <br>
     <br>
-    <button nz-button [nzType]="'primary'" [nzSize]="size" [nzShape]="'circle'">
+    <button nz-button [nzSize]="size" nzType="primary">
+      <span>Primary</span>
+    </button>
+    <button nz-button [nzSize]="size" nzType="default">
+      <span>Default</span>
+    </button>
+    <button nz-button [nzSize]="size" nzType="dashed">
+      <span>Dashed</span>
+    </button>
+    <button nz-button [nzSize]="size" nzType="danger">
+      <span>Danger</span>
+    </button>
+    <br>
+    <button nz-button nzType="primary" [nzSize]="size" nzShape="circle">
       <i class="anticon anticon-download"></i>
     </button>
-    <button nz-button [nzType]="'primary'" [nzSize]="size">
+    <button nz-button nzType="primary" [nzSize]="size">
       <i class="anticon anticon-download"></i><span>Download</span>
-    </button>
-    <button nz-button [nzType]="'primary'" [nzSize]="size">
-      <span>Normal</span>
     </button>
     <br>
     <nz-button-group [nzSize]="size">
-      <button nz-button [nzType]="'primary'">
+      <button nz-button nzType="primary">
         <i class="anticon anticon-left"></i><span>Backward</span>
       </button>
-      <button nz-button [nzType]="'primary'">
+      <button nz-button nzType="primary">
         <span>Forward</span><i class="anticon anticon-right"></i>
       </button>
     </nz-button-group>
@@ -32,5 +42,5 @@ import { Component } from '@angular/core';
   styles  : []
 })
 export class NzDemoButtonSizeComponent {
-  size = 'default';
+  size = 'large';
 }
--- scripts/utils/marked.js
+++ scripts/utils/marked.js
@@ -1,15 +1,17 @@
 const marked = require('marked');
 
 const renderer = new marked.Renderer();
+var Prism = require('node-prismjs');
 
 renderer.heading = function (text, level) {
-  const lowerText = text.toLowerCase().replace(/ /g,'-').replace(/\./g,'-').replace(/\?/g,'');
+  const lowerText = text.toLowerCase().replace(/ /g, '-').replace(/\./g, '-').replace(/\?/g, '');
   return `<h${level} id="${lowerText}"><span>${text}</span><a onclick="window.location.hash = '${lowerText}'" class="anchor">#</a></h${level}>`
 };
 
 marked.setOptions({
-  highlight: function (code) {
-    return require('highlight.js').highlightAuto(code).value;
+  highlight: function (code,lang) {
+    const language = Prism.languages[lang] || Prism.languages.autoit;
+    return Prism.highlight(code, language);
   },
   renderer : renderer
 });
