c41ce09d15caeea243e800e1319496f0ba0a4a25
VTHINKXIE <vthinkxie@users.noreply.github.com>
2018-02-23 19:05:29 +0800

    refactor(module:switch): refactor switch (#1067)
    * wip(module:switch): refactor switch
    
    * test(module:switch): add test for switch

--- components/switch/demo/text.ts
+++ components/switch/demo/text.ts
@@ -3,17 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'nz-demo-switch-text',
   template: `
-    <nz-switch [ngModel]="true">
-      <i class=" anticon anticon-check" checked></i>
-      <i class=" anticon anticon-cross" unchecked></i>
-    </nz-switch>
-    <div style="margin-top:8px;">
-      <nz-switch [ngModel]="false">
-        <span checked>开</span>
-        <span unchecked>关</span>
-      </nz-switch>
-    </div>
+    <nz-switch [ngModel]="true" nzCheckedChildren="开" nzUnCheckedChildren="关"></nz-switch>
+    <br>
+    <nz-switch [ngModel]="false" nzCheckedChildren="1" nzUnCheckedChildren="0"></nz-switch>
+    <br>
+    <nz-switch [ngModel]="true" [nzCheckedChildren]="checkedTemplate" [nzUnCheckedChildren]="unCheckedTemplate"></nz-switch>
+    <ng-template #checkedTemplate><i class="anticon anticon-check"></i></ng-template>
+    <ng-template #unCheckedTemplate><i class="anticon anticon-cross"></i></ng-template>
   `,
-  styles  : []
+  styles  : [ `
+    nz-switch {
+      margin-bottom: 8px;
+    }`
+  ]
 })
-export class NzDemoSwitchTextComponent { }
+export class NzDemoSwitchTextComponent {
+}
