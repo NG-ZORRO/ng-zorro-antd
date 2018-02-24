827ad7108caf1527f1b17677f1250fcdc36094c6
VTHINKXIE <vthinkxie@users.noreply.github.com>
2018-02-24 11:08:30 +0800

    refactor(module:badge): refactor badge (#1069)
    * wip(module:badge): refactor badge
    
    * test(module:badge): add test for badge

--- components/badge/demo/dot.ts
+++ components/badge/demo/dot.ts
@@ -3,25 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'nz-demo-badge-dot',
   template: `
-    <nz-badge nzDot>
-      <ng-template #content><i class="anticon anticon-notification"></i></ng-template>
-    </nz-badge>
-
-    <nz-badge nzDot>
-      <ng-template #content>
-        <a>Link something</a>
-      </ng-template>
-    </nz-badge>
-
+    <nz-badge nzDot><i class="anticon anticon-notification"></i></nz-badge>
+    <nz-badge nzDot><a>Link something</a></nz-badge>
   `,
-  styles  : [ `
-    ::host ::ng-deep .anticon-notification {
+  styles  : [
+      `.anticon-notification {
       width: 16px;
       height: 16px;
       line-height: 16px;
       font-size: 16px;
-    }
-  ` ]
+    }`
+  ]
 })
-export class NzDemoBadgeDotComponent { }
-
+export class NzDemoBadgeDotComponent {
+}
