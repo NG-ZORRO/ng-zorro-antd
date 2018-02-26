5478c8e4b78723493ed7ea5a3826946baab6e664
VTHINKXIE <vthinkxie@users.noreply.github.com>
2018-02-26 18:34:41 +0800

    refactor(module:carousel): refactor carousel (#1072)
    * wip(module:carousel): refactor carousel
    
    * test(module:carousel): add test for carousel

--- components/carousel/nz-carousel.component.ts
+++ components/carousel/nz-carousel.component.ts
@@ -5,14 +5,16 @@ import {
   ElementRef,
   EventEmitter,
   HostBinding,
-  HostListener,
   Input,
+  NgZone,
   OnDestroy,
   Output,
   QueryList,
   Renderer2,
   ViewChild
 } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { first } from 'rxjs/operators/first';
 
 import { toBoolean } from '../core/util/convert';
 
@@ -23,14 +25,17 @@ import { NzCarouselContentDirective } from './nz-carousel-content.directive';
   preserveWhitespaces: false,
   template           : `
     <div class="slick-initialized slick-slider" [class.slick-vertical]="nzVertical">
-      <div class="slick-list" #slickList>
-        <div class="slick-track" style="opacity: 1;" [style.transform]="transform" #slickTrack>
+      <div class="slick-list" #slickList tabindex="-1" (keydown)="onKeyDown($event)">
+        <div class="slick-track" [style.transform]="transform" #slickTrack>
           <ng-content></ng-content>
         </div>
       </div>
-      <ul class="slick-dots" style="display: block;" *ngIf="nzDots">
-        <li [class.slick-active]="content.isActive" *ngFor="let content of slideContents; let i =index" (click)="setActive(content,i)">
-          <button>1</button>
+      <ul class="slick-dots" *ngIf="nzDots">
+        <li
+          *ngFor="let content of slideContents; let i =index"
+          [class.slick-active]="content.isActive"
+          (click)="setActive(content,i)">
+          <button>{{i + 1}}</button>
         </li>
       </ul>
     </div>`,
@@ -45,9 +50,16 @@ import { NzCarouselContentDirective } from './nz-carousel-content.directive';
         width: 100%;
         height: 100%;
       }
+
+      .slick-dots {
+        display: block;
+      }
+
       .slick-track {
+        opacity: 1;
         transition: all 0.5s ease;
       }
+
       .slick-slide {
         transition: opacity 500ms ease;
       }
@@ -59,68 +71,24 @@ export class NzCarouselComponent implements AfterViewInit, OnDestroy {
   private _autoPlay = false;
   private _dots = true;
   private _vertical = false;
-  private _pauseOnHover = true;
+  private _effect = 'scrollx';
+  slideContentsSubscription: Subscription;
   activeIndex = 0;
   transform = 'translate3d(0px, 0px, 0px)';
   interval;
-  slideContents: QueryList<NzCarouselContentDirective>;
-  _autoPlaySpeed = 3000;
-  _mouseHover = false;
 
-  @ContentChildren(NzCarouselContentDirective)
-  set _slideContents(value: QueryList<NzCarouselContentDirective>) {
-    this.slideContents = value;
-    this.renderContent();
-  }
-
+  @ContentChildren(NzCarouselContentDirective) slideContents: QueryList<NzCarouselContentDirective>;
   @ViewChild('slickList') slickList: ElementRef;
   @ViewChild('slickTrack') slickTrack: ElementRef;
-  @HostBinding('class.ant-carousel') _nzCarousel = true;
-  @Input() nzEffect = 'scrollx';
   @Output() nzAfterChange: EventEmitter<number> = new EventEmitter();
-  @Output() nzBeforeChange: EventEmitter<{ form: number; to: number }> = new EventEmitter();
+  @Output() nzBeforeChange: EventEmitter<{ from: number; to: number }> = new EventEmitter();
 
-  @Input()
-  get nzAutoPlaySpeed(): number {
-    return this._autoPlaySpeed;
-  }
-
-  set nzAutoPlaySpeed(speed: number) {
-    // css transition speed is 500ms
-    this._autoPlaySpeed = Math.max(speed, 500);
-  }
-
-  @HostListener('mouseenter')
-  _onMouseenter(): void {
-    this._mouseHover = true;
-    if (this.nzAutoPlay && this.nzPauseOnHover) {
-      this.clearInterval();
-    }
-  }
-
-  @HostListener('mouseleave')
-  _onMouseleave(): void {
-    this._mouseHover = false;
-    if (!this.interval && this.nzAutoPlay) {
-      this.createInterval();
-    }
-  }
-
-  get _nextIndex(): number {
+  get nextIndex(): number {
     return this.activeIndex < this.slideContents.length - 1 ? (this.activeIndex + 1) : 0;
   }
 
-  get _prevIndex(): number {
+  get prevIndex(): number {
     return this.activeIndex > 0 ? (this.activeIndex - 1) : (this.slideContents.length - 1);
-  }
-
-  @Input()
-  set nzPauseOnHover(value: boolean) {
-    this._pauseOnHover = toBoolean(value);
-  }
-
-  get nzPauseOnHover(): boolean {
-    return this._pauseOnHover;
   }
 
   @Input()
@@ -133,8 +101,19 @@ export class NzCarouselComponent implements AfterViewInit, OnDestroy {
   }
 
   @Input()
+  set nzEffect(value: string) {
+    this._effect = value;
+    this.updateMode();
+  }
+
+  get nzEffect(): string {
+    return this._effect;
+  }
+
+  @Input()
   set nzAutoPlay(value: boolean) {
     this._autoPlay = toBoolean(value);
+    this.setUpAutoPlay();
   }
 
   get nzAutoPlay(): boolean {
@@ -145,77 +124,82 @@ export class NzCarouselComponent implements AfterViewInit, OnDestroy {
   @HostBinding('class.ant-carousel-vertical')
   set nzVertical(value: boolean) {
     this._vertical = toBoolean(value);
+    this.updateMode();
   }
 
   get nzVertical(): boolean {
     return this._vertical;
   }
 
-  constructor(public hostElement: ElementRef, private _renderer: Renderer2) {
-  }
-
   setActive(content: NzCarouselContentDirective, i: number): void {
-    if ((this.nzAutoPlay && !this.nzPauseOnHover) || (this.nzAutoPlay && this.nzPauseOnHover && !this._mouseHover)) {
-      this.createInterval();
-    }
-    const beforeIndex = this.slideContents.toArray().findIndex(slide => slide.isActive);
-    this.nzBeforeChange.emit({ form: beforeIndex, to: i });
-    this.activeIndex = i;
-    if (this.nzEffect !== 'fade') {
-      if (!this.nzVertical) {
-        this.transform = `translate3d(${-this.activeIndex * this.hostElement.nativeElement.offsetWidth}px, 0px, 0px)`;
+    if (this.slideContents && this.slideContents.length) {
+      this.setUpAutoPlay();
+      const beforeIndex = this.slideContents.toArray().findIndex(slide => slide.isActive);
+      this.nzBeforeChange.emit({ from: beforeIndex, to: i });
+      this.activeIndex = i;
+      if (this.nzEffect === 'scrollx') {
+        if (this.nzVertical) {
+          this.transform = `translate3d(0px, ${-this.activeIndex * this.elementRef.nativeElement.offsetHeight}px, 0px)`;
+        } else {
+          this.transform = `translate3d(${-this.activeIndex * this.elementRef.nativeElement.offsetWidth}px, 0px, 0px)`;
+        }
       } else {
-        this.transform = `translate3d(0px, ${-this.activeIndex * this.hostElement.nativeElement.offsetHeight}px, 0px)`;
+        this.transform = 'translate3d(0px, 0px, 0px)';
       }
+      this.slideContents.forEach(slide => slide.isActive = slide === content);
+      this.nzAfterChange.emit(i);
     }
-    this.slideContents.forEach(slide => slide.isActive = false);
-    content.isActive = true;
-    this.nzAfterChange.emit(i);
-  }
-
-  ngAfterViewInit(): void {
-    this.renderContent();
   }
 
   renderContent(): void {
-    setTimeout(_ => {
-      if (this.slideContents.first) {
-        this.slideContents.first.isActive = true;
-      }
+    if (this.slideContents && this.slideContents.length) {
+      this.slideContents.first.isActive = true;
       this.slideContents.forEach((content, i) => {
-        content.width = this.hostElement.nativeElement.offsetWidth;
+        content.width = this.elementRef.nativeElement.offsetWidth;
         if (this.nzEffect === 'fade') {
           content.fadeMode = true;
-          if (!this.nzVertical) {
-            content.left = -i * content.width;
+          if (this.nzVertical) {
+            content.top = -i * this.elementRef.nativeElement.offsetHeight;
           } else {
-            content.top = -i * this.hostElement.nativeElement.offsetHeight;
+            content.left = -i * content.width;
           }
+        } else {
+          content.fadeMode = false;
+          content.left = null;
+          content.top = null;
         }
       });
-      if (this.nzAutoPlay) {
-        this.createInterval();
-      }
-
       if (this.nzVertical) {
-        this._renderer.removeStyle(this.slickList.nativeElement, 'height');
-        if (this.slideContents.first) {
-          this._renderer.setStyle(this.slickList.nativeElement, 'height', `${this.slideContents.first.nativeElement.offsetHeight}px`);
-        }
-        this._renderer.removeStyle(this.slickTrack.nativeElement, 'height');
-        this._renderer.setStyle(this.slickTrack.nativeElement, 'height', `${this.slideContents.length * this.hostElement.nativeElement.offsetHeight}px`);
+        this.renderer.removeStyle(this.slickTrack.nativeElement, 'width');
+        this.renderer.removeStyle(this.slickList.nativeElement, 'width');
+        this.renderer.removeStyle(this.slickList.nativeElement, 'height');
+        this.renderer.setStyle(this.slickList.nativeElement, 'height', `${this.slideContents.first.el.offsetHeight}px`);
+        this.renderer.removeStyle(this.slickTrack.nativeElement, 'height');
+        this.renderer.setStyle(this.slickTrack.nativeElement, 'height', `${this.slideContents.length * this.elementRef.nativeElement.offsetHeight}px`);
       } else {
-        this._renderer.removeStyle(this.slickTrack.nativeElement, 'width');
-        this._renderer.setStyle(this.slickTrack.nativeElement, 'width', `${this.slideContents.length * this.hostElement.nativeElement.offsetWidth}px`);
+        this.renderer.removeStyle(this.slickTrack.nativeElement, 'height');
+        this.renderer.removeStyle(this.slickList.nativeElement, 'height');
+        this.renderer.removeStyle(this.slickTrack.nativeElement, 'width');
+        this.renderer.setStyle(this.slickTrack.nativeElement, 'width', `${this.slideContents.length * this.elementRef.nativeElement.offsetWidth}px`);
       }
-    });
+      this.setUpAutoPlay();
+    }
   }
 
-  createInterval(): void {
+  setUpAutoPlay(): void {
     this.clearInterval();
-    this.interval = setInterval(_ => {
-      this.setActive(this.slideContents.toArray()[ this._nextIndex ], this._nextIndex);
-    }, this.nzAutoPlaySpeed);
+    if (this.nzAutoPlay) {
+      this.interval = setInterval(_ => {
+        this.setActive(this.slideContents.toArray()[ this.nextIndex ], this.nextIndex);
+      }, 3000);
+    }
+  }
+
+  updateMode(): void {
+    if (this.slideContents && this.slideContents.length) {
+      this.renderContent();
+      this.setActive(this.slideContents.first, 0);
+    }
   }
 
   clearInterval(): void {
@@ -225,21 +209,47 @@ export class NzCarouselComponent implements AfterViewInit, OnDestroy {
     }
   }
 
-  nzSlickNext(): void {
-    this.setActive(this.slideContents.toArray()[ this._nextIndex ], this._nextIndex);
+  next(): void {
+    this.setActive(this.slideContents.toArray()[ this.nextIndex ], this.nextIndex);
   }
 
-  nzSlickPrev(): void {
-    this.setActive(this.slideContents.toArray()[ this._prevIndex ], this._prevIndex);
+  pre(): void {
+    this.setActive(this.slideContents.toArray()[ this.prevIndex ], this.prevIndex);
   }
 
-  nzSlickGoTo(index: number): void {
+  goTo(index: number): void {
     if (index >= 0 && index <= this.slideContents.length - 1) {
       this.setActive(this.slideContents.toArray()[ index ], index);
     }
   }
 
+  onKeyDown(e: KeyboardEvent): void {
+    if (e.keyCode === 37) { // Left
+      this.pre();
+      e.preventDefault();
+    } else if (e.keyCode === 39) { // Right
+      this.next();
+      e.preventDefault();
+    }
+  }
+
+  constructor(public elementRef: ElementRef, private renderer: Renderer2, private zone: NgZone) {
+  }
+
+  ngAfterViewInit(): void {
+    this.slideContentsSubscription = this.slideContents.changes.subscribe(() => {
+      this.renderContent();
+    });
+    this.zone.onStable.pipe(first()).subscribe(() => {
+      this.renderContent();
+    });
+  }
+
   ngOnDestroy(): void {
+    if (this.slideContentsSubscription) {
+      this.slideContentsSubscription.unsubscribe();
+      this.slideContentsSubscription = null;
+    }
     this.clearInterval();
   }
 
---
+++ components/carousel/public-api.ts
@@ -0,0 +1,3 @@
+export * from './nz-carousel.module';
+export * from './nz-carousel.component';
+export * from './nz-carousel-content.directive';
