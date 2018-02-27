1ac61101344653e08d437dfffb687fcad024dd7d
执衡 <yadong.xyd@alibaba-inc.com>
2018-02-27 16:22:50 +0800

    refactor(module:timeline): refactor timeline

--- components/timeline/demo/custom.ts
+++ components/timeline/demo/custom.ts
@@ -6,15 +6,13 @@ import { Component } from '@angular/core';
     <nz-timeline>
       <nz-timeline-item>Create a services site 2015-09-01</nz-timeline-item>
       <nz-timeline-item>Solve initial network problems 2015-09-01</nz-timeline-item>
-      <nz-timeline-item [nzColor]="'red'">
-        <ng-template #custom>
-          <i class="anticon anticon-clock-circle-o" style="font-size: 16px;"></i>
-        </ng-template>
-        Technical testing 2015-09-01
-      </nz-timeline-item>
+      <nz-timeline-item nzColor="red" [nzDot]="dotTemplate">Technical testing 2015-09-01</nz-timeline-item>
       <nz-timeline-item>Network problems being solved 2015-09-01</nz-timeline-item>
-    </nz-timeline>`,
-  styles  : []
+    </nz-timeline>
+    <ng-template #dotTemplate>
+      <i class="anticon anticon-clock-circle-o" style="font-size: 16px;"></i>
+    </ng-template>
+  `
 })
 
 export class NzDemoTimelineCustomComponent {
