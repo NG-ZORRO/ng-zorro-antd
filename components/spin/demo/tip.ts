91b5220c13b6ac024af1be5584136d37eb8d9ca5
执衡 <yadong.xyd@alibaba-inc.com>
2018-02-28 16:34:51 +0800

    wip(module:spin): refactor spin

--- components/spin/demo/tip.ts
+++ components/spin/demo/tip.ts
@@ -3,10 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'nz-demo-spin-tip',
   template: `
-    <nz-spin [nzTip]="'Loading...'">
-      <nz-alert [nzType]="'info'" [nzMessage]="'Alert message title'" [nzDescription]="'Further details about the context of this alert.'">
+    <nz-spin nzTip='Loading...'>
+      <nz-alert
+        nzType="info"
+        nzMessage="Alert message title"
+        nzDescription="Further details about the context of this alert.">
       </nz-alert>
-    </nz-spin>`,
-  styles  : []
+    </nz-spin>`
 })
-export class NzDemoSpinTipComponent { }
+export class NzDemoSpinTipComponent {
+}
