58cd42c32133968ee5400bcc285a7d1950985305
卡色 <cipchk@qq.com>
2018-02-28 15:46:13 +0800

    feat(module:divider): add divider component (#1029)
    * wip(module:divider): add divider component
    
    * Use NzUpdateHostClassService Instead of class operation
    
    * fix doc
    
    * fix string and template invalid switched
    
    * fix tslint

--- components/divider/demo/horizontal.md
+++ components/divider/demo/horizontal.md
@@ -12,19 +12,3 @@ title:
 ## en-US
 
 Divider default type is `horizontal`. Support inner text inside Divider.
-
-````jsx
-import { Divider } from 'antd';
-
-ReactDOM.render(
-  <div>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.</p>
-    <Divider />
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.</p>
-    <Divider>With Text</Divider>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.</p>
-    <Divider dashed />
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.</p>
-  </div>
-, mountNode);
-````
---
+++ components/divider/divider.spec.ts
@@ -0,0 +1,84 @@
+import { Component, DebugElement, ViewChild } from '@angular/core';
+import { fakeAsync, tick, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
+import { NzDividerComponent } from './nz-divider.component';
+import { NzDividerModule } from './nz-divider.module';
+
+describe('divider', () => {
+  let fixture: ComponentFixture<TestDividerComponent>;
+  let context: TestDividerComponent;
+  let dl: DebugElement;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ NzDividerModule ],
+      declarations: [ TestDividerComponent, TestDividerTextTemplateComponent ]
+    }).compileComponents();
+    fixture = TestBed.createComponent(TestDividerComponent);
+    context = fixture.componentInstance;
+    dl = fixture.debugElement;
+    fixture.detectChanges();
+  });
+
+  describe('#nzDashed', () => {
+    for (const value of [ true, false ]) {
+      it(`[${value}]`, () => {
+        context.nzDashed = value;
+        fixture.detectChanges();
+        expect(dl.query(By.css('.ant-divider-dashed')) != null).toBe(value);
+      });
+    }
+  });
+
+  describe('#nzType', () => {
+    for (const value of [ 'horizontal', 'vertical' ]) {
+      it(`[${value}]`, () => {
+        context.nzType = value;
+        fixture.detectChanges();
+        expect(dl.query(By.css(`.ant-divider-${value}`)) != null).toBe(true);
+      });
+    }
+  });
+
+  describe('#nzText', () => {
+    for (const item of [ { text: 'with text', ret: true }, { text: undefined, ret: false } ]) {
+      it(`[${item.text}]`, () => {
+        context.nzText = item.text;
+        fixture.detectChanges();
+        expect(dl.query(By.css('.ant-divider-inner-text')) != null).toBe(item.ret);
+      });
+    }
+
+    it('should be custom template', () => {
+      let fixtureTemplate: ComponentFixture<TestDividerTextTemplateComponent>;
+      fixtureTemplate = TestBed.createComponent(TestDividerTextTemplateComponent);
+      fixtureTemplate.detectChanges();
+      expect(fixtureTemplate.debugElement.query(By.css('.anticon-plus')) != null).toBe(true);
+    });
+  });
+});
+
+@Component({
+  template: `
+  <nz-divider #comp
+    [nzDashed]="nzDashed"
+    [nzType]="nzType"
+    [nzText]="nzText"></nz-divider>
+  `
+})
+class TestDividerComponent {
+  @ViewChild('comp') comp: NzDividerComponent;
+  nzDashed = false;
+  nzType = 'horizontal';
+  nzText = 'with text';
+}
+
+@Component({
+  template: `
+  <nz-divider nzDashed [nzText]="text">
+    <ng-template #text><i class="anticon anticon-plus"></i> Add</ng-template>
+  </nz-divider>
+  `
+})
+class TestDividerTextTemplateComponent {
+}
