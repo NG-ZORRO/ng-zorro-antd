6e177334eebe115e6aa12af8c8b00a8820a45550
VTHINKXIE <vthinkxie@users.noreply.github.com>
2018-03-15 00:15:24 +0800

    ci: move integration test back (#1139)

---
+++ integration/rollup/rollup.config.js
@@ -0,0 +1,33 @@
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import uglify from 'rollup-plugin-uglify'
+import angularOptimizer from 'rollup-plugin-angular-optimizer'
+
+function enableProdMode() {
+  return {
+    name: 'enable-prod-mode',
+    transform(content, id) {
+      if (!id.includes('main.js')) { return }
+
+      return content
+        .replace(/platformBrowserDynamic/g, `platformBrowser`)
+        .replace(/bootstrapModule/g, `bootstrapModuleFactory`)
+        .replace(/AppModule/g, `AppModuleNgFactory`)
+        .replace(`from '@angular/platform-browser-dynamic'`, `from '@angular/platform-browser'`)
+        .replace(`from './environments/environment'`, `from './environments/environment.prod'`)
+        .replace(`from './app/app.module'`, `from './app/app.module.ngfactory'`)
+    },
+  }
+}
+
+
+export default {
+  input: './out-tsc/app/src/main.js',
+  output: {
+    file: './dist/main.bundle.js',
+    format: 'iife',
+  },
+  globals: {},
+  externals: [],
+  plugins: [enableProdMode(), resolve(), angularOptimizer(), commonjs(), uglify()],
+}
---
+++ integration/webpack/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgZorroAntdModule.forRoot(),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
