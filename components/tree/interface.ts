f935a6d8d90d3a0c67d1a73960ef0b2173d0da63
Jason <tangjie@yinspect.com>
2018-03-26 10:20:47 +0800

    fix:(module: tree): fix nzDefaultCheckedKeys & support beforeDrop event (#1211)

--- components/tree/interface.ts
+++ components/tree/interface.ts
@@ -1,32 +1,40 @@
 import { NzTreeNode } from './nz-tree-node';
 
 export interface NzFormatEmitEvent {
-    eventName: string;
-    node: NzTreeNode;
-    event: MouseEvent | DragEvent;
-    dragNode?: NzTreeNode;
-    selectedKeys?: NzTreeNode[];
-    checkedKeys?: NzTreeNode[];
+  eventName: string;
+  node: NzTreeNode;
+  event: MouseEvent | DragEvent;
+  dragNode?: NzTreeNode;
+  selectedKeys?: NzTreeNode[];
+  checkedKeys?: NzTreeNode[];
+  matchedKeys?: NzTreeNode[];
 }
 
 export interface NzFormatPosition {
-    top: number;
-    left: number;
+  top: number;
+  left: number;
 }
+
+export interface NzFormatBeforeDropEvent {
+  dragNode: NzTreeNode;
+  node: NzTreeNode;
+  pos: number;
+}
+
 export interface NzTreeNodeOptions {
-    title?: string;
-    key?: string;
-    children?: NzTreeNodeOptions[];
-    isLeaf?: boolean;
-    checked?: boolean;
-    selected?: boolean;
-    selectable?: boolean;
-    disabled?: boolean;
-    disableCheckbox?: boolean;
-    expanded?: boolean;
+  title: string;
+  key: string;
+  isLeaf?: boolean;
+  checked?: boolean;
+  selected?: boolean;
+  selectable?: boolean;
+  disabled?: boolean;
+  disableCheckbox?: boolean;
+  expanded?: boolean;
+  children?: NzTreeNodeOptions[];
 }
 
 export interface NzFormatClickEvent {
-    event: MouseEvent;
-    node: NzTreeNode;
+  event: MouseEvent;
+  node: NzTreeNode;
 }
