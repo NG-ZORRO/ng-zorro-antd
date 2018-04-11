c5efed0239321accc93acaf76c7142e563260818
Zhicheng Wang <zcwang@thoughtworks.com>
2018-04-11 10:25:11 +0800

    fix(time-picker): can't parse the time part properly feat(time-picker): auto set focus and select all text when popup

--- components/time-picker/nz-time-value-accessor.directive.ts
+++ components/time-picker/nz-time-value-accessor.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostBinding, Input } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, OnInit } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { NzI18nService } from '../i18n';
 
@@ -8,24 +8,51 @@ import { NzI18nService } from '../i18n';
     { provide: NG_VALUE_ACCESSOR, useExisting: NzTimeValueAccessorDirective, multi: true }
   ]
 })
-export class NzTimeValueAccessorDirective implements ControlValueAccessor {
-  @HostBinding('value')
-  value: string;
+export class NzTimeValueAccessorDirective implements ControlValueAccessor, OnInit {
+  constructor(private i18n: NzI18nService, private elementRef: ElementRef) {
+  }
+
+  ngOnInit(): void {
+    Promise.resolve().then(() => {
+      this.element.focus();
+    });
+  }
+
+  get element(): HTMLInputElement {
+    return this.elementRef.nativeElement;
+  }
+
   @Input('nzTime')
   format: string;
+
+  @HostListener('keyup')
+  keyup(): void {
+    this.changed();
+  }
+
+  @HostListener('blur')
+  blur(): void {
+    this.touched();
+  }
+
   private _onChange: (value: Date) => void;
   private _onTouch: () => void;
 
-  constructor(private i18n: NzI18nService) {
-  }
-
   writeValue(value: Date): void {
-    this.value = this.i18n.formatDate(value, this.format);
+    this.element.value = this.i18n.formatDate(value, this.format);
+    this.element.setSelectionRange(0, this.element.value.length);
   }
 
   changed(): void {
     if (this._onChange) {
-      this._onChange(this.i18n.parseDate(this.value));
+      const value = this.i18n.parseTime(this.element.value);
+      this._onChange(value);
+    }
+  }
+
+  touched(): void {
+    if (this._onTouch) {
+      this._onTouch();
     }
   }
 
