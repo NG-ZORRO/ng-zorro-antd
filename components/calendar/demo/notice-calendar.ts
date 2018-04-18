04aeb4527cc6e4b5113676b70f7f002fe4f75b2d
VTHINKXIE <vthinkxie@users.noreply.github.com>
2018-04-18 14:18:42 +0800

    doc(module:calendar): fix calendar doc (#1348)
    close #1347

--- components/calendar/demo/notice-calendar.ts
+++ components/calendar/demo/notice-calendar.ts
@@ -5,9 +5,23 @@ import { Component } from '@angular/core';
   template: `
     <nz-calendar>
       <ul *nzDateCell="let date" class="events">
-        <li *ngFor="let item of getListData(date)">
-          <nz-badge [nzStatus]="item.type" [nzText]="item.content"></nz-badge>
-        </li>
+        <ng-container [ngSwitch]="date.getDate()">
+          <ng-container *ngSwitchCase="8">
+            <li *ngFor="let item of listDataMap.eight">
+              <nz-badge [nzStatus]="item.type" [nzText]="item.content"></nz-badge>
+            </li>
+          </ng-container>
+          <ng-container *ngSwitchCase="10">
+            <li *ngFor="let item of listDataMap.ten">
+              <nz-badge [nzStatus]="item.type" [nzText]="item.content"></nz-badge>
+            </li>
+          </ng-container>
+          <ng-container *ngSwitchCase="11">
+            <li *ngFor="let item of listDataMap.eleven">
+              <nz-badge [nzStatus]="item.type" [nzText]="item.content"></nz-badge>
+            </li>
+          </ng-container>
+        </ng-container>
       </ul>
       <ng-container *nzMonthCell="let month">
         <div *ngIf="getMonthData(month) as monthData" class="notes-month">
@@ -17,12 +31,13 @@ import { Component } from '@angular/core';
       </ng-container>
     </nz-calendar>
   `,
-  styles: [`
+  styles  : [ `
     .events {
       list-style: none;
       margin: 0;
       padding: 0;
     }
+
     .events .ant-badge-status {
       overflow: hidden;
       white-space: nowrap;
@@ -30,48 +45,39 @@ import { Component } from '@angular/core';
       text-overflow: ellipsis;
       font-size: 12px;
     }
+
     .notes-month {
       text-align: center;
       font-size: 28px;
     }
+
     .notes-month section {
       font-size: 28px;
     }
-  `]
+  ` ]
 })
 export class NzDemoCalendarNoticeCalendarComponent {
-  getListData(date: Date): Array<{type: string, content: string}> {
-    let listData;
-    switch (date.getDate()) {
-      case 8:
-        listData = [
-          { type: 'warning', content: 'This is warning event.' },
-          { type: 'success', content: 'This is usual event.' },
-        ];
-        break;
-      case 10:
-        listData = [
-          { type: 'warning', content: 'This is warning event.' },
-          { type: 'success', content: 'This is usual event.' },
-          { type: 'error', content: 'This is error event.' },
-        ];
-        break;
-      case 15:
-        listData = [
-          { type: 'warning', content: 'This is warning event' },
-          { type: 'success', content: 'This is very long usual event........' },
-          { type: 'error', content: 'This is error event 1.' },
-          { type: 'error', content: 'This is error event 2.' },
-          { type: 'error', content: 'This is error event 3.' },
-          { type: 'error', content: 'This is error event 4.' },
-        ];
-        break;
-      default:
-    }
-    return listData || [];
-  }
+  listDataMap = {
+    eight : [
+      { type: 'warning', content: 'This is warning event.' },
+      { type: 'success', content: 'This is usual event.' }
+    ],
+    ten   : [
+      { type: 'warning', content: 'This is warning event.' },
+      { type: 'success', content: 'This is usual event.' },
+      { type: 'error', content: 'This is error event.' }
+    ],
+    eleven: [
+      { type: 'warning', content: 'This is warning event' },
+      { type: 'success', content: 'This is very long usual event........' },
+      { type: 'error', content: 'This is error event 1.' },
+      { type: 'error', content: 'This is error event 2.' },
+      { type: 'error', content: 'This is error event 3.' },
+      { type: 'error', content: 'This is error event 4.' }
+    ]
+  };
 
-  getMonthData(date: Date): number|null {
+  getMonthData(date: Date): number | null {
     if (date.getMonth() === 8) {
       return 1394;
     }
