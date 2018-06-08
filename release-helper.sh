3f34726c6c4e9403aab8589e827c8d960451566c
simplejason <tangjie.coder@gmail.com>
2018-06-08 14:23:17 +0800

    doc: add 0.7.x doc
    doc: add 0.7.x doc
    
    doc: add 0.7.x doc

--- release-helper.sh
+++ release-helper.sh
@@ -2,4 +2,4 @@
 rm -rf issue-helper && rm -rf ./dist/issue-helper && git clone https://github.com/NG-ZORRO/issue-helper.git && cp -R issue-helper/issue-helper ./dist/
 rm -rf issue-helper
 # download 0.5.x 0.6.x
-rm -rf ./dist/version && mkdir ./dist/version && cd ./dist/version && git clone https://github.com/NG-ZORRO/0.5.5-doc.git 0.5.x && git clone https://github.com/NG-ZORRO/0.6.x-doc.git 0.6.x
+rm -rf ./dist/version && mkdir ./dist/version && cd ./dist/version && git clone https://github.com/NG-ZORRO/0.5.5-doc.git 0.5.x && rm -rf 0.5.x/.git/ && git clone https://github.com/NG-ZORRO/0.6.x-doc.git 0.6.x && rm -rf 0.6.x/.git/ && git clone https://github.com/NG-ZORRO/0.7.x-doc.git 0.7.x && rm -rf 0.7.x/.git/
