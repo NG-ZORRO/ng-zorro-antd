70cb5912d2791e3e6e36a3f33e0c9f2cbd37828d
VTHINKXIE <vthinkxie@users.noreply.github.com>
2018-06-22 15:36:48 +0800

    fix(module:card): fix card loading style (#1696)
    close #1695

--- components/card/demo/loading.ts
+++ components/card/demo/loading.ts
@@ -3,10 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'nz-demo-card-loading',
   template: `
-    <nz-card style="width:34%;" nzLoading nzTitle="Card title">
+    <nz-card [nzLoading]="loading" nzTitle="Card title">
       Whatever content
     </nz-card>
+    <button nz-button style="margin-top: 16px;" (click)="toggleLoading()">Toggle loading</button>
   `
 })
 export class NzDemoCardLoadingComponent {
+  loading = true;
+  toggleLoading(): void {
+    this.loading = !this.loading;
+  }
 }
