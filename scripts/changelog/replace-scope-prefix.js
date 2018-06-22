49ac01c127841b8f07480f2664ee7ee967912a73
Hsuan Lee <HsuanGM@gmail.com>
2018-06-22 14:44:07 +0800

    docs(changelog): add changelog and the generate script (#1688)
    * docs(changelog): add changelog and the generate script
    
    fix devDependencies
    
    * add replace scope-prefix script
    
    * fix history

---
+++ scripts/changelog/replace-scope-prefix.js
@@ -0,0 +1,12 @@
+const fs = require('fs-extra');
+const path = require('path');
+
+const changelogPath = path.resolve(__dirname, `../../CHANGELOG.md`);
+
+async function replace(path) {
+  const content = await fs.readFile(path, 'utf8');
+  const replaced = content.replace(/\*\*\w+:(?!\*\*)/g, '**');
+  await fs.outputFile(path, replaced);
+}
+
+replace(changelogPath);
