d77a7d44ea2c1d358050800ad5ca8fd3a21fbc66
wen <wendzhue@gmail.com>
2018-09-02 18:44:33 +0800

    release(1.4.1): release 1.4.1 (#2082)

--- site_scripts/_site/src/app/share/nz-codebox/nz-codebox.component.ts
+++ site_scripts/_site/src/app/share/nz-codebox/nz-codebox.component.ts
@@ -393,7 +393,7 @@ export class AppModule { }
         '@angular/router'                  : '^6.0.0',
         '@angular/animations'              : '^6.0.0',
         'date-fns'                         : '^1.29.0',
-        'ng-zorro-antd'                    : '^1.4.0'
+        'ng-zorro-antd'                    : '^1.4.1'
       },
       tags        : [ 'stackblitz', 'sdk' ]
     });
