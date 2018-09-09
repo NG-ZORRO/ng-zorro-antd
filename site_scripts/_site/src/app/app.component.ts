2fde3835764c3273354071cbe0e489613b69f9ec
Jason <tangjie@yinspect.com>
2018-09-09 16:25:41 +0800

    release(1.5.0): release 1.5.0 (#2116)
    release(1.5.0): release 1.5.0

--- site_scripts/_site/src/app/app.component.ts
+++ site_scripts/_site/src/app/app.component.ts
@@ -28,7 +28,7 @@ export class AppComponent implements OnInit, AfterViewInit {
     '0.6.x',
     '0.7.x'
   ];
-  currentVersion = '1.4.1';
+  currentVersion = '1.5.0';
 
   @ViewChild('searchInput') searchInput: ElementRef<HTMLInputElement>;
 
