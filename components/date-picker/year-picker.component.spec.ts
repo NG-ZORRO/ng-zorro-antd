0704682e1236cedb0d66e9c08e38670eb44f4802
鸿则 <hungtcs@163.com>
2018-09-19 19:14:32 +0800

    fix(module: date-picker): fix year-picker and month-picker style error within compacted input group (#2136)
    GLTM

--- components/date-picker/year-picker.component.spec.ts
+++ components/date-picker/year-picker.component.spec.ts
@@ -9,6 +9,7 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import * as isBefore from 'date-fns/is_before';
 import { dispatchMouseEvent } from '../core/testing';
+import { NzInputModule } from '../input/nz-input.module';
 import { NzDatePickerModule } from './date-picker.module';
 import { CandyDate } from './lib/candy-date';
 import { PickerResultSingle } from './standard-types';
@@ -24,7 +25,7 @@ describe('NzYearPickerComponent', () => {
 
   beforeEach(fakeAsync(() => {
     TestBed.configureTestingModule({
-      imports     : [ FormsModule, NoopAnimationsModule, NzDatePickerModule ],
+      imports     : [ FormsModule, NoopAnimationsModule, NzDatePickerModule, NzInputModule ],
       providers   : [],
       declarations: [
         NzTestYearPickerComponent
@@ -146,8 +147,19 @@ describe('NzYearPickerComponent', () => {
     it('should support nzClassName', () => {
       const className = fixtureInstance.nzClassName = 'my-test-class';
       fixture.detectChanges();
-      const picker = debugElement.query(By.css('.ant-calendar-picker')).nativeElement as HTMLElement;
+      const picker = debugElement.queryAll(By.css('.ant-calendar-picker'))[1].nativeElement as HTMLElement;
       expect(picker.classList.contains(className)).toBeTruthy();
+    });
+
+    it('should support nzCompact', () => {
+      fixtureInstance.useSuite = 4;
+
+      fixture.detectChanges();
+      const pickerInput = debugElement.query(By.css('input.ant-calendar-picker-input')).nativeElement as HTMLElement;
+      expect(pickerInput).not.toBeNull();
+      const compStyles = window.getComputedStyle(pickerInput);
+      expect(compStyles.getPropertyValue('border-top-right-radius') === '0px').toBeTruthy();
+      expect(compStyles.getPropertyValue('border-bottom-right-radius') === '0px').toBeTruthy();
     });
 
     it('should support nzLocale', () => {
@@ -386,11 +398,18 @@ describe('NzYearPickerComponent', () => {
 
       <!-- Suite 3 -->
       <nz-year-picker *ngSwitchCase="3" nzOpen [(ngModel)]="modelValue"></nz-year-picker>
+
+      <!-- Suite 4 -->
+      <nz-input-group *ngSwitchCase="4" nzCompact>
+        <nz-year-picker style="width: 200px;"></nz-year-picker>
+        <input nz-input type="text" style="width: 200px;" />
+      </nz-input-group>
+
     </ng-container>
   `
 })
 class NzTestYearPickerComponent {
-  useSuite: 1 | 2 | 3;
+  useSuite: 1 | 2 | 3 | 4;
   @ViewChild('tplExtraFooter') tplExtraFooter: TemplateRef<void>;
 
   // --- Suite 1
