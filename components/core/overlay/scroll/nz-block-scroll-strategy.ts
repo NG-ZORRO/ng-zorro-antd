a232d599b811965d74022ca4cc7be82a082ba523
Hsuan Lee <HsuanGM@gmail.com>
2018-09-21 18:26:40 +0800

    feat(module:drawer): support service to create drawer (#1981)
    close #1937

--- components/core/overlay/scroll/nz-block-scroll-strategy.ts
+++ components/core/overlay/scroll/nz-block-scroll-strategy.ts
@@ -1,19 +1,23 @@
 import { ScrollStrategy } from '@angular/cdk/overlay';
 import { Renderer2 } from '@angular/core';
+import { NzMeasureScrollbarService } from '../../services/nz-measure-scrollbar.service'
 
 export class NzBlockScrollStrategy implements ScrollStrategy {
 
-  constructor(private document: Document, private renderer: Renderer2) {
+  constructor(private document: Document, private renderer: Renderer2, private nzMeasureScrollbarService: NzMeasureScrollbarService) {
   }
 
   attach(): void {}
 
   enable(): void {
     this.renderer.setStyle(document.body, 'overflow', 'hidden');
+    this.renderer.setStyle(this.document.body, 'padding-right', `${this.nzMeasureScrollbarService.scrollBarWidth}px`);
+
   }
 
   disable(): void {
     this.renderer.removeStyle(document.body, 'overflow');
+    this.renderer.removeStyle(document.body, 'padding-right');
   }
 
 }
--- components/drawer/nz-drawer.component.ts
+++ components/drawer/nz-drawer.component.ts
@@ -1,24 +1,39 @@
+import { DOCUMENT } from '@angular/common';
 import {
+  AfterViewInit,
   ChangeDetectionStrategy,
+  ChangeDetectorRef,
   Component,
   ElementRef,
   EventEmitter,
-  Input, OnDestroy,
+  Inject,
+  Injector,
+  Input,
+  OnChanges,
+  OnDestroy,
   OnInit,
+  Optional,
   Output,
   Renderer2,
+  SimpleChanges,
   TemplateRef,
+  Type,
   ViewChild,
   ViewContainerRef
 } from '@angular/core';
 
-import { CdkOverlayOrigin, Overlay, OverlayConfig, OverlayRef } from '@angular/cdk/overlay';
-import { TemplatePortal } from '@angular/cdk/portal';
+import { FocusTrap, FocusTrapFactory } from '@angular/cdk/a11y';
+import { Overlay, OverlayConfig, OverlayRef } from '@angular/cdk/overlay';
+import { CdkPortalOutlet, ComponentPortal, PortalInjector, TemplatePortal } from '@angular/cdk/portal';
+
+import { Observable, Subject } from 'rxjs';
+
 import { NzScrollStrategyOptions } from '../core/overlay/scroll/nz-scroll-strategy-options';
-
 import { toCssPixel, InputBoolean } from '../core/util/convert';
+import { NzDrawerOptions, NzDrawerPlacement } from './nz-drawer-options';
+import { NzDrawerRef } from './nz-drawer-ref';
 
-export type NzDrawerPlacement = 'top' | 'right' | 'bottom' | 'left';
+export const DRAWER_ANIMATE_DURATION = 300;
 
 @Component({
   selector           : 'nz-drawer',
@@ -26,11 +41,18 @@ export type NzDrawerPlacement = 'top' | 'right' | 'bottom' | 'left';
   preserveWhitespaces: false,
   changeDetection    : ChangeDetectionStrategy.OnPush
 })
-export class NzDrawerComponent implements OnInit, OnDestroy {
-  origin: CdkOverlayOrigin;
+// tslint:disable-next-line:no-any
+export class NzDrawerComponent<T = any, R = any, D = any> extends NzDrawerRef<R> implements OnInit, OnDestroy, AfterViewInit, OnChanges, NzDrawerOptions {
+  previouslyFocusedElement: HTMLElement;
+  nzContentParams: D; // only service
   overlayRef: OverlayRef;
   portal: TemplatePortal;
+  focusTrap: FocusTrap;
   isOpen = false;
+  templateContext: { $implicit: D; drawerRef: NzDrawerRef<R> } = {
+    $implicit: undefined,
+    drawerRef: this as NzDrawerRef<R>
+  };
 
   get transform(): string {
 
@@ -61,29 +83,43 @@ export class NzDrawerComponent implements OnInit, OnDestroy {
   }
 
   @ViewChild('drawerTemplate') drawerTemplate: TemplateRef<{}>;
+  @ViewChild('contentTemplate') contentTemplate: TemplateRef<{}>;
+  @ViewChild(CdkPortalOutlet) bodyPortalOutlet: CdkPortalOutlet;
+  @Input() nzContent: TemplateRef<{ $implicit: D, drawerRef: NzDrawerRef<R> }> | Type<T>;
   @Input() @InputBoolean() nzClosable = true;
   @Input() @InputBoolean() nzMaskClosable = true;
   @Input() @InputBoolean() nzMask = true;
   @Input() nzTitle: string | TemplateRef<{}>;
+  @Input() nzPlacement: NzDrawerPlacement = 'right';
   @Input() nzMaskStyle: object = {};
   @Input() nzBodyStyle: object = {};
   @Input() nzWrapClassName: string;
   @Input() nzWidth: number | string = 256;
   @Input() nzHeight: number | string = 256;
-  @Input() nzPlacement: NzDrawerPlacement = 'right';
   @Input() nzZIndex = 1000;
   @Input() nzOffsetX = 0;
   @Input() nzOffsetY = 0;
-  @Output() nzOnClose = new EventEmitter<MouseEvent>();
 
   @Input()
   set nzVisible(value: boolean) {
     this.isOpen = value;
-    this.updateOverlayStyle();
   }
 
   get nzVisible(): boolean {
     return this.isOpen;
+  }
+
+  @Output() nzOnViewInit = new EventEmitter<void>();
+  @Output() nzOnClose = new EventEmitter<MouseEvent>();
+  nzAfterOpen = new Subject<void>();
+  nzAfterClose = new Subject<R>();
+
+  get afterOpen(): Observable<void> {
+    return this.nzAfterOpen.asObservable();
+  }
+
+  get afterClose(): Observable<R> {
+    return this.nzAfterClose.asObservable();
   }
 
   isNonEmptyString(value: {}): boolean {
@@ -95,25 +131,104 @@ export class NzDrawerComponent implements OnInit, OnDestroy {
   }
 
   constructor(
+    // tslint:disable-next-line:no-any
+    @Optional() @Inject(DOCUMENT) private document: any,
     private renderer: Renderer2,
     private overlay: Overlay,
     private elementRef: ElementRef,
+    private injector: Injector,
+    private changeDetectorRef: ChangeDetectorRef,
+    private focusTrapFactory: FocusTrapFactory,
     private nzScrollStrategyOptions: NzScrollStrategyOptions,
     private viewContainerRef: ViewContainerRef) {
-
+    super();
   }
 
   ngOnInit(): void {
-    this.origin = new CdkOverlayOrigin(this.elementRef);
     this.attachOverlay();
     this.updateOverlayStyle();
+    this.updateBodyOverflow();
+    this.templateContext = { $implicit: this.nzContentParams, drawerRef: this as NzDrawerRef<R> };
+    this.changeDetectorRef.detectChanges();
+  }
+
+  ngAfterViewInit(): void {
+    this.attachBodyContent();
+    setTimeout(() => {
+      this.nzOnViewInit.emit();
+    });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.hasOwnProperty('nzVisible')) {
+      const value = changes.nzVisible.currentValue;
+      this.updateOverlayStyle();
+      if (value) {
+        this.updateBodyOverflow();
+        this.savePreviouslyFocusedElement();
+        this.trapFocus();
+      } else {
+        setTimeout(() => {
+          this.updateBodyOverflow();
+          this.restoreFocus();
+        }, DRAWER_ANIMATE_DURATION);
+      }
+    }
   }
 
   ngOnDestroy(): void {
     this.disposeOverlay();
   }
 
-  attachOverlay(): void {
+  close(result?: R): void {
+    this.isOpen = false;
+    this.updateOverlayStyle();
+    this.changeDetectorRef.detectChanges();
+    setTimeout(() => {
+      this.updateBodyOverflow();
+      this.restoreFocus();
+      this.nzAfterClose.next(result);
+      this.nzAfterClose.complete();
+    }, DRAWER_ANIMATE_DURATION);
+  }
+
+  open(): void {
+    this.isOpen = true;
+    this.updateOverlayStyle();
+    this.updateBodyOverflow();
+    this.savePreviouslyFocusedElement();
+    this.trapFocus();
+    this.changeDetectorRef.detectChanges();
+    setTimeout(() => {
+      this.nzAfterOpen.next();
+    }, DRAWER_ANIMATE_DURATION);
+  }
+
+  closeClick(): void {
+    this.nzOnClose.emit();
+  }
+
+  maskClick(): void {
+    if (this.nzMaskClosable && this.nzMask) {
+      this.nzOnClose.emit();
+    }
+  }
+
+  private attachBodyContent(): void {
+    this.bodyPortalOutlet.dispose();
+
+    if (this.nzContent instanceof Type) {
+      const childInjector = new PortalInjector(this.injector, new WeakMap([ [ NzDrawerRef, this ] ]));
+      const componentPortal = new ComponentPortal<T>(this.nzContent, null, childInjector);
+      const componentRef = this.bodyPortalOutlet.attachComponentPortal(componentPortal);
+      if (this.nzContentParams) {
+        Object.assign(componentRef.instance, this.nzContentParams);
+      }
+      componentRef.changeDetectorRef.detectChanges();
+    }
+  }
+
+  private attachOverlay(): void {
     if (!this.overlayRef) {
       this.portal = new TemplatePortal(this.drawerTemplate, this.viewContainerRef);
       this.overlayRef = this.overlay.create(this.getOverlayConfig());
@@ -124,20 +239,25 @@ export class NzDrawerComponent implements OnInit, OnDestroy {
     }
   }
 
-  disposeOverlay(): void {
+  private disposeOverlay(): void {
     this.overlayRef.dispose();
     this.overlayRef = null;
   }
 
-  getOverlayConfig(): OverlayConfig {
+  private getOverlayConfig(): OverlayConfig {
     return new OverlayConfig({
       scrollStrategy: this.nzScrollStrategyOptions.block()
     });
   }
 
-  updateOverlayStyle(): void {
+  private updateOverlayStyle(): void {
     if (this.overlayRef && this.overlayRef.overlayElement) {
       this.renderer.setStyle(this.overlayRef.overlayElement, 'pointer-events', this.isOpen ? 'auto' : 'none');
+    }
+  }
+
+  private updateBodyOverflow(): void {
+    if (this.overlayRef) {
       if (this.isOpen) {
         this.overlayRef.getConfig().scrollStrategy.enable();
       } else {
@@ -146,14 +266,26 @@ export class NzDrawerComponent implements OnInit, OnDestroy {
     }
   }
 
-  close(e?: MouseEvent): void {
-    this.nzOnClose.emit(e);
-  }
-
-  maskClick(e: MouseEvent): void {
-    if (this.nzMaskClosable && this.nzMask) {
-      this.close(e);
+  savePreviouslyFocusedElement(): void {
+    if (this.document) {
+      this.previouslyFocusedElement = this.document.activeElement as HTMLElement;
+      this.previouslyFocusedElement.blur();
     }
   }
 
+  private trapFocus(): void {
+    if (!this.focusTrap) {
+      this.focusTrap = this.focusTrapFactory.create(this.overlayRef.overlayElement);
+    }
+    this.focusTrap.focusInitialElementWhenReady();
+  }
+
+  private restoreFocus(): void {
+    if (this.previouslyFocusedElement) {
+      this.previouslyFocusedElement.focus();
+    }
+    if (this.focusTrap) {
+      this.focusTrap.destroy();
+    }
+  }
 }
