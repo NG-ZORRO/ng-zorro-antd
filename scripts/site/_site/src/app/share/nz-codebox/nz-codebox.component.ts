8f8eadd275d50efed6c515927cd577c55f68843e
VTHINKXIE <vthinkxie@users.noreply.github.com>
2018-10-23 19:05:58 +0800

    release(1.7.1): release 1.7.1 (#2337)

--- scripts/site/_site/src/app/share/nz-codebox/nz-codebox.component.ts
+++ scripts/site/_site/src/app/share/nz-codebox/nz-codebox.component.ts
@@ -386,7 +386,7 @@ export class AppModule { }
         '@angular/router'                  : '^6.0.0',
         '@angular/animations'              : '^6.0.0',
         'date-fns'                         : '^1.29.0',
-        'ng-zorro-antd'                    : '^1.7.0'
+        'ng-zorro-antd'                    : '^1.7.1'
       },
       tags        : [ 'stackblitz', 'sdk' ]
     });
