f531be86402d01c312b48a6d97db1bd3a1f69ad7
VTHINKXIE <vthinkxie@users.noreply.github.com>
2018-10-23 16:09:12 +0800

    fix(module:icon): fix icon miss in components (#2321)
    * fix(module:icon): fix icon miss in components
    
    * fix(module:icon): fix table icon
    close #2326

--- components/dropdown/nz-dropdown.directive.ts
+++ components/dropdown/nz-dropdown.directive.ts
@@ -24,6 +24,7 @@ export class NzDropDownDirective implements OnInit {
 
   @HostListener('click', [ '$event' ])
   onClick(e: MouseEvent): void {
+    e.stopPropagation();
     this.$click.next(e);
   }
 
