e2d09a0f0c0e52b4f498871d1889b1e26a0d9c64
卡色 <cipchk@qq.com>
2018-10-26 23:46:10 +0800

    feat(module:list): list empty style (#2365)
    close #2362

--- components/list/nz-list.module.ts
+++ components/list/nz-list.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { NzAvatarModule } from '../avatar/nz-avatar.module';
 import { NzGridModule } from '../grid/nz-grid.module';
+import { NzI18nModule } from '../i18n/nz-i18n.module';
 import { NzSpinModule } from '../spin/nz-spin.module';
 
 import { NzListItemMetaComponent } from './nz-list-item-meta.component';
@@ -10,7 +11,7 @@ import { NzListItemComponent } from './nz-list-item.component';
 import { NzListComponent } from './nz-list.component';
 
 @NgModule({
-    imports:        [ CommonModule, NzSpinModule, NzGridModule, NzAvatarModule ],
+    imports:        [ CommonModule, NzSpinModule, NzGridModule, NzAvatarModule, NzI18nModule ],
     declarations:   [ NzListComponent, NzListItemComponent, NzListItemMetaComponent ],
     exports:        [ NzListComponent, NzListItemComponent, NzListItemMetaComponent ]
 })
