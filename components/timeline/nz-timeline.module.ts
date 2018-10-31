1d5e51cc6f5e04df1128eacd8ff5c93bb119675e
Hsuan Lee <HsuanGM@gmail.com>
2018-10-31 21:21:18 +0800

    fix(module:timeline): fix loading icon
    close #2377

--- components/timeline/nz-timeline.module.ts
+++ components/timeline/nz-timeline.module.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { NzIconModule } from '../icon/nz-icon.module';
 import { NzTimelineItemComponent } from './nz-timeline-item.component';
 import { NzTimelineComponent } from './nz-timeline.component';
 
 @NgModule({
   declarations: [ NzTimelineItemComponent, NzTimelineComponent ],
   exports     : [ NzTimelineItemComponent, NzTimelineComponent ],
-  imports     : [ CommonModule ]
+  imports     : [ CommonModule, NzIconModule ]
 })
 export class NzTimelineModule {
 }
