4387e7ebfd887991684774ed7dccd9471345166c
vthinkxie <vthinkxie@users.noreply.github.com>
2018-11-20 11:16:17 +0800

    refactor(module:divider): refactor divider (#2482)
    ref #2381

--- components/collapse/nz-collapse.module.ts
+++ components/collapse/nz-collapse.module.ts
@@ -1,15 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { NzStringTemplateOutletDirective } from '../core/addon/string_template_outlet';
+import { NzAddOnModule } from '../core/addon/addon.module';
 import { NzIconModule } from '../icon/nz-icon.module';
 
 import { NzCollapsePanelComponent } from './nz-collapse-panel.component';
 import { NzCollapseComponent } from './nz-collapse.component';
 
 @NgModule({
-  declarations: [ NzCollapsePanelComponent, NzCollapseComponent, NzStringTemplateOutletDirective ],
+  declarations: [ NzCollapsePanelComponent, NzCollapseComponent ],
   exports     : [ NzCollapsePanelComponent, NzCollapseComponent ],
-  imports     : [ CommonModule, NzIconModule ]
+  imports     : [ CommonModule, NzIconModule, NzAddOnModule ]
 })
 
 export class NzCollapseModule {
