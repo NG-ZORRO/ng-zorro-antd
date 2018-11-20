9965f01897ae4cfa359f81550bb3ac969163b470
Michael Krog <mic@apaq.dk>
2018-11-20 08:06:22 +0100

    feat: Adds danish locale

---
+++ components/i18n/languages/date-picker/da_DK.ts
@@ -0,0 +1,19 @@
+import CalendarLocale from '../calendar/da_DK';
+import TimePickerLocale from '../time-picker/da_DK';
+
+// Merge into a locale object
+const locale = {
+  lang: {
+    placeholder: 'Vælg dato',
+    rangePlaceholder: ['Startdato', 'Slutdato'],
+    ...CalendarLocale,
+  },
+  timePickerLocale: {
+    ...TimePickerLocale,
+  },
+};
+
+// All settings at:
+// https://github.com/ant-design/ant-design/blob/master/components/date-picker/locale/example.json
+
+export default locale;
