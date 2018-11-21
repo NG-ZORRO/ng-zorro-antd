d432f5d2bf276b590f3d4c890823a1093f489102
vthinkxie <vthinkxie@users.noreply.github.com>
2018-11-21 23:11:31 +0800

    refactor(module:layout): refactor layout (#2500)

--- components/layout/nz-layout.module.ts
+++ components/layout/nz-layout.module.ts
@@ -1,7 +1,7 @@
+import { LayoutModule } from '@angular/cdk/layout';
+import { PlatformModule } from '@angular/cdk/platform';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-
-import { NzMatchMediaService } from '../core/services/nz-match-media.service';
 import { NzIconModule } from '../icon/nz-icon.module';
 
 import { NzContentComponent } from './nz-content.component';
@@ -13,8 +13,7 @@ import { NzSiderComponent } from './nz-sider.component';
 @NgModule({
   declarations: [ NzLayoutComponent, NzHeaderComponent, NzContentComponent, NzFooterComponent, NzSiderComponent ],
   exports     : [ NzLayoutComponent, NzHeaderComponent, NzContentComponent, NzFooterComponent, NzSiderComponent ],
-  providers   : [ NzMatchMediaService ],
-  imports     : [ CommonModule, NzIconModule ]
+  imports     : [ CommonModule, NzIconModule, LayoutModule, PlatformModule ]
 })
 export class NzLayoutModule {
 }
