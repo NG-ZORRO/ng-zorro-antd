f7f9e4fbabcfbb982439eb99d74bd4252a038c72
vthinkxie <vthinkxie@users.noreply.github.com>
2018-11-25 20:31:30 +0800

    refactor(module:spin): refactor spin (#2530)

--- components/spin/nz-spin.component.ts
+++ components/spin/nz-spin.component.ts
@@ -4,10 +4,10 @@ import {
   Component,
   ElementRef,
   Input,
-  NgZone,
   Renderer2,
   TemplateRef,
-  ViewChild
+  ViewChild,
+  ViewEncapsulation
 } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
@@ -18,64 +18,49 @@ import { toBoolean } from '../core/util/convert';
 @Component({
   selector           : 'nz-spin',
   preserveWhitespaces: false,
+  encapsulation      : ViewEncapsulation.None,
   changeDetection    : ChangeDetectionStrategy.OnPush,
   templateUrl        : './nz-spin.component.html'
 })
 export class NzSpinComponent implements AfterViewInit {
-  private _tip: string;
-  private _delay = 0;
-  el: HTMLElement = this.elementRef.nativeElement;
-
-  baseSpinning$ = new BehaviorSubject(true);
-  resultSpinning$: Observable<boolean> = this.baseSpinning$.asObservable().pipe(debounceTime(this.nzDelay));
+  spinning$ = new BehaviorSubject(true);
+  debounceSpinning$: Observable<boolean> = this.spinning$.asObservable().pipe(debounceTime(0));
   @ViewChild('containerElement') containerElement: ElementRef;
   @ViewChild('nestedElement') nestedElement: ElementRef;
   @Input() nzIndicator: TemplateRef<void>;
   @Input() nzSize = 'default';
+  @Input() nzTip: string;
 
   @Input()
   set nzDelay(value: number) {
     if (isNotNil(value)) {
-      this._delay = value;
-      this.resultSpinning$ = this.baseSpinning$.asObservable().pipe(debounceTime(this.nzDelay));
+      this.debounceSpinning$ = this.spinning$.asObservable().pipe(debounceTime(value));
     }
-  }
-
-  get nzDelay(): number {
-    return this._delay;
-  }
-
-  @Input()
-  set nzTip(value: string) {
-    this._tip = value;
-  }
-
-  get nzTip(): string {
-    return this._tip;
   }
 
   @Input()
   set nzSpinning(value: boolean) {
-    this.baseSpinning$.next(toBoolean(value));
+    this.spinning$.next(toBoolean(value));
   }
 
   checkNested(): void {
+    const el: HTMLElement = this.elementRef.nativeElement;
     const containerElement = this.containerElement.nativeElement;
     const nestedElement = this.nestedElement.nativeElement;
     /** no way to detect empty https://github.com/angular/angular/issues/12530 **/
     /** https://github.com/angular/material2/issues/11280 **/
     if (!isEmpty(containerElement)) {
       this.renderer.removeStyle(containerElement, 'display');
-      this.renderer.setStyle(this.el, 'display', 'block');
+      this.renderer.setStyle(el, 'display', 'block');
       this.renderer.addClass(nestedElement, 'ant-spin-nested-loading');
     } else {
       this.renderer.setStyle(containerElement, 'display', 'none');
-      this.renderer.removeStyle(this.el, 'display');
+      this.renderer.removeStyle(el, 'display');
       this.renderer.removeClass(nestedElement, 'ant-spin-nested-loading');
     }
   }
 
-  constructor(private elementRef: ElementRef, private renderer: Renderer2, private zone: NgZone) {
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {
   }
 
   ngAfterViewInit(): void {
