4c22c4f2fab6a6fdcff58559f154c561b56ff824
Wendell <wendzhue@gmail.com>
2018-11-30 14:29:22 +0800

    refactor(module:steps): refactor steps (#2512)
    ref #2381

--- components/steps/nz-steps.module.ts
+++ components/steps/nz-steps.module.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { NzAddOnModule } from '../core/addon/addon.module';
 import { NzIconModule } from '../icon/nz-icon.module';
+
 import { NzStepComponent } from './nz-step.component';
 import { NzStepsComponent } from './nz-steps.component';
 
 @NgModule({
-  imports     : [ CommonModule, NzIconModule ],
+  imports     : [ CommonModule, NzIconModule, NzAddOnModule ],
   exports     : [ NzStepsComponent, NzStepComponent ],
   declarations: [ NzStepsComponent, NzStepComponent ]
 })
