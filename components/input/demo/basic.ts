db5a1beeaa6dc37ad274f7559b6ba42aa411a9a6
vthinkxie <vthinkxie@users.noreply.github.com>
2018-12-07 15:41:43 +0800

    refactor(module:input-number): refactor input-number support OnPush & nzBlur & nzFocus (#2601)
    close #2340 close #1707 close #1554

--- components/input/demo/basic.ts
+++ components/input/demo/basic.ts
@@ -4,6 +4,9 @@ import { Component } from '@angular/core';
   selector: 'nz-demo-input-basic',
   template: `
     <input nz-input placeholder="Basic usage" [(ngModel)]="value">
+    <br>
+    <br>
+    <input nz-input placeholder="Basic usage" [(ngModel)]="value" [disabled]="true">
   `
 })
 export class NzDemoInputBasicComponent {
