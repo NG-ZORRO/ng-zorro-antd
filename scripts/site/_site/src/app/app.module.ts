a7bf36391a09e3ce5ca7ef9a681b50d64ae5cbbb
Wendell <wendzhue@gmail.com>
2018-12-20 10:22:25 +0800

    feat: make the doc website a pwa

--- scripts/site/_site/src/app/app.module.ts
+++ scripts/site/_site/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { environment } from '../environments/environment';
 import { DEMOComponent } from './_demo/demo.component';
 import { AppComponent } from './app.component';
 import { routes } from './app.routing.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 const icons: IconDefinition[] = [ LeftOutline, RightOutline ];
 
@@ -28,6 +29,7 @@ const icons: IconDefinition[] = [ LeftOutline, RightOutline ];
     ShareModule,
     NgZorroAntdModule,
     RouterModule.forRoot(routes, environment.production ? { preloadingStrategy: PreloadAllModules } : {})
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers   : [
     Title,
