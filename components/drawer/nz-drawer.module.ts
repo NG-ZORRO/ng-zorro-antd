1e081f0e250b1b375b51bafbb1a8b04f02b43cb8
Hsuan Lee <HsuanGM@gmail.com>
2019-02-02 11:32:02 +0800

    refactor(module:drawer): refactor drawer component (#2867)
    * refactor(module:drawer): refactor drawer component
    
    close #2615, close #2227, ref #2381
    
    * style(module:drawer): fix lint

--- components/drawer/nz-drawer.module.ts
+++ components/drawer/nz-drawer.module.ts
@@ -2,13 +2,14 @@ import { OverlayModule } from '@angular/cdk/overlay';
 import { PortalModule } from '@angular/cdk/portal';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { NzAddOnModule } from '../core/addon/addon.module';
 import { NzIconModule } from '../icon/nz-icon.module';
 
 import { NzDrawerComponent } from './nz-drawer.component';
 import { NzDrawerService } from './nz-drawer.service';
 
 @NgModule({
-  imports        : [ CommonModule, OverlayModule, PortalModule, NzIconModule ],
+  imports        : [ CommonModule, OverlayModule, PortalModule, NzIconModule, NzAddOnModule ],
   exports        : [ NzDrawerComponent ],
   declarations   : [ NzDrawerComponent ],
   entryComponents: [ NzDrawerComponent ],
