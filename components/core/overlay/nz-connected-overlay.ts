dcb29bdb32ba55476b9972dc016ebf4e5303bc1c
Hsuan Lee <hsuangm@gmail.com>
2019-02-21 11:50:05 +0800

    chore: rename `NzConnectedOverlayDirective` selector

--- components/core/overlay/nz-connected-overlay.ts
+++ components/core/overlay/nz-connected-overlay.ts
@@ -1,15 +1,13 @@
 import { CdkConnectedOverlay } from '@angular/cdk/overlay';
 import { Directive } from '@angular/core';
-import { NzOverlayConfig } from './nz-overlay-config';
 
 @Directive({
-  selector: '[cdkConnectedOverlay][nz-connected-overlay], [cdkConnectedOverlay][nzConnectedOverlay]'
+  selector: '[cdkConnectedOverlay][nzConnectedOverlay]'
 })
 export class NzConnectedOverlayDirective {
 
   constructor(private cdkConnectedOverlay: CdkConnectedOverlay) {
-    const config = new NzOverlayConfig();
-    this.cdkConnectedOverlay.backdropClass = config.backdropClass as string; // This can only be string
+    this.cdkConnectedOverlay.backdropClass = 'nz-overlay-transparent-backdrop';
   }
 
 }
--- components/core/overlay/nz-overlay-config.ts
+++
@@ -1,9 +0,0 @@
-import { OverlayConfig } from '@angular/cdk/overlay';
-
-export class NzOverlayConfig extends OverlayConfig {
-
-  constructor(config?: OverlayConfig) {
-    super(config);
-    this.backdropClass = 'nz-overlay-transparent-backdrop';
-  }
-}
