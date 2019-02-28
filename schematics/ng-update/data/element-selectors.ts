b33fe7a72d1f942037c9b888d7ce632efaf95647
Hsuan Lee <HsuanGM@gmail.com>
2019-02-28 17:57:28 +0800

    build(module:schematics): add v7 update schematics (#2994)

---
+++ schematics/ng-update/data/element-selectors.ts
@@ -0,0 +1,5 @@
+import { ElementSelectorUpgradeData, TargetVersion, VersionChanges } from '@angular/cdk/schematics';
+
+export const elementSelectors: VersionChanges<ElementSelectorUpgradeData> = {
+  [ TargetVersion.V7 ]: []
+};
