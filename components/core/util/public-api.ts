92f3158d11aeaa998d6e27259f3b38bf237a3289
vthinkxie <vthinkxie@users.noreply.github.com>
2019-03-04 16:40:26 +0800

    fix(module:fesm2015): fix fesm2015 build error (#3015)

--- components/core/util/public-api.ts
+++ components/core/util/public-api.ts
@@ -1,7 +1,6 @@
 export * from './check';
 export * from './convert';
 export * from './getMentions';
-export * from './nz-global-monitor';
 export * from './string';
 export * from './textarea-caret-position';
 export * from './throttleByAnimationFrame';
