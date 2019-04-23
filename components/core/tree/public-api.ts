ed9917a0fdce9d6b29e276660661a18274f11c18
Hsuan Lee <hsuangm@gmail.com>
2019-04-23 11:04:01 +0800

    feat(module:tree-select): add the public methods

--- components/core/tree/public-api.ts
+++ components/core/tree/public-api.ts
@@ -2,3 +2,4 @@ export * from './nz-tree-base-node';
 export * from './nz-tree-base.definitions';
 export * from './nz-tree-base.service';
 export * from './nz-tree-service.resolver';
+export * from './nz-tree-base';
