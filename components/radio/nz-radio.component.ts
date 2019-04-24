539ad0a6bb20f5fade6d2e04a6670ba76e538352
binglingshuang <binglingshuang@163.com>
2019-04-24 16:51:12 +0800

    fix(module:radio, tab): Fixed memory leak problem. (fix tslint)

--- components/radio/nz-radio.component.ts
+++ components/radio/nz-radio.component.ts
@@ -142,7 +142,7 @@ export class NzRadioComponent implements ControlValueAccessor, AfterViewInit, On
     }
   }
 
-  ngOnDestroy() {
+  ngOnDestroy(): void {
     this.focusMonitor.stopMonitoring(this.elementRef);
   }
 }
