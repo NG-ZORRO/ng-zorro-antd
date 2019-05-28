ef412ae8a80ad9118588ef661a3a8cbe6542dde4
wenqi73 <1264578441@qq.com>
2019-05-28 10:27:47 +0800

    fix: correct migration style file path

--- scripts/build/migration-styles.js
+++ scripts/build/migration-styles.js
@@ -8,5 +8,5 @@ fs.mkdirsSync(targetPath);
 fs.copySync(path.resolve(sourcePath, `style`), path.resolve(targetPath, `style`));
 fs.copySync(path.resolve(sourcePath, `ng-zorro-antd.css`), path.resolve(targetPath, `ng-zorro-antd.css`));
 fs.copySync(path.resolve(sourcePath, `ng-zorro-antd.min.css`), path.resolve(targetPath, `ng-zorro-antd.min.css`));
-fs.outputFileSync(path.resolve(targetPath, `ng-zorro-antd.less`), `@import "../style/index.less";
+fs.outputFileSync(path.resolve(targetPath, `ng-zorro-antd.less`), `@import "../style/entry.less";
 @import "../components.less";`);
