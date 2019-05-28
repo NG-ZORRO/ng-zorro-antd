ddc77a653e1b97ca8d32e10728c97e165690bd08
Hsuan Lee <hsuangm@gmail.com>
2019-05-28 10:37:57 +0800

    docs(module:button): fix demo

--- components/button/demo/disabled.ts
+++ components/button/demo/disabled.ts
@@ -12,7 +12,7 @@ import { Component } from '@angular/core';
     <button nz-button nzType="dashed">Dashed</button>
     <button nz-button nzType="dashed" disabled>Dashed(disabled)</button>
     <br />
-    <button nz-button nzType="link">link</button>
+    <button nz-button nzType="link">Link</button>
     <button nz-button nzType="link" disabled>Link(disabled)</button>
     <div style="padding: 8px 8px 0px; background: rgb(190, 200, 200);">
       <button nz-button nzGhost>Ghost</button>
