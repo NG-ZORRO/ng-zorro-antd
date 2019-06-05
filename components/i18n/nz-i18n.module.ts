19ad7fd0c0af7ec7f163c87cd8002eb80931869c
Wenqi <1264578441@qq.com>
2019-06-05 19:08:29 +0800

    fix(module:date-picker): replace DatePipe with formatDate function (#3500)
    close #3487

--- components/i18n/nz-i18n.module.ts
+++ components/i18n/nz-i18n.module.ts
@@ -6,17 +6,13 @@
  * found in the LICENSE file at https://github.com/NG-ZORRO/ng-zorro-antd/blob/master/LICENSE
  */
 
-import { DatePipe } from '@angular/common';
 import { NgModule } from '@angular/core';
-
 import { LoggerModule } from 'ng-zorro-antd/core';
-
 import { NzI18nPipe } from './nz-i18n.pipe';
 
 @NgModule({
   imports: [LoggerModule],
   declarations: [NzI18nPipe],
-  exports: [NzI18nPipe],
-  providers: [DatePipe]
+  exports: [NzI18nPipe]
 })
 export class NzI18nModule {}
