a53a43a9ffac498d2f8aa7e46e4a814c7107b335
Wendell <wendzhue@gmail.com>
2019-06-05 16:23:29 +0800

    feat(module:carousel): support custom strategies (#3501)
    * feat(module:cascader): support custom strategies
    
    * docs: add docs
    
    * test: add test
    
    * docs: fix typo
    
    * fix: fix test and tsconfig

--- components/carousel/public-api.ts
+++ components/carousel/public-api.ts
@@ -10,3 +10,4 @@ export * from './nz-carousel.module';
 export * from './nz-carousel.component';
 export * from './nz-carousel-content.directive';
 export * from './nz-carousel-definitions';
+export * from './strategies/base-strategy';
