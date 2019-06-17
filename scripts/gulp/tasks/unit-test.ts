d05ef812d07dd9fc20d011c34eb60e042997b222
Hsuan Lee <hsuangm@gmail.com>
2019-06-17 18:22:52 +0800

    build: fix `test:watch` task

--- scripts/gulp/tasks/unit-test.ts
+++ scripts/gulp/tasks/unit-test.ts
@@ -3,11 +3,15 @@ import { execTask } from '../util/task-helpers';
 
 task('test:watch', (done) => {
   const argv = require('minimist')(process.argv.slice(2));
+  let tags = '';
+  if (argv.tags && typeof argv.tags === 'string') {
+    tags = argv.tags;
+  }
   return execTask(
     'ng',
     ['test', '--watch=true', '--code-coverage'],
     {
-      'NG_TEST_TAGS': argv.tags || ''
+      'NG_TEST_TAGS': tags
     }
   )(done);
 });
