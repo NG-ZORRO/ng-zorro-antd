7585ba48b7445777333b16c191031e7d494d87bd
vthinkxie <vthinkxie@users.noreply.github.com>
2019-06-26 17:57:15 +0800

    fix(module:card): fix card tab ng-template (#3654)

--- components/progress/demo/segment.ts
+++ components/progress/demo/segment.ts
@@ -3,9 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'nz-demo-progress-segment',
   template: `
-    <nz-tooltip nzTitle="3 done / 3 in progress / 4 to do">
-      <nz-progress nz-tooltip [nzPercent]="60" [nzSuccessPercent]="30"></nz-progress>
-    </nz-tooltip>
+    <nz-progress
+      nz-tooltip
+      nzTitle="3 done / 3 in progress / 4 to do"
+      [nzPercent]="60"
+      [nzSuccessPercent]="30"
+    ></nz-progress>
   `
 })
 export class NzDemoProgressSegmentComponent {}
--- components/steps/demo/customized-progress-dot.ts
+++ components/steps/demo/customized-progress-dot.ts
@@ -10,11 +10,9 @@ import { Component } from '@angular/core';
       <nz-step nzTitle="Waiting" nzDescription="You can hover on the dot."></nz-step>
     </nz-steps>
     <ng-template #progressTemplate let-dot let-status="status" let-index="index">
-      <nz-popover nzContent="steps {{ index }} status: {{ status }}">
-        <span nz-popover style="margin-left: -100%;">
-          <ng-template [ngTemplateOutlet]="dot"></ng-template>
-        </span>
-      </nz-popover>
+      <span nz-popover nzContent="steps {{ index }} status: {{ status }}" style="margin-left: -100%;">
+        <ng-template [ngTemplateOutlet]="dot"></ng-template>
+      </span>
     </ng-template>
   `
 })
