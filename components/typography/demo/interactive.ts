c2c28a44b8a50a1b0c3cdb6a41ab674063380249
Hsuan Lee <HsuanGM@gmail.com>
2019-06-29 13:35:13 +0800

    fix(module:typography): fix the actions button order (#3677)

--- components/typography/demo/interactive.ts
+++ components/typography/demo/interactive.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'nz-demo-typography-interactive',
   template: `
-    <p nz-paragraph nzEditable [(nzContent)]="str"></p>
-    <p nz-paragraph nzCopyable nzContent="This is a copyable text."></p>
+    <p nz-paragraph nzEditable [(nzContent)]="editStr"></p>
+    <p nz-paragraph nzCopyable nzEditable [(nzContent)]="copyStr"></p>
     <p nz-paragraph nzCopyable nzCopyText="Hello, Ant Design!">Replace copy text.</p>
   `
 })
 export class NzDemoTypographyInteractiveComponent {
-  str = 'This is an editable text.';
+  editStr = 'This is an editable text.';
+  copyStr = 'This is a copyable text.';
 }
