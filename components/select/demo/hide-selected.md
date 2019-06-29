9d33198e0650893cb3de652d4fb60b4488b1285d
Chunxiang Liu <danranvm@gmail.com>
2019-06-29 16:58:53 +0800

    feat(module:select): modify demo and docs

--- components/select/demo/hide-selected.md
+++ components/select/demo/hide-selected.md
@@ -7,8 +7,8 @@ title:
 
 ## zh-CN
 
-隐藏下拉列表中已选择的选项。
+通过 `nzHideSelectedOptions` 隐藏下拉列表中已选择的选项。
 
 ## en-US
 
-Hide already selected options in the dropdown.
+use `nzHideSelectedOptions` to hide already selected options in the dropdown.
--- components/select/demo/hide-selected.ts
+++ components/select/demo/hide-selected.ts
@@ -7,19 +7,16 @@ import { Component } from '@angular/core';
       style="width: 100%"
       nzMode="multiple"
       nzPlaceHolder="Inserted are removed"
+      [nzHideSelectedOptions]="true"
       [(ngModel)]="listOfSelectedValue"
     >
       <ng-container *ngFor="let option of listOfOption">
-        <nz-option [nzLabel]="option" [nzValue]="option" *ngIf="isNotSelected(option)"></nz-option>
+        <nz-option [nzLabel]="option" [nzValue]="option"></nz-option>
       </ng-container>
     </nz-select>
   `
 })
 export class NzDemoSelectHideSelectedComponent {
   listOfOption = ['Apples', 'Nails', 'Bananas', 'Helicopters'];
-  listOfSelectedValue: string[] = [];
-
-  isNotSelected(value: string): boolean {
-    return this.listOfSelectedValue.indexOf(value) === -1;
-  }
+  listOfSelectedValue: string[] = ['Apples', 'Nails'];
 }
