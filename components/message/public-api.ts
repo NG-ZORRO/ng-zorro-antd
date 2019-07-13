173067c5e44ef243bb6a655d6c10232f73bfee6f
vthinkxie <vthinkxie@users.noreply.github.com>
2019-07-14 00:17:44 +0800

    fix(module:all): fix singleton service (#3789)

--- components/message/public-api.ts
+++ components/message/public-api.ts
@@ -7,7 +7,6 @@
  */
 
 export * from './nz-message.service';
-export * from './nz-message.service.module';
 export * from './nz-message.module';
 export * from './nz-message.component';
 export * from './nz-message.definitions';
