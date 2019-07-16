03069f2af32cab392ca479a6d45df30e7cc86c20
vthinkxie <yadong.xyd@alibaba-inc.com>
2019-07-16 11:46:31 +0800

    fix(module:table): fix table scrollbar bug close #3796

--- components/core/services/public-api.ts
+++ components/core/services/public-api.ts
@@ -6,8 +6,6 @@
  * found in the LICENSE file at https://github.com/NG-ZORRO/ng-zorro-antd/blob/master/LICENSE
  */
 
-export * from './nz-measure-scrollbar.service';
-export * from './nz-measure-scrollbar.service.module';
 export * from './update-host-class.service';
 export * from './nz-copy-to-clipboard.service';
 export * from './nz-copy-to-clipboard.service.module';
