483785e9c32156ed8f0ee5dc306b617aaea31008
Nearzxh <270761322@qq.com>
2019-07-26 17:14:24 +0800

    fix(module:*): import PlatformModule when use platform in component (#3823)

--- components/descriptions/nz-descriptions.module.ts
+++ components/descriptions/nz-descriptions.module.ts
@@ -5,17 +5,16 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/NG-ZORRO/ng-zorro-antd/blob/master/LICENSE
  */
-
+import { PlatformModule } from '@angular/cdk/platform';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-
 import { NzAddOnModule } from 'ng-zorro-antd/core';
 
 import { NzDescriptionsItemComponent } from './nz-descriptions-item.component';
 import { NzDescriptionsComponent } from './nz-descriptions.component';
 
 @NgModule({
-  imports: [CommonModule, NzAddOnModule],
+  imports: [CommonModule, NzAddOnModule, PlatformModule],
   declarations: [NzDescriptionsComponent, NzDescriptionsItemComponent],
   exports: [NzDescriptionsComponent, NzDescriptionsItemComponent]
 })
--- components/timeline/nz-timeline.module.ts
+++ components/timeline/nz-timeline.module.ts
@@ -5,10 +5,9 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/NG-ZORRO/ng-zorro-antd/blob/master/LICENSE
  */
-
+import { PlatformModule } from '@angular/cdk/platform';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-
 import { NzAddOnModule } from 'ng-zorro-antd/core';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 
@@ -18,6 +17,6 @@ import { NzTimelineComponent } from './nz-timeline.component';
 @NgModule({
   declarations: [NzTimelineItemComponent, NzTimelineComponent],
   exports: [NzTimelineItemComponent, NzTimelineComponent],
-  imports: [CommonModule, NzIconModule, NzAddOnModule]
+  imports: [CommonModule, PlatformModule, NzIconModule, NzAddOnModule]
 })
 export class NzTimelineModule {}
