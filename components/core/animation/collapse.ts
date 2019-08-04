80bcd6c7e6c6689172a2a842eaa414ae92206bec
mifan <605655316@qq.com>
2019-08-04 22:47:03 +0800

    fix: set overflow: hidden before the collapse animation
    set overflow: hidden before the collapse animation, otherwise the content will overflow.

--- components/core/animation/collapse.ts
+++ components/core/animation/collapse.ts
@@ -13,8 +13,8 @@ export const collapseMotion: AnimationTriggerMetadata = trigger('collapseMotion'
   state('expanded', style({ height: '*' })),
   state('collapsed', style({ height: 0, overflow: 'hidden' })),
   state('hidden', style({ height: 0, overflow: 'hidden', borderTopWidth: '0' })),
-  transition('expanded => collapsed', animate(`150ms ${AnimationCurves.EASE_IN_OUT}`)),
-  transition('expanded => hidden', animate(`150ms ${AnimationCurves.EASE_IN_OUT}`)),
-  transition('collapsed => expanded', animate(`150ms ${AnimationCurves.EASE_IN_OUT}`)),
-  transition('hidden => expanded', animate(`150ms ${AnimationCurves.EASE_IN_OUT}`))
+  transition('expanded => collapsed', [style({ overflow: 'hidden' }), animate(`150ms ${AnimationCurves.EASE_IN_OUT}`)]),
+  transition('expanded => hidden', [style({ overflow: 'hidden' }), animate(`150ms ${AnimationCurves.EASE_IN_OUT}`)]),
+  transition('collapsed => expanded', [style({ overflow: 'hidden' }), animate(`150ms ${AnimationCurves.EASE_IN_OUT}`)]),
+  transition('hidden => expanded', [style({ overflow: 'hidden' }), animate(`150ms ${AnimationCurves.EASE_IN_OUT}`)])
 ]);
