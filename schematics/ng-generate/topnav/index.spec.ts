7f656f9305140ce8dc1cae5755cc2b46aecbdc96
Hsuan Lee <hsuangm@gmail.com>
2019-08-12 16:54:58 +0800

    test(schematics): update test

--- schematics/ng-generate/topnav/index.spec.ts
+++ schematics/ng-generate/topnav/index.spec.ts
@@ -10,7 +10,7 @@ describe('top-nav schematic', () => {
 
   beforeEach(async () => {
     runner = new SchematicTestRunner('schematics', require.resolve('../../collection.json'));
-    appTree = await createTestApp(runner);
+    appTree = await createTestApp(runner, {name: 'ng-zorro-top-nav-top-nav'});
   });
 
   it('should create top-nav files', async () => {
@@ -18,15 +18,15 @@ describe('top-nav schematic', () => {
     const files = tree.files;
     expect(files).toEqual(
       jasmine.arrayContaining([
-        '/projects/ng-zorro/src/app/app.component.html',
-        '/projects/ng-zorro/src/app/app.component.css',
-        '/projects/ng-zorro/src/app/app.component.ts',
-        '/projects/ng-zorro/src/app/app-routing.module.ts',
-        '/projects/ng-zorro/src/app/pages/welcome/welcome-routing.module.ts',
-        '/projects/ng-zorro/src/app/pages/welcome/welcome.module.ts',
-        '/projects/ng-zorro/src/app/pages/welcome/welcome.component.ts',
-        '/projects/ng-zorro/src/app/pages/welcome/welcome.component.css',
-        '/projects/ng-zorro/src/app/pages/welcome/welcome.component.html'
+        '/projects/ng-zorro-top-nav/src/app/app.component.html',
+        '/projects/ng-zorro-top-nav/src/app/app.component.css',
+        '/projects/ng-zorro-top-nav/src/app/app.component.ts',
+        '/projects/ng-zorro-top-nav/src/app/app-routing.module.ts',
+        '/projects/ng-zorro-top-nav/src/app/pages/welcome/welcome-routing.module.ts',
+        '/projects/ng-zorro-top-nav/src/app/pages/welcome/welcome.module.ts',
+        '/projects/ng-zorro-top-nav/src/app/pages/welcome/welcome.component.ts',
+        '/projects/ng-zorro-top-nav/src/app/pages/welcome/welcome.component.css',
+        '/projects/ng-zorro-top-nav/src/app/pages/welcome/welcome.component.html'
       ])
     );
   });
@@ -34,24 +34,24 @@ describe('top-nav schematic', () => {
   it('should set the style preprocessor correctly', async () => {
     const tree = await runner.runSchematicAsync('topnav', {style: Style.Less}, appTree).toPromise();
     const files = tree.files;
-    const appContent = getFileContent(tree, '/projects/ng-zorro/src/app/app.component.ts');
-    const welcomeContent = getFileContent(tree, '/projects/ng-zorro/src/app/pages/welcome/welcome.component.ts');
+    const appContent = getFileContent(tree, '/projects/ng-zorro-top-nav/src/app/app.component.ts');
+    const welcomeContent = getFileContent(tree, '/projects/ng-zorro-top-nav/src/app/pages/welcome/welcome.component.ts');
 
     expect(appContent).toContain('app.component.less');
     expect(welcomeContent).toContain('welcome.component.less');
 
     expect(files).toEqual(
       jasmine.arrayContaining([
-        '/projects/ng-zorro/src/app/app.component.less',
-        '/projects/ng-zorro/src/app/pages/welcome/welcome.component.less'
+        '/projects/ng-zorro-top-nav/src/app/app.component.less',
+        '/projects/ng-zorro-top-nav/src/app/pages/welcome/welcome.component.less'
       ])
     );
   });
 
   it('should set the prefix correctly', async () => {
     const tree = await runner.runSchematicAsync('topnav', {prefix: 'nz'}, appTree).toPromise();
-    const appContent = getFileContent(tree, '/projects/ng-zorro/src/app/app.component.ts');
-    const welcomeContent = getFileContent(tree, '/projects/ng-zorro/src/app/pages/welcome/welcome.component.ts');
+    const appContent = getFileContent(tree, '/projects/ng-zorro-top-nav/src/app/app.component.ts');
+    const welcomeContent = getFileContent(tree, '/projects/ng-zorro-top-nav/src/app/pages/welcome/welcome.component.ts');
 
     expect(appContent).toContain(`selector: 'nz-root'`);
     expect(welcomeContent).toContain(`selector: 'nz-welcome'`);
