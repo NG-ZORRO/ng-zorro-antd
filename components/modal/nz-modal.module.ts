25601a766d177af5d96c5f4bed07eb9f74cc423f
Wendell <wendzhue@gmail.com>
2019-08-28 10:06:13 +0800

    refactor(module:core): move pipes to a new module to share them

--- components/modal/nz-modal.module.ts
+++ components/modal/nz-modal.module.ts
@@ -11,11 +11,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { NzButtonModule } from 'ng-zorro-antd/button';
-import { NzAddOnModule, NzNoAnimationModule } from 'ng-zorro-antd/core';
+import { NzAddOnModule, NzNoAnimationModule, NzPipesModule } from 'ng-zorro-antd/core';
 import { NzI18nModule } from 'ng-zorro-antd/i18n';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 
-import { CssUnitPipe } from './css-unit.pipe';
 import { NzModalControlServiceModule } from './nz-modal-control.service.module';
 import { NzModalFooterDirective } from './nz-modal-footer.directive';
 import { NzModalComponent } from './nz-modal.component';
@@ -29,12 +28,13 @@ import { NzModalServiceModule } from './nz-modal.service.module';
     NzI18nModule,
     NzButtonModule,
     NzIconModule,
+    NzPipesModule,
     NzNoAnimationModule,
     NzModalServiceModule,
     NzModalControlServiceModule
   ],
   exports: [NzModalComponent, NzModalFooterDirective],
-  declarations: [NzModalComponent, NzModalFooterDirective, CssUnitPipe],
+  declarations: [NzModalComponent, NzModalFooterDirective],
   entryComponents: [NzModalComponent]
 })
 export class NzModalModule {}
