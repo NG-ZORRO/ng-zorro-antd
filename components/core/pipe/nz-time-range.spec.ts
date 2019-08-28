eee327be4806e96bd942eef391e43128d21ed2b1
Wendell <wendzhue@gmail.com>
2019-08-28 10:06:13 +0800

    refactor(module:core): move pipes to a new module to share them

--- components/core/pipe/nz-time-range.spec.ts
+++ components/core/pipe/nz-time-range.spec.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { NzStatisticModule } from './nz-statistic.module';
+import { NzStatisticModule } from '../../statistic/nz-statistic.module';
 
 @Component({
   template: `
