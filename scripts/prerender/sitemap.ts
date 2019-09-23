c81053334849a26ab2b67a928b68827402c10e3e
Hsuan Lee <HsuanGM@gmail.com>
2019-09-23 11:44:53 +0800

    docs: fix sitemap hreflang (#4199)

--- scripts/prerender/sitemap.ts
+++ scripts/prerender/sitemap.ts
@@ -5,22 +5,27 @@ import { ROUTES } from './static.paths';
 
 import { buildConfig } from '../build-config';
 
-const urls = Array.from(new Set(ROUTES.filter(r => r !== '/').map(r => r.replace(/\/(zh|en)$/, '')))).map(
-  (r: string) => {
-    return {
-      url: `${r}/en`,
-      changefreq: 'weekly',
-      priority: r.includes('docs') ? 0.5 : 0.8,
-      links: [{ lang: 'en', url: `${r}/en` }, { lang: 'zh', url: `${r}/zh` }]
-    };
-  }
-// tslint:disable-next-line:no-any
-) as any;
-
+function generateUrls(lang: 'zh' | 'en'): sitemap.ISitemapItemOptionsLoose[] {
+  const urls = Array.from(new Set(ROUTES.filter(r => r !== '/').map(r => r.replace(/\/(zh|en)$/, ''))));
+  return urls.map(
+    (r: string) => {
+      return {
+        url: `${r}/${lang}`,
+        changefreq: sitemap.EnumChangefreq.WEEKLY,
+        priority: r.includes('docs') ? 0.5 : 0.8,
+        links: [{ lang: 'en', url: `${r}/en` }, { lang: 'zh', url: `${r}/zh` }]
+      };
+    }
+  );
+}
 const sitemapInstance = sitemap.createSitemap({
   hostname: 'https://ng.ant.design',
   cacheTime: 600000,
-  urls: [{ url: '/', changefreq: 'weekly', priority: 0.5, lastmodrealtime: true }, ...urls]
+  urls: [
+    { url: '/', changefreq: sitemap.EnumChangefreq.WEEKLY, priority: 0.5, lastmodrealtime: true },
+    ...generateUrls('en'),
+    ...generateUrls('zh')
+  ]
 });
 
-writeFileSync(join(buildConfig.outputDir, 'sitemap.xml'), sitemapInstance.toString());
+writeFileSync(join(buildConfig.outputDir, 'sitemap.xml'), sitemapInstance.toString(true));
