533dda8e0000b0029f1f726a0bdea2d347d9e337
Hsuan Lee <hsuangm@gmail.com>
2019-09-26 12:02:54 +0800

    docs: use clean plugin for dark theme

--- scripts/site/generate-theme.js
+++ scripts/site/generate-theme.js
@@ -1,15 +1,16 @@
 const less = require('less');
+const LessPluginCleanCSS = require('less-plugin-clean-css');
 const path = require('path');
 const fs = require('fs-extra');
 const darkTheme = require('@ant-design/dark-theme');
 
 const themePath = path.join(__dirname, '../../site/doc/styles.less');
 
-const lightContent = `
-html.light {
-  @import '${themePath}';
-}
-`;
+// const lightContent = `
+// html.light {
+//   @import '${themePath}';
+// }
+// `;
 
 const darkContent = `
 @import '${themePath}';
@@ -17,18 +18,20 @@ const darkContent = `
 
 
 
-less.render(lightContent, {
-  javascriptEnabled: true
-}).then(data => {
-  fs.writeFileSync(path.join(__dirname, '../../site/doc/light.css'), data.css);
-}).catch(e => {
-  console.log(e);
-});
+// less.render(lightContent, {
+//   javascriptEnabled: true,
+//   plugins: [new LessPluginCleanCSS({ advanced: true })]
+// }).then(data => {
+//   fs.writeFileSync(path.join(__dirname, '../../site/doc/light.css'), data.css);
+// }).catch(e => {
+//   console.log(e);
+// });
 less.render(darkContent, {
   javascriptEnabled: true,
+  plugins: [new LessPluginCleanCSS({ advanced: true })],
   modifyVars: darkTheme.default
 }).then(data => {
-  fs.writeFileSync(path.join(__dirname, '../../site/doc/dark.css'), data.css);
+  fs.writeFileSync(path.join(__dirname, '../../site/doc/assets/dark.css'), data.css);
 }).catch(e => {
   console.log(e);
 });
