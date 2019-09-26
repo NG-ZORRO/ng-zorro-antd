2a10e372b2491734fc35451ad38807edd2054059
Hsuan Lee <hsuangm@gmail.com>
2019-09-26 12:03:12 +0800

    docs: use clean plugin for dark theme

--- scripts/site/generate-theme.js
+++ scripts/site/generate-theme.js
@@ -3,29 +3,12 @@ const LessPluginCleanCSS = require('less-plugin-clean-css');
 const path = require('path');
 const fs = require('fs-extra');
 const darkTheme = require('@ant-design/dark-theme');
-
 const themePath = path.join(__dirname, '../../site/doc/styles.less');
-
-// const lightContent = `
-// html.light {
-//   @import '${themePath}';
-// }
-// `;
 
 const darkContent = `
 @import '${themePath}';
 `;
 
-
-
-// less.render(lightContent, {
-//   javascriptEnabled: true,
-//   plugins: [new LessPluginCleanCSS({ advanced: true })]
-// }).then(data => {
-//   fs.writeFileSync(path.join(__dirname, '../../site/doc/light.css'), data.css);
-// }).catch(e => {
-//   console.log(e);
-// });
 less.render(darkContent, {
   javascriptEnabled: true,
   plugins: [new LessPluginCleanCSS({ advanced: true })],
