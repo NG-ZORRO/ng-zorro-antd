cde1ea94b5d76362c21633a9af9d732d8d1d1007
Wendell <wendzhue@gmail.com>
2019-11-06 09:57:40 +0800

    fix: typo

--- components/tooltip/base/nz-tooltip-base.directive.ts
+++ components/tooltip/base/nz-tooltip-base.directive.ts
@@ -320,7 +320,7 @@ export abstract class NzTooltipBaseDirective implements OnChanges, OnInit, OnDes
 
   // tslint:disable-next-line no-any
   private updateComponentValue(key: string, value: any): void {
-    if (typeof value !== undefined) {
+    if (typeof value !== 'undefined') {
       // @ts-ignore
       this.tooltip[key] = value;
     }
