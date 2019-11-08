e94cfaeebd63bb57c33630dbd654ae094507fc97
Hsuan Lee <HsuanGM@gmail.com>
2019-11-08 17:31:53 +0800

    fix(module:auto-complete): default value not selected (#4366)
    * fix(module:auto-complete): default value not selected
    
    close #4362
    
    * docs(module:auto-complete): remove OnPush

---
+++ components/core/types/compare-with.ts
@@ -0,0 +1,10 @@
+/**
+ * @license
+ * Copyright Alibaba.com All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/NG-ZORRO/ng-zorro-antd/blob/master/LICENSE
+ */
+
+// tslint:disable-next-line:no-any
+export type CompareWith = (o1: any, o2: any) => boolean;
