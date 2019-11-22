14b6b160f3110646f846f9ffef3bf69e7b4aee6a
Hsuan Lee <hsuangm@gmail.com>
2019-11-22 19:29:49 +0800

    feat(schematics): add v9 migration rules for icon and calendar

--- schematics/utils/ng-update/elements.ts
+++ schematics/utils/ng-update/elements.ts
@@ -1,5 +1,9 @@
 import { parseFragment, DefaultTreeDocument, DefaultTreeElement } from 'parse5';
 
+const hasClassName = (node: DefaultTreeElement, className: string) => {
+  return Array.isArray(node.attrs) && node.attrs.find(attr => attr.name === 'class' && attr.value.indexOf(className) !== -1)
+};
+
 export function findElementWithTag(html: string, tagName: string): number[] {
   const document = parseFragment(html, {sourceCodeLocationInfo: true}) as DefaultTreeDocument;
   const elements: DefaultTreeElement[] = [];
@@ -19,4 +23,25 @@ export function findElementWithTag(html: string, tagName: string): number[] {
   visitNodes(document.childNodes);
 
   return elements.map(element => element.sourceCodeLocation.startTag.startOffset)
+}
+
+export function findElementWithClassName(html: string, className: string, tagName: string): number[] {
+  const document = parseFragment(html, {sourceCodeLocationInfo: true}) as DefaultTreeDocument;
+  const elements: DefaultTreeElement[] = [];
+
+  const visitNodes = nodes => {
+    nodes.forEach(node => {
+      if (node.childNodes) {
+        visitNodes(node.childNodes);
+      }
+
+      if (hasClassName(node, className) && node.tagName && node.tagName.toLowerCase() === tagName.toLowerCase()) {
+        elements.push(node);
+      }
+    });
+  };
+
+  visitNodes(document.childNodes);
+
+  return elements.map(element => element.sourceCodeLocation.attrs.class.startOffset)
 }
