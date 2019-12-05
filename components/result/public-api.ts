80d576ac446defbf7d82d21918e8e9162a6190c1
Hsuan Lee <hsuangm@gmail.com>
2019-12-05 19:34:40 +0800

    chore: fix typo

--- components/result/public-api.ts
+++ components/result/public-api.ts
@@ -10,7 +10,7 @@ export * from './nz-result.module';
 export * from './nz-result.component';
 export * from './nz-result-cells';
 
-// Making these partial components not visible to users but comprehensive to ng-packagr.
+// Making these partial components not visible to users but comprehensible to ng-packagr.
 export { NzResultNotFoundComponent as ɵNzResultNotFoundComponent } from './partial/not-found';
 export { NzResultServerErrorComponent as ɵNzResultServerErrorComponent } from './partial/server-error.component';
 export { NzResultUnauthorizedComponent as ɵNzResultUnauthorizedComponent } from './partial/unauthorized';
