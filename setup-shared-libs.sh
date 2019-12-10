1c4eb61649afec9ef01391aff1c2be366fb0b46f
vthinkxie <yadong.xyd@alibaba-inc.com>
2019-12-10 17:13:44 +0800

    ci: fix coverage

--- setup-shared-libs.sh
+++ setup-shared-libs.sh
@@ -1,13 +1,11 @@
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TARGET="$ROOT/integration/$1"
-echo $TARGET
 
 mkdir -p "$TARGET/ng-zorro-antd"
 rsync -av --exclude=".*" "$ROOT/publish/" "$TARGET/ng-zorro-antd" > /dev/null 2>&1
 
 for lib in "@angular/animations" "@angular/cdk" "@angular/common" "@angular/compiler" "@angular/core" "@angular/forms" "@angular/platform-browser" "@angular/platform-browser-dynamic" "@angular/router" "@angular/cli" "@angular/compiler-cli" "rxjs" "typescript" "zone.js" "date-fns" "core-js"
 do
-  echo "$TARGET/$lib"
   mkdir -p "$TARGET/$lib"
   rsync -av --exclude=".*" "$ROOT/node_modules/$lib/" "$TARGET/$lib" > /dev/null 2>&1
   rm -rf "$TARGET/$lib/node_modules"
