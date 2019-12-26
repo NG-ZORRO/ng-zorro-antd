57ca5b850cf4543f76aa6843b2156984def73ab7
Wendell <wendellhu95@gmail.com>
2019-12-26 20:09:54 +0800

    test: fix tests

--- components/progress/nz-progress.definitions.ts
+++ components/progress/nz-progress.definitions.ts
@@ -30,7 +30,7 @@ export type NzProgressColorGradient = { direction?: string } & (NzProgressGradie
 
 export type NzProgressStrokeColorType = string | NzProgressColorGradient;
 
-export type NzProgressFormatter = (percent: number) => string | TemplateRef<{ $implicit: number }>;
+export type NzProgressFormatter = ((percent: number) => string) | TemplateRef<{ $implicit: number }>;
 
 export interface NzProgressCirclePath {
   stroke: string | null;
