04bad6c028fd90ed804b321b62ee188865641caa
Saeed Rahimi <saeedr.com@gmail.com>
2020-02-02 08:03:10 +0330

    feat(module:layout): added rtl direction

--- components/layout/layout.component.ts
+++ components/layout/layout.component.ts
@@ -3,7 +3,18 @@
  * found in the LICENSE file at https://github.com/NG-ZORRO/ng-zorro-antd/blob/master/LICENSE
  */
 
-import { ChangeDetectionStrategy, Component, ContentChildren, QueryList, ViewEncapsulation } from '@angular/core';
+/**
+ * @license
+ * Copyright Alibaba.com All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/NG-ZORRO/ng-zorro-antd/blob/master/LICENSE
+ */
+
+import { Direction, Directionality } from '@angular/cdk/bidi';
+import { ChangeDetectionStrategy, Component, ContentChildren, OnDestroy, QueryList, ViewEncapsulation } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { NzSiderComponent } from './sider.component';
 
 @Component({
@@ -15,9 +26,25 @@ import { NzSiderComponent } from './sider.component';
   template: ` <ng-content></ng-content> `,
   host: {
     '[class.ant-layout-has-sider]': 'listOfNzSiderComponent.length > 0',
-    '[class.ant-layout]': 'true'
+    '[class.ant-layout]': 'true',
+    '[class.ant-layout-rtl]': `dir === 'rtl'`
   }
 })
 export class NzLayoutComponent {
   @ContentChildren(NzSiderComponent) listOfNzSiderComponent!: QueryList<NzSiderComponent>;
+
+  dir: Direction;
+  private destroy$ = new Subject<void>();
+
+  constructor(directionality: Directionality) {
+    this.dir = directionality.value;
+    directionality.change.pipe(takeUntil(this.destroy$)).subscribe(() => {
+      this.dir = directionality.value;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
