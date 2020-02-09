da8821a329e9cf4719087dfef01995a8cea71255
Yadong Xie <yadong.xyd@alibaba-inc.com>
2020-02-09 19:59:23 +0800

    perf(module:checkbox): use css empty selector instead of observeContent (#4761)

--- components/checkbox/checkbox.module.ts
+++ components/checkbox/checkbox.module.ts
@@ -6,17 +6,16 @@
  * found in the LICENSE file at https://github.com/NG-ZORRO/ng-zorro-antd/blob/master/LICENSE
  */
 
-import { ObserversModule } from '@angular/cdk/observers';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { NzCheckboxGroupComponent } from './nz-checkbox-group.component';
-import { NzCheckboxWrapperComponent } from './nz-checkbox-wrapper.component';
-import { NzCheckboxComponent } from './nz-checkbox.component';
+import { NzCheckboxGroupComponent } from './checkbox-group.component';
+import { NzCheckboxWrapperComponent } from './checkbox-wrapper.component';
+import { NzCheckboxComponent } from './checkbox.component';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, ObserversModule],
+  imports: [CommonModule, FormsModule],
   declarations: [NzCheckboxComponent, NzCheckboxGroupComponent, NzCheckboxWrapperComponent],
   exports: [NzCheckboxComponent, NzCheckboxGroupComponent, NzCheckboxWrapperComponent]
 })
