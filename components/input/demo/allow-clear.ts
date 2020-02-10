1cc53dcb70b6a1ea2945056db3e6f328e18ad7b7
vthinkxie <yadong.xyd@alibaba-inc.com>
2020-02-10 15:31:07 +0800

    feat(module:input): support textarea with clear icon close #4623

--- components/input/demo/allow-clear.ts
+++ components/input/demo/allow-clear.ts
@@ -3,14 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'nz-demo-input-allow-clear',
   template: `
-    <nz-input-group [nzSuffix]="suffixTemplate">
+    <nz-input-group [nzSuffix]="inputClearTpl">
       <input type="text" nz-input [(ngModel)]="inputValue" placeholder="input with clear icon" />
     </nz-input-group>
-    <ng-template #suffixTemplate
+    <ng-template #inputClearTpl
       ><i nz-icon class="ant-input-clear-icon" nzTheme="fill" nzType="close-circle" *ngIf="inputValue" (click)="inputValue = null"></i
+    ></ng-template>
+    <br />
+    <br />
+    <nz-input-group [nzSuffix]="textAreaClearTpl">
+      <textarea nz-input [(ngModel)]="textValue" placeholder="textarea with clear icon"></textarea>
+    </nz-input-group>
+    <ng-template #textAreaClearTpl
+      ><i
+        nz-icon
+        class="ant-input-textarea-clear-icon"
+        nzTheme="fill"
+        nzType="close-circle"
+        *ngIf="textValue"
+        (click)="textValue = null"
+      ></i
     ></ng-template>
   `
 })
 export class NzDemoInputAllowClearComponent {
   inputValue: string | null;
+  textValue: string | null;
 }
