30a0cdebe3372a1086b1192efa7d0eb869f3dc7a
Hsuan Lee <hsuangm@gmail.com>
2020-02-27 16:08:36 +0800

    docs: change selectors

--- components/typography/demo/ellipsis.ts
+++ components/typography/demo/ellipsis.ts
@@ -3,21 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'nz-demo-typography-ellipsis',
   template: `
-    <p nz-paragraph nzEllipsis>
+    <p nz-typography nzEllipsis>
       Ant Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background
       applications, is refined by Ant UED Team. Ant Design, a design language for background applications, is refined by Ant UED Team. Ant
       Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background
       applications, is refined by Ant UED Team. Ant Design, a design language for background applications, is refined by Ant UED Team.
     </p>
     <br />
-    <p nz-paragraph nzEllipsis nzExpandable [nzEllipsisRows]="3">
+    <p nz-typography nzEllipsis nzExpandable [nzEllipsisRows]="3">
       Ant Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background
       applications, is refined by Ant UED Team. Ant Design, a design language for background applications, is refined by Ant UED Team. Ant
       Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background
       applications, is refined by Ant UED Team. Ant Design, a design language for background applications, is refined by Ant UED Team.
     </p>
     <br />
-    <p nz-paragraph nzEllipsis nzEditable [nzEllipsisRows]="2" [nzContent]="dynamicContent" (nzContentChange)="onChange($event)"></p>
+    <p nz-typography nzEllipsis nzEditable [nzEllipsisRows]="2" [nzContent]="dynamicContent" (nzContentChange)="onChange($event)"></p>
   `
 })
 export class NzDemoTypographyEllipsisComponent {
