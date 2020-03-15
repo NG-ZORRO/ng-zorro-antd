89d5db0bf9df9a0d0407352a64e8165702613f71
Hsuan Lee <hsuangm@gmail.com>
2020-03-15 15:26:37 +0800

    docs(module:list): update DEMO

--- components/list/demo/vertical.ts
+++ components/list/demo/vertical.ts
@@ -33,29 +33,6 @@ interface ItemData {
         </nz-list-item-extra>
       </nz-list-item>
     </nz-list>
-    <!--    <nz-list [nzDataSource]="data" [nzItemLayout]="'vertical'" [nzRenderItem]="item" [nzPagination]="pagination" [nzFooter]="footer">-->
-    <!--      <ng-template #item let-item>-->
-    <!--        <nz-list-item [nzContent]="item.content" [nzActions]="[starAction, likeAction, msgAction]" [nzExtra]="extra">-->
-    <!--          <ng-template #starAction><i nz-icon nzType="star-o" style="margin-right: 8px;"></i> 156</ng-template>-->
-    <!--          <ng-template #likeAction><i nz-icon nzType="like-o" style="margin-right: 8px;"></i> 156</ng-template>-->
-    <!--          <ng-template #msgAction><i nz-icon nzType="message" style="margin-right: 8px;"></i> 2</ng-template>-->
-    <!--          <nz-list-item-meta [nzAvatar]="item.avatar" [nzTitle]="nzTitle" [nzDescription]="item.description">-->
-    <!--            <ng-template #nzTitle-->
-    <!--              ><a href="{{ item.href }}">{{ item.title }}</a></ng-template-->
-    <!--            >-->
-    <!--          </nz-list-item-meta>-->
-    <!--          <ng-template #extra>-->
-    <!--            <img width="272" alt="logo" src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png" />-->
-    <!--          </ng-template>-->
-    <!--        </nz-list-item>-->
-    <!--      </ng-template>-->
-    <!--      <ng-template #footer>-->
-    <!--        <div><b>ant design</b> footer part</div>-->
-    <!--      </ng-template>-->
-    <!--      <ng-template #pagination>-->
-    <!--        <nz-pagination [nzPageIndex]="1" [nzTotal]="50" (nzPageIndexChange)="loadData($event)"></nz-pagination>-->
-    <!--      </ng-template>-->
-    <!--    </nz-list>-->
   `
 })
 export class NzDemoListVerticalComponent implements OnInit {
