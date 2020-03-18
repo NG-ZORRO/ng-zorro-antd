e72472d90d62e218e307a12d0d68a62071ebbbd2
wenqi73 <1264578441@qq.com>
2020-03-19 00:45:01 +0800

    test: fix tests

--- components/empty/public-api.ts
+++ components/empty/public-api.ts
@@ -10,3 +10,5 @@ export * from './embed-empty.component';
 export * from './empty.component';
 export * from './empty.module';
 export * from './config';
+export { NzEmptySimpleComponent } from './partial/simple';
+export { NzEmptyDefaultComponent } from './partial/default';
