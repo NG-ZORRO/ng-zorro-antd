e47cce591c4e3a722791283c8b0b25ba5dd0e2de
Wendell Hu <wendellhu95@gmail.com>
2020-03-23 19:10:00 +0800

    chore: add module

--- components/typography/typography.module.ts
+++ components/typography/typography.module.ts
@@ -5,6 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/NG-ZORRO/ng-zorro-antd/blob/master/LICENSE
  */
+import { ClipboardModule } from '@angular/cdk/clipboard';
 import { PlatformModule } from '@angular/cdk/platform';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
@@ -20,7 +21,7 @@ import { NzTextEditComponent } from './text-edit.component';
 import { NzTypographyComponent } from './typography.component';
 
 @NgModule({
-  imports: [CommonModule, NzIconModule, NzToolTipModule, NzInputModule, NzI18nModule, NzTransButtonModule],
+  imports: [CommonModule, NzIconModule, NzToolTipModule, NzInputModule, NzI18nModule, NzTransButtonModule, ClipboardModule],
   exports: [NzTypographyComponent, NzTextCopyComponent, NzTextEditComponent, PlatformModule],
   declarations: [NzTypographyComponent, NzTextCopyComponent, NzTextEditComponent]
 })
