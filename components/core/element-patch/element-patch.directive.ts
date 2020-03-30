1b1fc4032b1460772ade9589136361a2efa159f6
Wendell Hu <wendellhu95@gmail.com>
2020-03-30 22:11:22 +0800

    chore: simplify element patch directive

--- components/core/element-patch/element-patch.directive.ts
+++ components/core/element-patch/element-patch.directive.ts
@@ -6,30 +6,20 @@
  * found in the LICENSE file at https://github.com/NG-ZORRO/ng-zorro-antd/blob/master/LICENSE
  */
 
-import { Directive, ElementRef, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 /**
  * A patch directive to select the element of a component.
- *
- * @ref https://stackoverflow.com/questions/42183472/access-dom-element-with-template-reference-variables-on-component
  */
 @Directive({
   selector: '[nz-element]',
   exportAs: 'nzElement'
 })
-export class NzElementPatchDirective implements OnInit {
-  @Output() readonly elementChange = new EventEmitter<HTMLElement | undefined>();
-
-  constructor(public elementRef: ElementRef) {
-    this.elementChange.next(undefined);
-  }
+export class NzElementPatchDirective {
+  constructor(public elementRef: ElementRef) {}
 
   @Input()
   get element(): HTMLElement | undefined {
     return this.elementRef.nativeElement;
-  }
-
-  ngOnInit(): void {
-    this.elementChange.next(this.elementRef.nativeElement);
   }
 }
