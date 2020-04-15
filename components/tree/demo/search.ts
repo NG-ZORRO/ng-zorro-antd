3fdeca6f0e06ea82832ccae102fc6544d04f2eec
Hsuan Lee <HsuanGM@gmail.com>
2020-04-15 18:50:30 +0800

    chore: add the core entry port compatibility (#5027)

--- components/tree/demo/search.ts
+++ components/tree/demo/search.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NzFormatEmitEvent } from 'ng-zorro-antd/core/tree';
+import { NzFormatEmitEvent } from 'ng-zorro-antd/tree';
 
 @Component({
   selector: 'nz-demo-tree-search',
