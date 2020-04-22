3cefa56f9a3a82b505bc8d32762ccf87cd0ed7a2
Hsuan Lee <hsuangm@gmail.com>
2020-04-22 11:04:05 +0800

    fix(module:modal): `NoopAnimations` do not work with modal mask
    close #5093

--- components/modal/modal-config.ts
+++ components/modal/modal-config.ts
@@ -13,3 +13,19 @@ export interface NzModalConfig {
   nzMaskClosable?: boolean;
 }
 export const NZ_MODAL_CONFIG = new InjectionToken<NzModalConfig>('NZ_MODAL_CONFIG');
+
+export const ZOOM_CLASS_NAME_MAP = {
+  enter: 'zoom-enter',
+  enterActive: 'zoom-enter-active',
+  leave: 'zoom-leave',
+  leaveActive: 'zoom-leave-active'
+};
+
+export const FADE_CLASS_NAME_MAP = {
+  enter: 'fade-enter',
+  enterActive: 'fade-enter-active',
+  leave: 'fade-leave',
+  leaveActive: 'fade-leave-active'
+};
+
+export const MODAL_MASK_CLASS_NAME = 'ant-modal-mask';
