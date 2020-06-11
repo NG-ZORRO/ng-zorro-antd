ee9c9efe15aadbfc903cf66bf342a5da2970336e
Hsuan Lee <HsuanGM@gmail.com>
2020-06-11 11:34:23 +0800

    feat(module:typography): support `nzOnEllipsis` output (#5297)

--- components/typography/demo/ellipsis.ts
+++ components/typography/demo/ellipsis.ts
@@ -3,11 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'nz-demo-typography-ellipsis',
   template: `
-    <p nz-typography nzEllipsis>
+    <p nz-typography nzEllipsis nzEllipsisRows="3">
       Ant Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background
       applications, is refined by Ant UED Team. Ant Design, a design language for background applications, is refined by Ant UED Team. Ant
       Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background
-      applications, is refined by Ant UED Team. Ant Design, a design language for background applications, is refined by Ant UED Team.
+      applications, is refined by Ant UED Team. Ant Design
     </p>
     <br />
     <p nz-typography nzEllipsis nzExpandable [nzEllipsisRows]="3">
@@ -21,9 +21,11 @@ import { Component } from '@angular/core';
   `
 })
 export class NzDemoTypographyEllipsisComponent {
-  dynamicContent = `Ant Design, a design language for background applications, is refined by Ant UED Team.
-Ant Design, a design language for background applications, is refined by Ant UED Team.
-Ant Design, a design language for background applications, is refined by Ant UED Team.`;
+  dynamicContent =
+    'Ant Design, a design language for background applications, is refined by Ant UED Team. ' +
+    'Ant Design, a design language for background applications, is refined by Ant UED Team. ' +
+    'Ant Design, a design language for background applications, is refined by Ant UED Team. ' +
+    'Ant Design, a design language for background applications, is refined by Ant UED Team.';
 
   onChange(event: string): void {
     this.dynamicContent = event;
