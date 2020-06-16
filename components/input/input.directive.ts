8270009eacd79f944daf64b89951725427a80244
nuonuoge <932273325@qq.com>
2020-06-16 10:24:51 +0800

    fix(module:input): support reactive form disabled (#5316)

--- components/input/input.directive.ts
+++ components/input/input.directive.ts
@@ -3,9 +3,9 @@
  * found in the LICENSE file at https://github.com/NG-ZORRO/ng-zorro-antd/blob/master/LICENSE
  */
 
-import { Directive, ElementRef, Input, OnChanges, Renderer2, SimpleChanges } from '@angular/core';
+import { Directive, ElementRef, Input, OnChanges, Optional, Renderer2, Self, SimpleChanges } from '@angular/core';
+import { NgControl } from '@angular/forms';
 import { BooleanInput, NzSizeLDSType } from 'ng-zorro-antd/core/types';
-import { InputBoolean } from 'ng-zorro-antd/core/util';
 import { Subject } from 'rxjs';
 
 @Directive({
@@ -22,10 +22,20 @@ export class NzInputDirective implements OnChanges {
   static ngAcceptInputType_disabled: BooleanInput;
 
   @Input() nzSize: NzSizeLDSType = 'default';
-  @Input() @InputBoolean() disabled = false;
+  @Input()
+  get disabled(): boolean {
+    if (this.ngControl && this.ngControl.disabled !== null) {
+      return this.ngControl.disabled;
+    }
+    return this._disabled;
+  }
+  set disabled(value: boolean) {
+    this._disabled = value != null && `${value}` !== 'false';
+  }
+  _disabled = false;
   disabled$ = new Subject<boolean>();
 
-  constructor(renderer: Renderer2, elementRef: ElementRef) {
+  constructor(@Optional() @Self() public ngControl: NgControl, renderer: Renderer2, elementRef: ElementRef) {
     renderer.addClass(elementRef.nativeElement, 'ant-input');
   }
   ngOnChanges(changes: SimpleChanges): void {
