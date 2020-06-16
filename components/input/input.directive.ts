6d403e3902f5d580bf243affb3ba8ae96fb0ea9c
Yadong Xie <yadong.xyd@alibaba-inc.com>
2020-06-16 11:26:29 +0800

    fix(module:input): fix reactive form disabled input-group style (#5428)
    close #5137

--- components/input/input.directive.ts
+++ components/input/input.directive.ts
@@ -3,10 +3,11 @@
  * found in the LICENSE file at https://github.com/NG-ZORRO/ng-zorro-antd/blob/master/LICENSE
  */
 
-import { Directive, ElementRef, Input, OnChanges, Optional, Renderer2, Self, SimpleChanges } from '@angular/core';
+import { Directive, ElementRef, Input, OnChanges, OnDestroy, OnInit, Optional, Renderer2, Self, SimpleChanges } from '@angular/core';
 import { NgControl } from '@angular/forms';
 import { BooleanInput, NzSizeLDSType } from 'ng-zorro-antd/core/types';
 import { Subject } from 'rxjs';
+import { filter, takeUntil } from 'rxjs/operators';
 
 @Directive({
   selector: 'input[nz-input],textarea[nz-input]',
@@ -18,7 +19,7 @@ import { Subject } from 'rxjs';
     '[attr.disabled]': 'disabled || null'
   }
 })
-export class NzInputDirective implements OnChanges {
+export class NzInputDirective implements OnChanges, OnInit, OnDestroy {
   static ngAcceptInputType_disabled: BooleanInput;
 
   @Input() nzSize: NzSizeLDSType = 'default';
@@ -34,14 +35,34 @@ export class NzInputDirective implements OnChanges {
   }
   _disabled = false;
   disabled$ = new Subject<boolean>();
+  private destroy$ = new Subject<void>();
 
   constructor(@Optional() @Self() public ngControl: NgControl, renderer: Renderer2, elementRef: ElementRef) {
     renderer.addClass(elementRef.nativeElement, 'ant-input');
   }
+
+  ngOnInit(): void {
+    if (this.ngControl) {
+      this.ngControl.statusChanges
+        ?.pipe(
+          filter(() => this.ngControl.disabled !== null),
+          takeUntil(this.destroy$)
+        )
+        .subscribe(() => {
+          this.disabled$.next(this.ngControl.disabled!);
+        });
+    }
+  }
+
   ngOnChanges(changes: SimpleChanges): void {
     const { disabled } = changes;
     if (disabled) {
       this.disabled$.next(this.disabled);
     }
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
