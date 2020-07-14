1a408ee3e24d437385e18208e945a6ca4bf48b00
vthinkxie <yadong.xyd@alibaba-inc.com>
2020-07-14 15:22:08 +0800

    feat(module:collapse): support nzGhost property

---
+++ components/collapse/demo/ghost.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nz-demo-collapse-ghost',
+  template: `
+    <nz-collapse nzGhost>
+      <nz-collapse-panel *ngFor="let panel of panels" [nzHeader]="panel.name" [nzActive]="panel.active" [nzDisabled]="panel.disabled">
+        <p style="margin:0;">
+          A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome guest in many
+          households across the world.
+        </p>
+      </nz-collapse-panel>
+    </nz-collapse>
+  `
+})
+export class NzDemoCollapseGhostComponent {
+  panels = [
+    {
+      active: true,
+      name: 'This is panel header 1',
+      disabled: false
+    },
+    {
+      active: false,
+      disabled: false,
+      name: 'This is panel header 2'
+    },
+    {
+      active: false,
+      disabled: true,
+      name: 'This is panel header 3'
+    }
+  ];
+}
