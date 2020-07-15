ee861731cca2bd5854d98e15fb3f762b408ae575
wendellhu <wendellhu95@gmail.com>
2020-07-15 21:01:20 +0800

    chore(module:core): enhance nzElementRef
    close #5566

--- components/core/element-patch/element-patch.directive.ts
+++ components/core/element-patch/element-patch.directive.ts
@@ -5,6 +5,8 @@
 
 import { Directive, ElementRef } from '@angular/core';
 
+import { NzSafeAny } from '../types';
+
 /**
  * A patch directive to select the element of a component.
  */
@@ -13,5 +15,9 @@ import { Directive, ElementRef } from '@angular/core';
   exportAs: 'nzElement'
 })
 export class NzElementPatchDirective {
+  get nativeElement(): NzSafeAny {
+    return this.elementRef.nativeElement;
+  }
+
   constructor(public elementRef: ElementRef) {}
 }
