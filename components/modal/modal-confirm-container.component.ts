9805620935632bc080273bd78495a62ed53e6181
rppig42 <48581046+rppig42@users.noreply.github.com>
2020-07-22 20:41:56 +0800

    fix(module:modal): change back to FocusTrapFactory (#5596)
    Co-authored-by: rppig42 <rppig42@gmail.com>
    close #5591

--- components/modal/modal-confirm-container.component.ts
+++ components/modal/modal-confirm-container.component.ts
@@ -3,7 +3,7 @@
  * found in the LICENSE file at https://github.com/NG-ZORRO/ng-zorro-antd/blob/master/LICENSE
  */
 
-import { ConfigurableFocusTrapFactory } from '@angular/cdk/a11y';
+import { FocusTrapFactory } from '@angular/cdk/a11y';
 import { OverlayRef } from '@angular/cdk/overlay';
 import { CdkPortalOutlet } from '@angular/cdk/portal';
 import { DOCUMENT } from '@angular/common';
@@ -114,7 +114,7 @@ export class NzModalConfirmContainerComponent extends BaseModalContainerComponen
   constructor(
     private i18n: NzI18nService,
     elementRef: ElementRef,
-    focusTrapFactory: ConfigurableFocusTrapFactory,
+    focusTrapFactory: FocusTrapFactory,
     cdr: ChangeDetectorRef,
     render: Renderer2,
     overlayRef: OverlayRef,
