aa2ea54eba8e4f68c23240ca88f84682f7cf6552
Junhan Yang <50307523+yangjunhan@users.noreply.github.com>
2020-09-10 15:40:45 +0800

    feat(module:skeleton): add nzRound prop and skeleton-element image (#5710)

--- components/skeleton/demo/element.ts
+++ components/skeleton/demo/element.ts
@@ -10,82 +10,76 @@ import {
 @Component({
   selector: 'nz-demo-skeleton-element',
   template: `
+    <nz-space nzSize="middle">
+      <nz-space-item>
+        <nz-skeleton-element
+          nzType="button"
+          [nzActive]="elementActive"
+          [nzSize]="elementSize"
+          [nzShape]="buttonShape"
+        ></nz-skeleton-element>
+      </nz-space-item>
+      <nz-space-item>
+        <nz-skeleton-element
+          nzType="avatar"
+          [nzActive]="elementActive"
+          [nzSize]="elementSize"
+          [nzShape]="avatarShape"
+        ></nz-skeleton-element>
+      </nz-space-item>
+      <nz-space-item>
+        <nz-skeleton-element nzType="input" [nzActive]="elementActive" [nzSize]="elementSize" style="width:200px"></nz-skeleton-element>
+      </nz-space-item>
+    </nz-space>
+    <br />
+    <br />
+    <nz-skeleton-element nzType="image" [nzActive]="elementActive"></nz-skeleton-element>
+    <nz-divider></nz-divider>
     <div nz-row nzAlign="middle" [nzGutter]="8">
-      <div nz-col nzSpan="5">
-        ButtonActive:
-        <nz-switch [(ngModel)]="buttonActive"></nz-switch>
-      </div>
-      <div nz-col nzSpan="9">
-        ButtonSize:
-        <nz-radio-group [(ngModel)]="buttonSize">
+      <div nz-col nzSpan="10">
+        Size:
+        <nz-radio-group [(ngModel)]="elementSize">
           <label nz-radio-button nzValue="default">Default</label>
           <label nz-radio-button nzValue="large">Large</label>
           <label nz-radio-button nzValue="small">Small</label>
         </nz-radio-group>
       </div>
-      <div nz-col nzSpan="9">
-        ButtonShape:
+      <div nz-col nzSpan="5">
+        Active:
+        <nz-switch [(ngModel)]="elementActive"></nz-switch>
+      </div>
+    </div>
+    <br />
+    <br />
+    <div nz-row nzAlign="middle" [nzGutter]="8">
+      <div nz-col nzSpan="10">
+        Button Shape:
         <nz-radio-group [(ngModel)]="buttonShape">
           <label nz-radio-button nzValue="default">Default</label>
           <label nz-radio-button nzValue="circle">Circle</label>
           <label nz-radio-button nzValue="round">Round</label>
         </nz-radio-group>
       </div>
-    </div>
-    <br />
-    <nz-skeleton-element nzType="button" [nzActive]="buttonActive" [nzSize]="buttonSize" [nzShape]="buttonShape"></nz-skeleton-element>
-    <br />
-    <br />
-    <div nz-row nzAlign="middle" [nzGutter]="8">
-      <div nz-col nzSpan="5">
-        AvatarActive:
-        <nz-switch [(ngModel)]="avatarActive"></nz-switch>
-      </div>
-      <div nz-col nzSpan="9">
-        AvatarSize:
-        <nz-radio-group [(ngModel)]="avatarSize">
-          <label nz-radio-button nzValue="default">Default</label>
-          <label nz-radio-button nzValue="large">Large</label>
-          <label nz-radio-button nzValue="small">Small</label>
-        </nz-radio-group>
-      </div>
-      <div nz-col nzSpan="9">
-        AvatarShape:
+      <div nz-col nzSpan="10">
+        Avatar Shape:
         <nz-radio-group [(ngModel)]="avatarShape">
           <label nz-radio-button nzValue="circle">Circle</label>
           <label nz-radio-button nzValue="square">Square</label>
         </nz-radio-group>
       </div>
     </div>
-    <br />
-    <nz-skeleton-element nzType="avatar" [nzActive]="avatarActive" [nzSize]="avatarSize" [nzShape]="avatarShape"></nz-skeleton-element>
-    <br />
-    <br />
-    <div nz-row nzAlign="middle" [nzGutter]="8">
-      <div nz-col nzSpan="5">
-        InputActive:
-        <nz-switch [(ngModel)]="inputActive"></nz-switch>
-      </div>
-      <div nz-col nzSpan="9">
-        InputSize:
-        <nz-radio-group [(ngModel)]="inputSize">
-          <label nz-radio-button nzValue="default">Default</label>
-          <label nz-radio-button nzValue="large">Large</label>
-          <label nz-radio-button nzValue="small">Small</label>
-        </nz-radio-group>
-      </div>
-    </div>
-    <br />
-    <nz-skeleton-element nzType="input" [nzActive]="inputActive" [nzSize]="inputSize" style="width:300px"></nz-skeleton-element>
   `
 })
 export class NzDemoSkeletonElementComponent {
   buttonActive = false;
   avatarActive = false;
   inputActive = false;
+  imageActive = false;
   buttonSize: NzSkeletonButtonSize = 'default';
   avatarSize: NzSkeletonAvatarSize = 'default';
   inputSize: NzSkeletonInputSize = 'default';
+  elementActive = false;
   buttonShape: NzSkeletonButtonShape = 'default';
   avatarShape: NzSkeletonAvatarShape = 'circle';
+  elementSize: NzSkeletonInputSize = 'default';
 }
