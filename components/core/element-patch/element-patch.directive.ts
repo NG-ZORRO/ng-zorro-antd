7adf199030906eaf4d819d9d9985a9d0fe605bf2
Wendell Hu <wendellhu95@gmail.com>
2020-09-14 14:32:56 +0800

    chore(module:core): enhance nzElementRef (#5567)
    * chore(module:core): enhance nzElementRef
    
    close #5566

--- components/core/element-patch/element-patch.directive.ts
+++ components/core/element-patch/element-patch.directive.ts
@@ -4,14 +4,19 @@
  */
 
 import { Directive, ElementRef } from '@angular/core';
+import { NzSafeAny } from 'ng-zorro-antd/core/types';
 
 /**
  * A patch directive to select the element of a component.
  */
 @Directive({
-  selector: '[nz-element]',
+  selector: '[nzElement], [nz-element]',
   exportAs: 'nzElement'
 })
 export class NzElementPatchDirective {
+  get nativeElement(): NzSafeAny {
+    return this.elementRef.nativeElement;
+  }
+
   constructor(public elementRef: ElementRef) {}
 }
