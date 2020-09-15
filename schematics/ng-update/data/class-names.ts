a48fe6a160d40831495775ad5e6fbe56e18f3b71
Hsuan Lee <HsuanGM@gmail.com>
2020-09-15 12:59:21 +0800

    feat(schematics): add v10 migration for upload component

--- schematics/ng-update/data/class-names.ts
+++ schematics/ng-update/data/class-names.ts
@@ -1,5 +1,18 @@
 import { ClassNameUpgradeData, TargetVersion, VersionChanges } from '@angular/cdk/schematics';
 
 export const classNames: VersionChanges<ClassNameUpgradeData> = {
-  [ TargetVersion.V7 ]: [ ]
+  [TargetVersion.V10]: [
+    {
+      pr: 'https://github.com/NG-ZORRO/ng-zorro-antd/pull/5774',
+      changes: [
+        {replace: 'UploadType', replaceWith: 'NzUploadType'},
+        {replace: 'UploadListType', replaceWith: 'NzUploadListType'},
+        {replace: 'UploadFile', replaceWith: 'NzUploadFile'},
+        {replace: 'UploadChangeParam', replaceWith: 'NzUploadChangeParam'},
+        {replace: 'ShowUploadListInterface', replaceWith: 'NzShowUploadList'},
+        {replace: 'UploadTransformFileType', replaceWith: 'NzUploadTransformFileType'},
+        {replace: 'UploadXHRArgs', replaceWith: 'NzUploadXHRArgs'}
+      ]
+    }
+  ]
 };
