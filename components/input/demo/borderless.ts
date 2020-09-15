bb885f73dfffa014a558e3e209d3039ec3faf48d
vthinkxie <yadong.xyd@alibaba-inc.com>
2020-09-15 16:49:09 +0800

    feat(module:input): support borderless

---
+++ components/input/demo/borderless.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nz-demo-input-borderless',
+  template: `
+    <input nz-input placeholder="borderless" nzBorderless />
+  `
+})
+export class NzDemoInputBorderlessComponent {}
