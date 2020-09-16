53156b2f9f13d0baaaaf52531205cc419cf7ac0b
Hsuan Lee <HsuanGM@gmail.com>
2020-09-16 14:29:08 +0800

    refactor(module:form,grid): remove deprecated APIs for v10
    BREAKING CHANGES:
    
    - `nz-form-item[nzFlex]` has been removed, Please manually remove this input.
    - `nz-form-item[nzType]` has been removed, Please manually remove this input.
    - `nz-row[nzType]` has been removed, Please manually remove this input.

--- schematics/ng-update/data/property-names.ts
+++ schematics/ng-update/data/property-names.ts
@@ -4,7 +4,7 @@ import { PropertyNameUpgradeData, TargetVersion, VersionChanges } from '@angular
 export const propertyNames: VersionChanges<PropertyNameUpgradeData> = {
   [TargetVersion.V10]: [
     {
-      pr: '',
+      pr: 'https://github.com/NG-ZORRO/ng-zorro-antd/pull/5779',
       changes: [
         {
           replace: 'nzPosition',
