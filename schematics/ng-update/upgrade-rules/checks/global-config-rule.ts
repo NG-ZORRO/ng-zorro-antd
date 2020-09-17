39e0a735069991fc5eadfabafcb7b9c977155c0a
Hsuan Lee <HsuanGM@gmail.com>
2020-09-17 11:19:13 +0800

    refactor(module:code-editor): remove deprecated APIs for v10
    BREAKING CHANGES:
    
    - `NzCodeEditorService.updateDefaultOption` has been removed, use `NzConfigService.set` instead.
    - Inject token `NZ_CODE_EDITOR_CONFIG`  has been removed, use `NZ_CONFIG` instead.

--- schematics/ng-update/upgrade-rules/checks/global-config-rule.ts
+++ schematics/ng-update/upgrade-rules/checks/global-config-rule.ts
@@ -2,9 +2,17 @@ import { TargetVersion } from '@angular/cdk/schematics';
 import { InjectionTokenRule } from './injection-token-rule';
 
 export class GlobalConfigRule extends InjectionTokenRule {
-  enabled = this.targetVersion === TargetVersion.V9;
-  tokens = ['NZ_MESSAGE_CONFIG', 'NZ_NOTIFICATION_CONFIG', 'NZ_DEFAULT_EMPTY_CONTENT', 'NZ_ICON_DEFAULT_TWOTONE_COLOR'];
+  enabled = this.targetVersion === TargetVersion.V9 || this.targetVersion === TargetVersion.V10;
+  tokens =
+    (
+      this.targetVersion === TargetVersion.V9 &&
+      ['NZ_MESSAGE_CONFIG', 'NZ_NOTIFICATION_CONFIG', 'NZ_DEFAULT_EMPTY_CONTENT', 'NZ_ICON_DEFAULT_TWOTONE_COLOR', 'NZ_CODE_EDITOR_CONFIG']
+    ) ||
+    (
+      this.targetVersion === TargetVersion.V10 &&
+      ['NZ_CODE_EDITOR_CONFIG']
+    )
   getFailure(token: string): string {
-    return `Found deprecated symbol "${token}" which has been removed. Use global config to instead please.`;
+    return `Found deprecated symbol "${token}" which has been removed. Please use 'NzConfigService' instead.`;
   }
 }
