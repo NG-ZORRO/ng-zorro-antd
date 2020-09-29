f35481b2627f340c2a33cfb49166c16b384a85a7
Hsuan Lee <hsuangm@gmail.com>
2020-09-29 11:57:21 +0800

    build: fix site build

--- scripts/gulp/tasks/universal.ts
+++ scripts/gulp/tasks/universal.ts
@@ -4,6 +4,7 @@ import { execNodeTask } from '../util/task-helpers';
 
 /** Run prerender script on the output directory, to render static html. */
 task(':run:prerender', execNodeTask('@angular/cli', 'ng', ['run', 'ng-zorro-antd-doc:prerender']));
+task(':run:service-work', execNodeTask('@angular/cli', 'ng', ['run', 'ng-zorro-antd-doc:service-work']));
 
 /** Run sitemap script on the output directory, to create sitemap.xml */
 task(':run:sitemap', done => {
@@ -12,7 +13,7 @@ task(':run:sitemap', done => {
 });
 
 /** Parallel run sitemap and prerender scripts */
-task('run:prerender', series(':run:sitemap', ':run:prerender'));
+task('run:prerender', series(':run:prerender', ':run:service-work', ':run:sitemap'));
 
 /** Task that builds the universal-app and runs the prerender and sitemap script. */
 task('prerender', series('run:prerender'));
