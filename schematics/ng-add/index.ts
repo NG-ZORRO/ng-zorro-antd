d1e76f3d8d41b0e3b9f01887e4805ff43f48d0fa
maroon1 <sage2257@outlook.com>
2020-12-17 10:42:55 +0800

    feat(schematics): make ng-add schematics chainable (#6203)

--- schematics/ng-add/index.ts
+++ schematics/ng-add/index.ts
@@ -1,6 +1,5 @@
-import { WorkspaceDefinition } from '@angular-devkit/core/src/workspace';
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
-import { NodePackageInstallTask, RunSchematicTask } from '@angular-devkit/schematics/tasks';
+import { chain, Rule, schematic, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 import { getProjectFromWorkspace } from '@angular/cdk/schematics';
 import { getWorkspace } from '@schematics/angular/utility/workspace';
 import { addPackageToPackageJson } from '../utils/package-config';
@@ -8,25 +7,31 @@ import { getProjectStyle } from '../utils/project-style';
 import { hammerjsVersion, zorroVersion } from '../utils/version-names';
 import { Schema } from './schema';
 
-export default function(options: Schema): Rule {
-  return async (host: Tree, context: SchematicContext) => {
-    if (!options.skipPackageJson) {
-      addPackageToPackageJson(host, 'ng-zorro-antd', zorroVersion);
-      if (options.gestures) {
-        addPackageToPackageJson(host, 'hammerjs', hammerjsVersion);
+export default function (options: Schema): Rule {
+  return chain([
+    (host: Tree) => {
+      if (!options.skipPackageJson) {
+        addPackageToPackageJson(host, 'ng-zorro-antd', zorroVersion);
+        if (options.gestures) {
+          addPackageToPackageJson(host, 'hammerjs', hammerjsVersion);
+        }
       }
+    },
+    schematic('ng-add-setup-project', options),
+    async (host: Tree) => {
+      if (options.template) {
+        const workspace = await getWorkspace(host);
+        const project = getProjectFromWorkspace(workspace, options.project);
+        const style = getProjectStyle(project);
+
+        return schematic(options.template, {...options, style: style});
+      }
+    },
+    (_: Tree, context: SchematicContext) => {
+      if (options.skipInstall) {
+        return;
+      }
+      context.addTask(new NodePackageInstallTask());
     }
-
-    const installTaskId = context.addTask(new NodePackageInstallTask());
-
-    context.addTask(new RunSchematicTask('ng-add-setup-project', options), [installTaskId]);
-
-    if (options.template) {
-      const workspace = await getWorkspace(host);
-      const project = getProjectFromWorkspace(workspace as unknown as WorkspaceDefinition, options.project);
-      const style = getProjectStyle(project);
-      context.addTask(new RunSchematicTask(options.template, {...options, style: style}));
-    }
-
-  };
+  ]);
 }
