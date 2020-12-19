a5822156beeaa633ad58aa27d2d9019abf9af79a
vthinkxie <yadong.xyd@alibaba-inc.com>
2020-12-19 23:13:02 +0800

    build: bump versons

--- scripts/gulp/tasks/default.ts
+++ scripts/gulp/tasks/default.ts
@@ -1,13 +1,13 @@
-import chalk from 'chalk';
+import { yellow } from 'chalk';
 import { parallel, task } from 'gulp';
 
 task('help', done => {
   console.log();
   console.log('Please specify a gulp task you want to run.');
-  console.log(chalk.yellow('start:dev    '), 'Start development.');
-  console.log(chalk.yellow('build:library'), 'Build ng-zorro-antd-lib to publish/ directory.');
-  console.log(chalk.yellow('build:preview'), 'Build preview site to dist/ directory.');
-  console.log(chalk.yellow('build:release'), 'Build releaseable library to publish/ directory and deployable site to dist/ directory.');
+  console.log(yellow('start:dev    '), 'Start development.');
+  console.log(yellow('build:library'), 'Build ng-zorro-antd-lib to publish/ directory.');
+  console.log(yellow('build:preview'), 'Build preview site to dist/ directory.');
+  console.log(yellow('build:release'), 'Build releaseable library to publish/ directory and deployable site to dist/ directory.');
   console.log();
   done();
 });
