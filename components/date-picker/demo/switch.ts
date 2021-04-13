c2660d0924d23d7137fef5d4caae10bcab38767a
stygian-desolator <73412177+stygian-desolator@users.noreply.github.com>
2021-04-13 17:16:08 +0800

    docs(module:image,date-picker,skeleton): remove 'nz-space-item' (#6597)

--- components/date-picker/demo/switch.ts
+++ components/date-picker/demo/switch.ts
@@ -4,17 +4,13 @@ import { Component } from '@angular/core';
   selector: 'nz-demo-date-picker-switch',
   template: `
     <nz-space>
-      <nz-space-item>
-        <nz-select [(ngModel)]="mode">
-          <nz-option nzValue="date" nzLabel="Date"></nz-option>
-          <nz-option nzValue="week" nzLabel="Week"></nz-option>
-          <nz-option nzValue="month" nzLabel="Month"></nz-option>
-          <nz-option nzValue="year" nzLabel="Year"></nz-option>
-        </nz-select>
-      </nz-space-item>
-      <nz-space-item>
-        <nz-date-picker [nzMode]="mode"></nz-date-picker>
-      </nz-space-item>
+      <nz-select *nzSpaceItem [(ngModel)]="mode">
+        <nz-option nzValue="date" nzLabel="Date"></nz-option>
+        <nz-option nzValue="week" nzLabel="Week"></nz-option>
+        <nz-option nzValue="month" nzLabel="Month"></nz-option>
+        <nz-option nzValue="year" nzLabel="Year"></nz-option>
+      </nz-select>
+      <nz-date-picker *nzSpaceItem [nzMode]="mode"></nz-date-picker>
     </nz-space>
   `
 })
