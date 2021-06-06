5b8f8f642d93ff9d2dd8ce8e84b0a12f5583590c
stygian-desolator <73412177+stygian-desolator@users.noreply.github.com>
2021-06-06 11:16:13 +0800

    docs(module:cascader): use `nzModalContent` instead

--- components/cascader/demo/modal.ts
+++ components/cascader/demo/modal.ts
@@ -47,7 +47,7 @@ const options = [
   selector: 'nz-demo-cascader-modal',
   template: `
     <nz-modal [(nzVisible)]="isVisible" nzTitle="Please select" (nzOnCancel)="handleCancel($event)" (nzOnOk)="handleOk($event)">
-      <nz-cascader [nzOptions]="nzOptions" [(ngModel)]="values" (ngModelChange)="onChanges($event)"> </nz-cascader>
+      <nz-cascader *nzModalContent [nzOptions]="nzOptions" [(ngModel)]="values" (ngModelChange)="onChanges($event)"></nz-cascader>
     </nz-modal>
 
     <button nz-button (click)="open()">Open Dialog</button>
