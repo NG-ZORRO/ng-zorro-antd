1fec1e03b7361734206bfcc159497f524ece8b39
ywager <17868062+ywager@users.noreply.github.com>
2022-01-13 19:21:29 +0800

    docs(module:icon): support upload image to search icon (#5170) (#6931)
    * docs(module:icon): support upload image to search icon (#5170)
    
    Same as [1], on the icon page, support to quickly find the icons
    that users may find through upload or paste icon.
    
    [1] https://ant.design/components/icon-cn
    
    * docs(module:icon): fix upload error
    
    docs(module:icon): fix upload error
    
    docs(module:icon): fix undefined variable
    
    Co-authored-by: simplejason <simplejason.coder@gmail.com>

--- components/icon/page/index.ts
+++ components/icon/page/index.ts
@@ -4,13 +4,27 @@
  */
 
 import { DOCUMENT } from '@angular/common';
-import { Component, Inject, OnInit } from '@angular/core';
+import {
+  Component,
+  Inject,
+  OnDestroy,
+  OnInit,
+  ComponentFactoryResolver,
+  Input,
+  ViewChild,
+  TemplateRef,
+  ViewContainerRef
+} from '@angular/core';
+import { of, Subscription } from 'rxjs';
 
 import { manifest, ThemeType } from '@ant-design/icons-angular';
 import { AccountBookFill } from '@ant-design/icons-angular/icons';
 
+import { PREFIX } from 'ng-zorro-antd/core/logger';
 import { NzSafeAny } from 'ng-zorro-antd/core/types';
 import { NzIconService } from 'ng-zorro-antd/icon';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { NzUploadChangeParam, NzUploadFile, NzUploadXHRArgs } from 'ng-zorro-antd/upload';
 
 export interface Categories {
   direction: string[];
@@ -19,6 +33,27 @@ export interface Categories {
   data: string[];
   edit: string[];
   other?: string[];
+}
+
+declare global {
+  interface Window {
+    antdIconClassifier: AntdIconClassifier;
+  }
+}
+
+interface AntdIconClassifier {
+  load: Function;
+  predict: Function;
+}
+
+interface Result {
+  className: string;
+  score: number;
+}
+
+interface Icon {
+  type: string;
+  score: number;
 }
 
 const direction = [
@@ -282,7 +317,7 @@ declare const locale: NzSafeAny;
           Two Tone
         </label>
       </nz-radio-group>
-      <nz-input-group [nzSuffix]="suffixIconSearch">
+      <nz-input-group nzSearch [nzSuffix]="suffixIconCamera" [nzAddOnAfter]="addOnAfterIconSearch">
         <input
           nz-input
           [placeholder]="localeObj.search"
@@ -290,8 +325,29 @@ declare const locale: NzSafeAny;
           (ngModelChange)="onSearchChange()"
         />
       </nz-input-group>
-      <ng-template #suffixIconSearch>
-        <i nz-icon nzType="search"></i>
+      <ng-template #suffixIconCamera>
+        <div class="icon-pic-searcher">
+          <i
+            class="icon-pic-btn"
+            nz-icon
+            nz-popover
+            [nzPopoverTrigger]="null"
+            [(nzPopoverVisible)]="popoverVisible"
+            [nzPopoverContent]="localeObj.picSearcherIntro"
+            (click)="toggleModal()"
+          >
+            <svg viewBox="64 64 896 896">
+              <path
+                d="M864 248H728l-32.4-90.8a32.07 32.07 0 00-30.2-21.2H358.6c-13.5 0-25.6 8.5-30.1 21.2L296 248H160c-44.2 0-80 35.8-80 80v456c0 44.2 35.8 80 80 80h704c44.2 0 80-35.8 80-80V328c0-44.2-35.8-80-80-80zm8 536c0 4.4-3.6 8-8 8H160c-4.4 0-8-3.6-8-8V328c0-4.4 3.6-8 8-8h186.7l17.1-47.8 22.9-64.2h250.5l22.9 64.2 17.1 47.8H864c4.4 0 8 3.6 8 8v456zM512 384c-88.4 0-160 71.6-160 160s71.6 160 160 160 160-71.6 160-160-71.6-160-160-160zm0 256c-53 0-96-43-96-96s43-96 96-96 96 43 96 96-43 96-96 96z"
+              ></path>
+            </svg>
+          </i>
+        </div>
+      </ng-template>
+      <ng-template #addOnAfterIconSearch>
+        <button nz-button nzType="default" nzSearch>
+          <i nz-icon nzType="search"></i>
+        </button>
       </ng-template>
     </div>
     <ng-container *ngFor="let category of categoryNames; let i = index">
@@ -310,6 +366,76 @@ declare const locale: NzSafeAny;
         </li>
       </ul>
     </ng-container>
+    <nz-modal
+      [nzTitle]="localeObj.picSearcherTitle"
+      [(nzVisible)]="modalVisible"
+      (nzOnCancel)="toggleModal()"
+      [nzFooter]="null"
+    >
+      <ng-container *nzModalContent>
+        <ng-container *ngIf="modelLoaded; else modelLoadingTpl">
+          <nz-upload
+            nzType="drag"
+            nzAccept="image/jpeg, image/png"
+            nzListType="picture"
+            [nzCustomRequest]="customRequestUploadFile"
+            [nzFileList]="fileList"
+            [nzShowUploadList]="{ showPreviewIcon: false, showRemoveIcon: false }"
+          >
+            <p class="ant-upload-drag-icon">
+              <i nz-icon nzType="inbox" nzTheme="outline"></i>
+            </p>
+            <p class="ant-upload-text">{{ localeObj.picSearcherUploadText }}</p>
+            <p class="ant-upload-hint">{{ localeObj.picSearcherUploadHint }}</p>
+          </nz-upload>
+          <nz-spin [nzSpinning]="loading" [nzTip]="localeObj.picSearcherMatching">
+            <div class="icon-pic-search-result">
+              <ng-container *ngIf="icons.length">
+                <div class="result-tip">
+                  {{ localeObj.picSearcherResultTip }}
+                </div>
+                <table>
+                  <thead>
+                    <tr>
+                      <th class="col-icon">
+                        {{ localeObj.picSearcherThIcon }}
+                      </th>
+                      <th>{{ localeObj.picSearcherThScore }}</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr *ngFor="let icon of icons">
+                      <td class="col-icon">
+                        <i
+                          nz-icon
+                          nz-tooltip
+                          [nzTooltipTitle]="icon.type"
+                          nzTooltipPlacement="right"
+                          [nzType]="icon.type"
+                          [nzTheme]="currentTheme"
+                          (click)="onIconClick($event, icon.type)"
+                        >
+                        </i>
+                      </td>
+                      <td>
+                        <nz-progress nzStrokeLinecap="round" [nzPercent]="icon.score"></nz-progress>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </ng-container>
+              <nz-result *ngIf="error" nzStatus="500" nzTitle="503" [nzSubTitle]="localeObj.picSearcherServerError">
+              </nz-result>
+            </div>
+          </nz-spin>
+        </ng-container>
+        <ng-template #modelLoadingTpl>
+          <nz-spin [nzTip]="localeObj.picSearcherModelLoading">
+            <div style="height: 100px;"></div>
+          </nz-spin>
+        </ng-template>
+      </ng-container>
+    </nz-modal>
   `,
   styles: [
     `
@@ -336,12 +462,20 @@ declare const locale: NzSafeAny;
     `
   ]
 })
-export class NzPageDemoIconComponent implements OnInit {
+export class NzPageDemoIconComponent implements OnInit, OnDestroy {
   displayedNames: Array<{ name: string; icons: string[] }> = [];
   categoryNames: string[] = [];
   currentTheme: ThemeType = 'outline';
   localeObj: { [key: string]: string } = locale;
   searchingString = '';
+
+  error = false;
+  loading = false;
+  modelLoaded = false;
+  modalVisible = false;
+  popoverVisible = false;
+  fileList: NzUploadFile[] = [];
+  icons: Icon[] = [];
 
   trackByFn = (_index: number, item: string): string => `${item}-${this.currentTheme}`;
 
@@ -358,6 +492,9 @@ export class NzPageDemoIconComponent implements OnInit {
         target.classList.remove('copied');
       }, 1000);
     });
+
+    const content = this.getCopiedStringTemplateRef(copiedString);
+    this.message.success(content);
   }
 
   private _copy(value: string): Promise<string> {
@@ -418,15 +555,154 @@ export class NzPageDemoIconComponent implements OnInit {
     this.prepareIcons();
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  constructor(@Inject(DOCUMENT) private dom: any, private _iconService: NzIconService) {
+  private getCopiedStringTemplateRef(copiedString: string): TemplateRef<void> {
+    this.viewContainerRef.clear();
+    const factory = this.componentFactoryResolver.resolveComponentFactory(NzPageDemoIconCopiedCodeComponent);
+    const componentRef = this.viewContainerRef.createComponent(factory);
+    componentRef.instance.copiedCode = copiedString;
+
+    return componentRef.instance.templateRef;
+  }
+
+  private loadModel(): void {
+    if (window.antdIconClassifier) {
+      this.onLoad();
+      return;
+    }
+
+    const script = this.dom.createElement('script');
+    const source = 'https://cdn.jsdelivr.net/gh/lewis617/antd-icon-classifier@0.0/dist/main.js';
+    script.type = 'text/javascript';
+    script.src = source;
+    script.onload = async () => {
+      await window.antdIconClassifier.load();
+      this.onLoad();
+    };
+    script.onerror = () => {
+      throw new Error(`${PREFIX} cannot load assets of antd icon classifier from source "${source}".`);
+    };
+
+    this.dom.head.appendChild(script);
+  }
+
+  private onLoad(): void {
+    this.modelLoaded = true;
+    this.dom.addEventListener('paste', this.onPaste as EventListener);
+  }
+
+  private onPaste = (event: ClipboardEvent): void => {
+    const items = event.clipboardData && event.clipboardData.items;
+    let file = null;
+    if (items && items.length) {
+      for (let i = 0; i < items.length; i += 1) {
+        if (items[i].type.indexOf('image') !== -1) {
+          file = items[i].getAsFile();
+          break;
+        }
+      }
+    }
+    if (file) this.uploadFile(file);
+  };
+
+  // We don't need to upload it, so just fake api here
+  customRequestUploadFile = (o: NzUploadXHRArgs): Subscription => {
+    return of(true).subscribe(() => {
+      this.uploadFile(o.file);
+    });
+  };
+
+  private uploadFile = (file: File | NzUploadFile): void => {
+    this.loading = true;
+    const reader: FileReader = new FileReader();
+    reader.onload = () => {
+      this.toImage(reader.result as string).then(this.predict);
+      this.fileList = [{ uid: '1', name: file.name, status: 'done', url: reader.result as string }];
+    };
+    reader.readAsDataURL(file as File);
+  };
+
+  private toImage(url: string): Promise<HTMLImageElement> {
+    return new Promise(resolve => {
+      const img = new Image();
+      img.setAttribute('crossOrigin', 'anonymous');
+      img.src = url;
+      img.onload = function onload() {
+        resolve(img);
+      };
+    });
+  }
+
+  private predict = (imgEl: HTMLImageElement): void => {
+    try {
+      const results = window.antdIconClassifier.predict(imgEl);
+      this.icons = results.map((r: Result) => ({
+        score: Math.ceil(r.score * 100),
+        type: r.className.replace(/\s/g, '-')
+      }));
+      this.loading = false;
+      this.error = false;
+    } catch (err) {
+      this.loading = false;
+      this.error = true;
+    }
+  };
+
+  toggleModal(): void {
+    this.modalVisible = !this.modalVisible;
+    this.popoverVisible = false;
+    this.fileList = [];
+    this.icons = [];
+
+    if (!localStorage.getItem('disableIconTip')) {
+      localStorage.setItem('disableIconTip', 'true');
+    }
+  }
+
+  handleChange({ file, fileList }: NzUploadChangeParam): void {
+    if (file.status === 'uploading' || file.status === 'error') {
+      this.fileList = fileList;
+    } else if (file.status === 'done') {
+      this.uploadFile(file.originFileObj!);
+    }
+  }
+
+  constructor(
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    @Inject(DOCUMENT) private dom: any,
+    private _iconService: NzIconService,
+    private componentFactoryResolver: ComponentFactoryResolver,
+    private message: NzMessageService,
+    private viewContainerRef: ViewContainerRef
+  ) {
     // This is to test that tree shake works!
     this._iconService.addIcon(AccountBookFill);
   }
 
   ngOnInit(): void {
     this.setIconsShouldBeDisplayed('outline');
+    this.loadModel();
+    this.popoverVisible = !localStorage.getItem('disableIconTip');
   }
+
+  ngOnDestroy(): void {
+    this.dom.removeEventListener('paste', this.onPaste as EventListener);
+    this.viewContainerRef.clear();
+  }
+}
+
+@Component({
+  selector: 'nz-page-demo-icon-copied-code',
+  template: `
+    <ng-template #templateRef>
+      <span>
+        <code class="copied-code">{{ copiedCode }}</code> copied 🎉
+      </span>
+    </ng-template>
+  `
+})
+export class NzPageDemoIconCopiedCodeComponent {
+  @Input() copiedCode!: string;
+  @ViewChild('templateRef', { static: true }) templateRef!: TemplateRef<void>;
 }
 
 function camelCase(value: string): string {
