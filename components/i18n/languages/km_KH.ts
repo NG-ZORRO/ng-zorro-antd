351fc70688cd10dc69fcbd0e7250628c508cc6c3
SamnangNhum <68495204+SamnangNhum@users.noreply.github.com>
2022-01-19 15:13:49 +0700

    feat: add km_kh to i18n

---
+++ components/i18n/languages/km_KH.ts
@@ -0,0 +1,174 @@
+/**
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/NG-ZORRO/ng-zorro-antd/blob/master/LICENSE
+ */
+
+export default {
+  locale: 'km',
+  Pagination: {
+    items_per_page: '/ ទំព័រ',
+    jump_to: 'លោត​ទៅ',
+    jump_to_confirm: 'បញ្ជាក់',
+    page: 'ទំព័រ',
+    prev_page: 'ទំព័រ​មុន',
+    next_page: 'ទំព័រ​​បន្ទាប់',
+    prev_5: '៥ ទំព័រថយក្រោយ',
+    next_5: '៥ ទំព័រទៅមុខ',
+    prev_3: '៣ ទំព័រថយក្រោយ',
+    next_3: '៣ ទំព័រទៅមុខ'
+  },
+  DatePicker: {
+    lang: {
+      placeholder: 'រើសថ្ងៃ',
+      yearPlaceholder: 'រើសឆ្នាំ',
+      quarterPlaceholder: 'រើសត្រីមាស',
+      monthPlaceholder: 'រើសខែ',
+      weekPlaceholder: 'រើសសប្តាហ៍',
+      rangePlaceholder: ['ថ្ងៃចាប់ផ្ដើម', 'ថ្ងៃបញ្ចប់'],
+      rangeYearPlaceholder: ['ឆ្នាំចាប់ផ្ដើម', 'ឆ្នាំបញ្ចប់'],
+      rangeMonthPlaceholder: ['ខែចាប់ផ្ដើម', 'ខែបញ្ចប់'],
+      rangeWeekPlaceholder: ['សប្ដាហ៍ចាប់ផ្ដើម', 'សប្ដាហ៍បញ្ចប់'],
+      locale: 'km_KH',
+      today: 'ថ្ងៃនេះ',
+      now: 'ឥឡូវ​នេះ',
+      backToToday: 'ត្រលប់ទៅថ្ងៃនេះ',
+      ok: 'យល់ព្រម',
+      clear: 'ត្រឡប់ដើម',
+      month: 'ខែ',
+      year: 'ឆ្នាំ',
+      timeSelect: 'រើសម៉ោង',
+      dateSelect: 'រើសកាលបរិច្ឆេទ',
+      weekSelect: 'រើសសប្តាហ៍',
+      monthSelect: 'រើសខែ',
+      yearSelect: 'រើសឆ្នាំ',
+      decadeSelect: 'រើសទសវត្សរ៍',
+      yearFormat: 'YYYY',
+      dateFormat: 'M/D/YYYY',
+      dayFormat: 'D',
+      dateTimeFormat: 'M/D/YYYY HH:mm:ss',
+      monthBeforeYear: true,
+      previousMonth: 'ខែមុន (PageUp)',
+      nextMonth: 'ខែក្រោយ (PageDown)',
+      previousYear: 'ឆ្នាំមុន (Control + left)',
+      nextYear: 'ឆ្នាំក្រោយ (Control + right)',
+      previousDecade: 'ទសវត្សរ៍មុន',
+      nextDecade: 'ទសវត្សរ៍ក្រោយ',
+      previousCentury: 'សតវត្សមុន',
+      nextCentury: 'សតវត្សក្រោយ'
+    },
+    timePickerLocale: {
+      placeholder: 'រើសម៉ោង',
+      rangePlaceholder: ['ម៉ោងចប់ផ្ដើម', 'ម៉ោងបញ្ចប់']
+    }
+  },
+  TimePicker: {
+    placeholder: 'រើសម៉ោង',
+    rangePlaceholder: ['ម៉ោងចប់ផ្ដើម', 'ម៉ោងបញ្ចប់']
+  },
+  Calendar: {
+    lang: {
+      placeholder: 'រើសថ្ងៃ',
+      yearPlaceholder: 'រើសឆ្នាំ',
+      quarterPlaceholder: 'រើសត្រីមាស',
+      monthPlaceholder: 'រើសខែ',
+      weekPlaceholder: 'រើសសប្តាហ៍',
+      rangePlaceholder: ['ថ្ងៃចាប់ផ្ដើម', 'ថ្ងៃបញ្ចប់'],
+      rangeYearPlaceholder: ['ឆ្នាំចាប់ផ្ដើម', 'ឆ្នាំបញ្ចប់'],
+      rangeMonthPlaceholder: ['ខែចាប់ផ្ដើម', 'ខែបញ្ចប់'],
+      rangeWeekPlaceholder: ['សប្ដាហ៍ចាប់ផ្ដើម', 'សប្ដាហ៍បញ្ចប់'],
+      locale: 'km_KH',
+      today: 'ថ្ងៃនេះ',
+      now: 'ឥឡូវ​នេះ',
+      backToToday: 'ត្រលប់ទៅថ្ងៃនេះ',
+      ok: 'យល់ព្រម',
+      clear: 'ត្រឡប់ដើម',
+      month: 'ខែ',
+      year: 'ឆ្នាំ',
+      timeSelect: 'រើសម៉ោង',
+      dateSelect: 'រើសកាលបរិច្ឆេទ',
+      weekSelect: 'រើសសប្តាហ៍',
+      monthSelect: 'រើសខែ',
+      yearSelect: 'រើសឆ្នាំ',
+      decadeSelect: 'រើសទសវត្សរ៍',
+      yearFormat: 'YYYY',
+      dateFormat: 'M/D/YYYY',
+      dayFormat: 'D',
+      dateTimeFormat: 'M/D/YYYY HH:mm:ss',
+      monthBeforeYear: true,
+      previousMonth: 'ខែមុន (PageUp)',
+      nextMonth: 'ខែក្រោយ (PageDown)',
+      previousYear: 'ឆ្នាំមុន (Control + left)',
+      nextYear: 'ឆ្នាំក្រោយ (Control + right)',
+      previousDecade: 'ទសវត្សរ៍មុន',
+      nextDecade: 'ទសវត្សរ៍ក្រោយ',
+      previousCentury: 'សតវត្សមុន',
+      nextCentury: 'សតវត្សក្រោយ'
+    },
+    timePickerLocale: {
+      placeholder: 'រើសម៉ោង',
+      rangePlaceholder: ['ម៉ោងចប់ផ្ដើម', 'ម៉ោងបញ្ចប់']
+    }
+  },
+  global: {
+    placeholder: 'សូមជ្រើសរើស'
+  },
+  Table: {
+    filterTitle: 'បញ្ចីតម្រៀប',
+    filterConfirm: 'យល់ព្រម',
+    filterReset: 'ត្រឡប់ដើម',
+    filterEmptyText: 'គ្មានបញ្ចីតម្រៀប',
+    emptyText: 'គ្មានទិន្នន័យ',
+    selectAll: 'រើសក្នុងទំព័រនេះ',
+    selectInvert: 'បញ្ច្រាសក្នុងទំព័រនេះ',
+    selectionAll: 'រើសយកទាំងអស់',
+    sortTitle: 'តម្រៀប',
+    expand: 'ពន្លាត',
+    collapse: 'បិតបាំង',
+    triggerDesc: 'ចុចដើម្បីរៀបតាមលំដាប់ធំ',
+    triggerAsc: 'ចុចដើម្បីរៀបតាមលំដាប់តូច​',
+    cancelSort: 'ចុចដើម្បីបោះបង់'
+  },
+  Modal: {
+    okText: 'យល់ព្រម',
+    cancelText: 'បោះបង់',
+    justOkText: 'យល់ព្រម'
+  },
+  Popconfirm: {
+    okText: 'យល់ព្រម',
+    cancelText: 'បោះបង់'
+  },
+  Transfer: {
+    titles: ['', ''],
+    searchPlaceholder: 'ស្វែងរកនៅទីនេះ',
+    itemUnit: 'item',
+    itemsUnit: 'items',
+    remove: 'លុប',
+    selectCurrent: 'រើសទំព័របច្ចុប្បន្ន',
+    removeCurrent: 'លុបទំព័របច្ចុប្បន្ន',
+    selectAll: 'រើសទិន្នន័យទាំងអស់',
+    removeAll: 'លុបទិន្នន័យទាំងអស់',
+    selectInvert: 'បញ្ច្រាសទំព័របច្ចុប្បន្ន'
+  },
+  Upload: {
+    uploading: 'កំពុងបញ្ចូលឡើង...',
+    removeFile: 'លុបឯកសារ',
+    uploadError: 'បញ្ចូលមិនជោគជ័យ',
+    previewFile: 'មើលឯកសារ',
+    downloadFile: 'ទាញយកឯកសារ'
+  },
+  Empty: {
+    description: 'គ្មានទិន្នន័យ'
+  },
+  Icon: {
+    icon: 'icon'
+  },
+  Text: {
+    edit: 'កែ',
+    copy: 'Copy',
+    copied: 'Copied',
+    expand: 'ពង្រីក'
+  },
+  PageHeader: {
+    back: 'Back'
+  }
+};
