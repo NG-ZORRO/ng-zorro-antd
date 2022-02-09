bac706ff23b1605bc5b05b95d3363507f81890ba
chenc <double_cl@163.com>
2022-02-09 16:17:51 +0800

    chore: clean up the code

--- components/slider/slider.component.ts
+++ components/slider/slider.component.ts
@@ -272,8 +272,9 @@ export class NzSliderComponent implements ControlValueAccessor, OnInit, OnChange
     } else if (!valuesEqual(this.value!, value!)) {
       this.value = value;
       this.updateTrackAndHandles();
-      this.nzOnAfterChange.emit(this.getValue(true));
-      this.onValueChange(this.getValue(true));
+      const sliderValue = this.getValue(true);
+      this.nzOnAfterChange.emit(sliderValue);
+      this.onValueChange(sliderValue);
     }
   }
 
