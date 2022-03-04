8d7adbbf8f1678f76c223a8e2407f277a8971683
arturovt <arthurandrosovich@gmail.com>
2022-03-04 12:56:01 +0200

    perf(module:modal): call `focus()` on the next rendering frame to prevent frame drop
    Calling `focus()` on the element causes browsers to do re-layouts. This can been seen here:
    https://gist.github.com/paulirish/5d52fb081b3570c81e3a#setting-focus
    Microtasks are executed asynchronously, but within the current rendering frame. Using `requestAnimationFrame`
    except of `Promise.resolve` basically unloads the current frame and calls `focus()` on the next rendering
    frame, this prevent frame drops on slower devices when the modal is opened.

--- components/modal/modal.spec.ts
+++ components/modal/modal.spec.ts
@@ -1095,7 +1095,7 @@ describe('NzModal', () => {
       });
 
       fixture.detectChanges();
-      flushMicrotasks();
+      tick(16);
 
       expect(document.activeElement!.tagName).toBe('NZ-MODAL-CONTAINER', 'Expected modal container to be focused.');
     }));
