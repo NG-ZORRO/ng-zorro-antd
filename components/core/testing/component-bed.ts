444a46f8a96449b5a4f31e9fa07bf664c50e5f55
chenc <double_cl@163.com>
2022-09-19 15:32:26 +0800

    fix(module:tooltip): remove space

--- components/core/testing/component-bed.ts
+++ components/core/testing/component-bed.ts
@@ -5,7 +5,7 @@
 
 import { CommonModule } from '@angular/common';
 import { DebugElement, NgModule, NO_ERRORS_SCHEMA, Type } from '@angular/core';
-import { ComponentFixture,  TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 type ComponentBedOptions = Pick<NgModule, 'providers' | 'declarations' | 'imports'>;
