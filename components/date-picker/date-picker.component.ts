b1fc6931445f667e4c013ee6e51614449238a850
Nicolas Frizzarin <nicolas.frizzarin54@gmail.com>
2023-06-30 15:59:53 +0200

    refactor(module:datepicker): :recycle: use existing method to clone

--- components/date-picker/date-picker.component.ts
+++ components/date-picker/date-picker.component.ts
@@ -22,7 +22,6 @@ import {
   Component,
   ElementRef,
   EventEmitter,
-  forwardRef,
   Host,
   Inject,
   Input,
@@ -37,7 +36,8 @@ import {
   TemplateRef,
   ViewChild,
   ViewChildren,
-  ViewEncapsulation
+  ViewEncapsulation,
+  forwardRef
 } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { fromEvent, of as observableOf } from 'rxjs';
@@ -50,7 +50,7 @@ import { NzFormNoStatusService, NzFormStatusService } from 'ng-zorro-antd/core/f
 import { NzNoAnimationDirective } from 'ng-zorro-antd/core/no-animation';
 import { DATE_PICKER_POSITION_MAP, DEFAULT_DATE_PICKER_POSITIONS } from 'ng-zorro-antd/core/overlay';
 import { NzDestroyService } from 'ng-zorro-antd/core/services';
-import { CandyDate, cloneDate, CompatibleValue, wrongSortOrder } from 'ng-zorro-antd/core/time';
+import { CandyDate, CompatibleValue, cloneDate, wrongSortOrder } from 'ng-zorro-antd/core/time';
 import {
   BooleanInput,
   FunctionProp,
@@ -61,7 +61,7 @@ import {
   OnChangeType,
   OnTouchedType
 } from 'ng-zorro-antd/core/types';
-import { getStatusClassNames, InputBoolean, toBoolean, valueFunctionProp } from 'ng-zorro-antd/core/util';
+import { InputBoolean, getStatusClassNames, toBoolean, valueFunctionProp } from 'ng-zorro-antd/core/util';
 import {
   DateHelperService,
   NzDatePickerI18nInterface,
@@ -780,10 +780,8 @@ export class NzDatePickerComponent implements OnInit, OnChanges, AfterViewInit, 
   onTouchedFn: OnTouchedType = () => void 0;
 
   writeValue(value: CompatibleDate): void {
-    if (value) {
-      this.setValue(value);
-      this.cdr.markForCheck();
-    }
+    this.setValue(value);
+    this.cdr.markForCheck();
   }
 
   registerOnChange(fn: OnChangeType): void {
@@ -841,10 +839,9 @@ export class NzDatePickerComponent implements OnInit, OnChanges, AfterViewInit, 
 
   // Safe way of setting value with default
   private setValue(value: CompatibleDate): void {
-    const initialDate = value;
-    const currentDate = Array.isArray(value) ? [...value] : new Date(initialDate as Date);
-    this.datePickerService.setValue(this.datePickerService.makeValue(currentDate));
-    this.datePickerService.initialValue = this.datePickerService.makeValue(initialDate);
+    const newValue = this.datePickerService.makeValue(value);
+    this.datePickerService.setValue(newValue);
+    this.datePickerService.initialValue = cloneDate(newValue);
     this.cdr.detectChanges();
   }
 
