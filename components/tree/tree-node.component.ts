2d0b3f71490e38f8512285f81fcf3baa8f6eb4db
simplejason <simplejason.coder@gmail.com>
2023-07-16 18:33:15 +0800

    fix(module: tree): fix nz-tree-node keep dragging class with nzBeforeDrop (#8015)
    * fix(module: tree): fix nz-tree-node keep dragging class when nzBeforeDrop passed in
    
    * fix(module:tree): fix dragging class
    
    * fix(module:tree): add  markForCheck
    
    ---------
    
    Co-authored-by: BBBoy01 <1156678002@qq.com>

--- components/tree/tree-node.component.ts
+++ components/tree/tree-node.component.ts
@@ -357,6 +357,11 @@ export class NzTreeNodeBuiltinComponent implements OnInit, OnChanges, OnDestroy 
         this.draggingKey = null;
         const eventNext = this.nzTreeService.formatEvent('dragend', this.nzTreeNode, e);
         this.nzOnDragEnd.emit(eventNext);
+      } else {
+        // clear dragging state
+        this.draggingKey = null;
+        this.markForCheck();
+        console.log('end');
       }
     });
   }
