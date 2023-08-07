f98335ed2175e97840b07b404ce1e4a6130ca43d
hyperlife1119 <hyperlife1119@qq.com>
2023-08-07 23:40:39 +0800

    feat(module:notification): support for more custom templates

--- components/notification/typings.ts
+++ components/notification/typings.ts
@@ -8,6 +8,8 @@ import { Subject } from 'rxjs';
 
 import { NgClassInterface, NgStyleInterface } from 'ng-zorro-antd/core/types';
 
+import type { NzNotificationComponent } from './notification.component';
+
 export type NzNotificationPlacement = 'topLeft' | 'topRight' | 'bottomLeft' | 'bottomRight' | 'top' | 'bottom';
 
 export interface NzNotificationDataOptions<T = {}> {
@@ -15,6 +17,7 @@ export interface NzNotificationDataOptions<T = {}> {
   nzStyle?: NgStyleInterface;
   nzClass?: NgClassInterface | string;
   nzCloseIcon?: TemplateRef<void> | string;
+  nzButton?: TemplateRef<{ $implicit: NzNotificationComponent }>;
   nzPlacement?: NzNotificationPlacement;
   nzData?: T;
   nzDuration?: number;
@@ -23,13 +26,13 @@ export interface NzNotificationDataOptions<T = {}> {
 }
 
 export interface NzNotificationData {
+  title?: string | TemplateRef<void>;
   content?: string | TemplateRef<void>;
   createdAt?: Date;
   messageId?: string;
   options?: NzNotificationDataOptions;
   state?: 'enter' | 'leave';
   template?: TemplateRef<{}>;
-  title?: string;
   type?: 'success' | 'info' | 'warning' | 'error' | 'blank' | string;
 
   // observables exposed to users
