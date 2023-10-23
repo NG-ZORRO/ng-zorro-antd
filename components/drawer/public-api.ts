24d06640a623f3ea2fd9fa459c729a103938d7fc
Nicolas Frizzarin <nicolas.frizzarin54@gmail.com>
2023-10-23 05:02:03 +0200

    fix(module:drawer): drawer not open (#8120)
    Drawer don't open in a lazyload module
    
    refactor(module:drawer): remove useless DrawerServiceModule

--- components/drawer/public-api.ts
+++ components/drawer/public-api.ts
@@ -3,10 +3,9 @@
  * found in the LICENSE file at https://github.com/NG-ZORRO/ng-zorro-antd/blob/master/LICENSE
  */
 
-export * from './drawer.component';
 export * from './drawer-content.directive';
-export * from './drawer.module';
-export * from './drawer.service';
-export * from './drawer.service.module';
 export * from './drawer-options';
 export * from './drawer-ref';
+export * from './drawer.component';
+export * from './drawer.module';
+export * from './drawer.service';
