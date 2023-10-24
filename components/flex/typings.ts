a1a6d96a809e68849d612105916a0ce693c8a1ea
Parsa Arvaneh <Parsa.Arvaneh@gmail.com>
2023-10-24 10:00:36 +0330

    [ADD] typings

---
+++ components/flex/typings.ts
@@ -0,0 +1,5 @@
+export type nzDirection = 'row' | 'row-reverse' | 'column' | 'column-reverse';
+
+export type nzJustify = 'flex-start' | 'center' | 'flex-end' | 'space-between' | 'space-around' | 'space-evenly';
+
+export type nzAlign = 'flex-start' | 'center' | 'flex-end';
