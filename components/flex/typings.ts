76b9fcbd4896a0f02dd13cf9b03a1e71a30f5647
Parsa Arvaneh <Parsa.Arvaneh@gmail.com>
2023-10-25 16:09:21 +0330

    [ADD] gap for flex

--- components/flex/typings.ts
+++ components/flex/typings.ts
@@ -1,5 +1,13 @@
+/**
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/NG-ZORRO/ng-zorro-antd/blob/master/LICENSE
+ */
+
 export type nzDirection = 'row' | 'row-reverse' | 'column' | 'column-reverse';
 
 export type nzJustify = 'flex-start' | 'center' | 'flex-end' | 'space-between' | 'space-around' | 'space-evenly';
 
 export type nzAlign = 'flex-start' | 'center' | 'flex-end';
+
+export type nzGap = 'small' | 'middle' | 'large' | nzCustomGap;
+export type nzCustomGap = number;
