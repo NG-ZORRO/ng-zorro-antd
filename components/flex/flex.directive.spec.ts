efff8da04e7eb089ae0c5be8932068dc8e50ef68
Parsa Arvaneh <Parsa.Arvaneh@gmail.com>
2023-11-02 20:05:27 +0330

    feat(module:flex): add flex component

--- components/flex/flex.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FlexDirective } from './flex.directive';
-
-describe('FlexDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FlexDirective();
-    expect(directive).toBeTruthy();
-  });
-});
