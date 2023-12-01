03cda216f2d0f1b31e365d6cb30a309309cbc868
Evgeniy Efimov <x_jenka_x@inbox.ru>
2023-12-01 12:45:43 +0100

    feat(module:anchor): support standalone component (#8185)
    * feat(module:anchor): support standalone component
    
    * feat(module:anchor): support standalone component
    
    * feat(module:anchor): support standalone component

--- components/anchor/anchor.module.ts
+++ components/anchor/anchor.module.ts
@@ -3,19 +3,13 @@
  * found in the LICENSE file at https://github.com/NG-ZORRO/ng-zorro-antd/blob/master/LICENSE
  */
 
-import { BidiModule } from '@angular/cdk/bidi';
-import { PlatformModule } from '@angular/cdk/platform';
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-
-import { NzAffixModule } from 'ng-zorro-antd/affix';
 
 import { NzAnchorLinkComponent } from './anchor-link.component';
 import { NzAnchorComponent } from './anchor.component';
 
 @NgModule({
-  declarations: [NzAnchorComponent, NzAnchorLinkComponent],
   exports: [NzAnchorComponent, NzAnchorLinkComponent],
-  imports: [BidiModule, CommonModule, NzAffixModule, PlatformModule]
+  imports: [NzAnchorComponent, NzAnchorLinkComponent]
 })
 export class NzAnchorModule {}
--- components/anchor/doc/index.en-US.md
+++ components/anchor/doc/index.en-US.md
@@ -17,7 +17,7 @@ import { NzAnchorModule } from 'ng-zorro-antd/anchor';
 
 ## API
 
-### nz-anchor
+### nz-anchor:standalone
 
 | Property | Description | Type | Default | Global Config |
 | -------- | ----------- | ---- | ------- | ------------- |
@@ -32,7 +32,7 @@ import { NzAnchorModule } from 'ng-zorro-antd/anchor';
 | `(nzChange)` | Listening for anchor link change | `EventEmitter<string>` | - |  |
 | `(nzScroll)` | The scroll function that is triggered when scrolling to an anchor. | `EventEmitter<NzAnchorLinkComponent>` | - |
 
-### nz-link
+### nz-link:standalone
 
 | Property | Description | Type | Default |
 | -------- | ----------- | ---- | ------- |
