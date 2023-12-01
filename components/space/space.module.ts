a84ddeff5582426a3dd608cab567245898be60c7
Parsa Arvaneh <62149413+ParsaArvanehPA@users.noreply.github.com>
2023-12-01 06:11:50 +0330

    feat(module:space): support standalone component (#8218)

--- components/space/space.module.ts
+++ components/space/space.module.ts
@@ -3,16 +3,13 @@
  * found in the LICENSE file at https://github.com/NG-ZORRO/ng-zorro-antd/blob/master/LICENSE
  */
 
-import { BidiModule } from '@angular/cdk/bidi';
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { NzSpaceItemDirective } from './space-item.directive';
 import { NzSpaceComponent } from './space.component';
 
 @NgModule({
-  declarations: [NzSpaceComponent, NzSpaceItemDirective],
-  exports: [NzSpaceComponent, NzSpaceItemDirective],
-  imports: [BidiModule, CommonModule]
+  imports: [NzSpaceComponent, NzSpaceItemDirective],
+  exports: [NzSpaceComponent, NzSpaceItemDirective]
 })
 export class NzSpaceModule {}
