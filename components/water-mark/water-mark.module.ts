e4d608274e0b56acf9b720cf519d757c660c125e
originRing <48777107+OriginRing@users.noreply.github.com>
2023-12-02 09:56:23 +0800

    fix(module:water-mark): nzWaterMark is a block element && standalone (#8197)
    fix: #8187

--- components/water-mark/water-mark.module.ts
+++ components/water-mark/water-mark.module.ts
@@ -3,14 +3,12 @@
  * found in the LICENSE file at https://github.com/NG-ZORRO/ng-zorro-antd/blob/master/LICENSE
  */
 
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { NzWaterMarkComponent } from './water-mark.component';
 
 @NgModule({
   exports: [NzWaterMarkComponent],
-  declarations: [NzWaterMarkComponent],
-  imports: [CommonModule]
+  imports: [NzWaterMarkComponent]
 })
 export class NzWaterMarkModule {}
