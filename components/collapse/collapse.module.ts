64fdde030a21472e3a467858cf8b8ef52df3cb26
Parsa Arvaneh <Parsa.Arvaneh@gmail.com>
2023-12-03 00:57:41 +0330

    feat(module:collapse): support standalone component

--- components/collapse/collapse.module.ts
+++ components/collapse/collapse.module.ts
@@ -3,20 +3,13 @@
  * found in the LICENSE file at https://github.com/NG-ZORRO/ng-zorro-antd/blob/master/LICENSE
  */
 
-import { BidiModule } from '@angular/cdk/bidi';
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-
-import { NzNoAnimationModule } from 'ng-zorro-antd/core/no-animation';
-import { NzOutletModule } from 'ng-zorro-antd/core/outlet';
-import { NzIconModule } from 'ng-zorro-antd/icon';
 
 import { NzCollapsePanelComponent } from './collapse-panel.component';
 import { NzCollapseComponent } from './collapse.component';
 
 @NgModule({
-  declarations: [NzCollapsePanelComponent, NzCollapseComponent],
-  exports: [NzCollapsePanelComponent, NzCollapseComponent],
-  imports: [BidiModule, CommonModule, NzIconModule, NzOutletModule, NzNoAnimationModule]
+  imports: [NzCollapsePanelComponent, NzCollapseComponent],
+  exports: [NzCollapsePanelComponent, NzCollapseComponent]
 })
 export class NzCollapseModule {}
