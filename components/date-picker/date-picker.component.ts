74181ca55bf396bd9230e26af1d0417eb224e03f
Parsa Arvaneh <Parsa.Arvaneh@gmail.com>
2023-12-05 01:20:24 +0330

    feat(module:date-picker): add bidi module in import section

--- components/date-picker/date-picker.component.ts
+++ components/date-picker/date-picker.component.ts
@@ -3,7 +3,7 @@
  * found in the LICENSE file at https://github.com/NG-ZORRO/ng-zorro-antd/blob/master/LICENSE
  */
 
-import { Direction, Directionality } from '@angular/cdk/bidi';
+import { BidiModule, Direction, Directionality } from '@angular/cdk/bidi';
 import { ESCAPE } from '@angular/cdk/keycodes';
 import {
   CdkConnectedOverlay,
@@ -264,7 +264,8 @@ export type NzPlacement = 'bottomLeft' | 'bottomRight' | 'topLeft' | 'topRight';
     DateRangePopupComponent,
     CdkConnectedOverlay,
     NzOverlayModule,
-    NzNoAnimationModule
+    NzNoAnimationModule,
+    BidiModule
   ],
   standalone: true
 })
