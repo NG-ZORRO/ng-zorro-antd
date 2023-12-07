6a4e766d07f0d3d4329719877478a9430debfd7d
Parsa Arvaneh <Parsa.Arvaneh@gmail.com>
2023-12-07 17:34:15 +0330

    feat(module:date-picker): support standalone component

--- components/date-picker/date-picker.component.ts
+++ components/date-picker/date-picker.component.ts
@@ -3,7 +3,7 @@
  * found in the LICENSE file at https://github.com/NG-ZORRO/ng-zorro-antd/blob/master/LICENSE
  */
 
-import { BidiModule, Direction, Directionality } from '@angular/cdk/bidi';
+import { Direction, Directionality } from '@angular/cdk/bidi';
 import { ESCAPE } from '@angular/cdk/keycodes';
 import {
   CdkConnectedOverlay,
@@ -264,8 +264,7 @@ export type NzPlacement = 'bottomLeft' | 'bottomRight' | 'topLeft' | 'topRight';
     DateRangePopupComponent,
     CdkConnectedOverlay,
     NzOverlayModule,
-    NzNoAnimationModule,
-    BidiModule
+    NzNoAnimationModule
   ],
   standalone: true
 })
