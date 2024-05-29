434ce5a346af2c5a53eb50ccc74361713b90f174
hyperlife1119 <hyperlife1119@qq.com>
2024-05-29 09:14:25 +0800

    fix: fix tinyColor

--- components/style/color/tinyColor.js
+++ components/style/color/tinyColor.js
@@ -3,7 +3,7 @@ const tinycolor = require('./tinycolor2');
 module.exports = {
   install: function (less, pluginManager, functions) {
     functions.add('tinycolor', function (...args) {
-      return tinycolor(args.map(ref => ref.value));
+      return tinycolor(...args.map(ref => ref.value));
     });
   }
 };
