5baffa71cce413d26f4f16eadd917ddbfafa001b
Laffery <laffery86@gmail.com>
2024-06-27 16:05:48 +0800

    docs: update angular doc links with angular.cn in zh_cn page

--- scripts/site/_site/doc/app/footer/footer.component.ts
+++ scripts/site/_site/doc/app/footer/footer.component.ts
@@ -19,8 +19,14 @@ import { NzColor } from 'ng-zorro-antd/color-picker';
               description="React"
             ></app-footer-item>
             <app-footer-item title="Ant Design" link="https://vue.ant.design/" description="Vue"></app-footer-item>
-            <app-footer-item title="Angular" link="https://angular.dev/"></app-footer-item>
-            <app-footer-item title="Angular CLI" link="https://angular.dev/tools/cli"></app-footer-item>
+            <app-footer-item
+              title="Angular"
+              [link]="language === 'zh' ? 'https://angular.cn/' : 'https://angular.dev/'"
+            ></app-footer-item>
+            <app-footer-item
+              title="Angular CLI"
+              [link]="language === 'zh' ? 'https://angular.cn/tools/cli' : 'https://angular.dev/tools/cli'"
+            ></app-footer-item>
           </div>
           <div app-footer-col [title]="language === 'zh' ? '社区' : 'Community'">
             <app-footer-item
