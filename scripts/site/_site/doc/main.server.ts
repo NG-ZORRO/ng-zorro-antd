fc6f7f3717ee1c5fa4f1c6130ecb7bc94d84342a
Laffery <laffery86@gmail.com>
2024-07-22 13:48:54 +0800

    refactor(module:docs): refactor docs to standalone mode

--- scripts/site/_site/doc/main.server.ts
+++ scripts/site/_site/doc/main.server.ts
@@ -1,6 +1,14 @@
+import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
 
 const bootstrap = () => bootstrapApplication(AppComponent, config);
 
