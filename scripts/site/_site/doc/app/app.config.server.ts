4310bb5f38da1af9cdefc5956a08400b6244d7d1
Laffery <laffery86@gmail.com>
2024-07-25 11:02:29 +0800

    feat(module:icon): update docs site with provideNzIcons api

--- scripts/site/_site/doc/app/app.config.server.ts
+++ scripts/site/_site/doc/app/app.config.server.ts
@@ -1,28 +1,25 @@
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
 import zh from '@angular/common/locales/zh';
-import { mergeApplicationConfig, ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 
 import * as AllIcons from '@ant-design/icons-angular/icons';
 import { IconDefinition } from '@ant-design/icons-angular';
-import { NzIconModule } from 'ng-zorro-antd/icon';
+import { provideNzIcons } from 'ng-zorro-antd/icon';
 
 import { appConfig } from './app.config';
 
 registerLocaleData(zh, 'zh-cn');
 registerLocaleData(en);
 
-const antDesignIcons = AllIcons as {
-  [key: string]: IconDefinition;
-};
-
+const antDesignIcons = AllIcons as Record<string, IconDefinition>;
 const icons: IconDefinition[] = Object.keys(antDesignIcons).map(key => antDesignIcons[key]);
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
-    importProvidersFrom(NzIconModule.forRoot(icons))
+    provideNzIcons(icons)
   ]
 };
 
