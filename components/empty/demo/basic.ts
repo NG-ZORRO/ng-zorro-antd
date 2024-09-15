895485040386b7d34eb9d8fa9a4cd70a8900e655
Laffery <laffery86@gmail.com>
2024-09-15 13:52:41 +0800

    refactor(module:empty): migrate demo to standalone mode

--- components/empty/demo/basic.ts
+++ components/empty/demo/basic.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 
+import { NzEmptyModule } from 'ng-zorro-antd/empty';
+
 @Component({
   selector: 'nz-demo-empty-basic',
-  template: ` <nz-empty></nz-empty> `
+  standalone: true,
+  imports: [NzEmptyModule],
+  template: `<nz-empty></nz-empty>`
 })
 export class NzDemoEmptyBasicComponent {}
