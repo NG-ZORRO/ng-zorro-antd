fa52494536fb9591e2cccea0b5fe743d6b2a0329
Laffery <laffery86@gmail.com>
2024-09-27 08:37:01 +0800

    refactor(module:rate): migrate demo to standalone mode

--- components/rate/demo/basic.ts
+++ components/rate/demo/basic.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { NzRateModule } from 'ng-zorro-antd/rate';
 
 @Component({
   selector: 'nz-demo-rate-basic',
   standalone: true,
-  imports: [NzRateModule],
+  imports: [FormsModule, NzRateModule],
   template: `<nz-rate [ngModel]="0"></nz-rate>`
 })
 export class NzDemoRateBasicComponent {}
--- components/rate/demo/disabled.ts
+++ components/rate/demo/disabled.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { NzRateModule } from 'ng-zorro-antd/rate';
 
 @Component({
   selector: 'nz-demo-rate-disabled',
   standalone: true,
-  imports: [NzRateModule],
+  imports: [FormsModule, NzRateModule],
   template: `<nz-rate [ngModel]="2" nzDisabled></nz-rate>`
 })
 export class NzDemoRateDisabledComponent {}
