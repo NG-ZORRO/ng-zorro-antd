72dbe4e24f04abae254ce9db184cf24f5f0b009b
Laffery <laffery86@gmail.com>
2024-09-27 10:01:09 +0800

    refactor(module:switch): migrate demo to standalone mode

--- components/switch/demo/disabled.ts
+++ components/switch/demo/disabled.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzSwitchModule } from 'ng-zorro-antd/switch';
 
 @Component({
   selector: 'nz-demo-switch-disabled',
+  standalone: true,
+  imports: [FormsModule, NzButtonModule, NzSwitchModule],
   template: `
     <nz-switch [(ngModel)]="switchValue" [nzDisabled]="isDisabled"></nz-switch>
     <br />
     <br />
-    <button nz-button [nzType]="'primary'" (click)="isDisabled = !isDisabled">Toggle disabled</button>
+    <button nz-button nzType="primary" (click)="isDisabled = !isDisabled">Toggle disabled</button>
   `
 })
 export class NzDemoSwitchDisabledComponent {
