72cbcdc92e6862d253c07cf6bcc2da82f94dc584
Laffery <laffery86@gmail.com>
2024-09-30 07:54:09 +0800

    refactor(module:qr-code): migrate demo to standalone mode

--- components/qr-code/demo/background.ts
+++ components/qr-code/demo/background.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 
+import { NzQRCodeModule } from 'ng-zorro-antd/qr-code';
+
 @Component({
   selector: 'nz-demo-qr-code-background',
+  standalone: true,
+  imports: [NzQRCodeModule],
   template: `
     <nz-qrcode nzBgColor="#f6f6f6" nzColor="#ff6600" nzValue="https://ng.ant.design/"></nz-qrcode>
     <nz-qrcode nzBgColor="#f6f6f6" nzColor="#1677ff" nzValue="https://ng.ant.design/"></nz-qrcode>
--- components/qr-code/demo/icon.ts
+++ components/qr-code/demo/icon.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 
+import { NzQRCodeModule } from 'ng-zorro-antd/qr-code';
+
 @Component({
   selector: 'nz-demo-qr-code-icon',
+  standalone: true,
+  imports: [NzQRCodeModule],
   template: `
     <nz-qrcode
       nzValue="https://ng.ant.design/"
