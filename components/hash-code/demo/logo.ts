44f6ce4212709879e5abae7b9d538a497cf56449
Laffery <49607541+Laffery@users.noreply.github.com>
2024-09-30 17:27:39 +0800

    Update logo.ts

--- components/hash-code/demo/logo.ts
+++ components/hash-code/demo/logo.ts
@@ -6,7 +6,7 @@ import { NzHashCodeModule } from 'ng-zorro-antd/hash-code';
   selector: 'nz-demo-hash-code-logo',
   standalone: true,
   imports: [NzHashCodeModule],
-  template: ` <nz-hash-code [nzValue]="value" nzLogo="Antd"></nz-hash-code> `
+  template: `<nz-hash-code [nzValue]="value" nzLogo="Antd"></nz-hash-code>`
 })
 export class NzDemoHashCodeLogoComponent {
   value = 'dfb5fe9ef7b99b2b1db102114a6d7d445d992f40a5d575f801c148990199a068';
