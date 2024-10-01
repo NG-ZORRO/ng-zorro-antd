444114bd512ec0063d169dda05fe565ade089ab1
Laffery <laffery86@gmail.com>
2024-10-02 07:48:32 +0800

    refactor(module:tree-select): migrate demo to standalone mode

--- components/tree-select/demo/status.ts
+++ components/tree-select/demo/status.ts
@@ -8,6 +8,8 @@ import { NzTreeSelectModule } from 'ng-zorro-antd/tree-select';
   imports: [NzTreeSelectModule],
   template: `
     <nz-tree-select [nzNodes]="[]" nzStatus="error" nzPlaceHolder="Error" style="width:100%;"></nz-tree-select>
+    <br />
+    <br />
     <nz-tree-select
       nzMultiple
       [nzNodes]="[]"
