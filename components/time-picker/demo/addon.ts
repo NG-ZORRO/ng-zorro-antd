1a43a4cac4f771d58368a985cc5ab2957b585b47
Laffery <laffery86@gmail.com>
2024-10-02 11:05:51 +0800

    refactor(module:time-picker): migrate demo to standalone mode

--- components/time-picker/demo/addon.ts
+++ components/time-picker/demo/addon.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzTimePickerModule } from 'ng-zorro-antd/time-picker';
 
 @Component({
   selector: 'nz-demo-time-picker-addon',
+  standalone: true,
+  imports: [FormsModule, NzButtonModule, NzTimePickerModule],
   template: `
     <nz-time-picker [(ngModel)]="time" [nzAddOn]="addOnTemplate" #timePicker></nz-time-picker>
     <ng-template #addOnTemplate>
--- components/time-picker/demo/interval-options.ts
+++ components/time-picker/demo/interval-options.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 
+import { NzTimePickerModule } from 'ng-zorro-antd/time-picker';
+
 @Component({
   selector: 'nz-demo-time-picker-interval-options',
-  template: ` <nz-time-picker [nzMinuteStep]="15" [nzSecondStep]="10"></nz-time-picker> `
+  standalone: true,
+  imports: [NzTimePickerModule],
+  template: `<nz-time-picker [nzMinuteStep]="15" [nzSecondStep]="10"></nz-time-picker>`
 })
 export class NzDemoTimePickerIntervalOptionsComponent {}
