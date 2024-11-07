5b594a274b97ead55a257389bf0698482e7cd1b1
Laffery <laffery86@gmail.com>
2024-11-07 10:35:54 +0800

    docs: remove useless class fields

--- scripts/site/_site/doc/app/online-ide/online-ide.service.ts
+++ scripts/site/_site/doc/app/online-ide/online-ide.service.ts
@@ -14,42 +14,16 @@ import tsconfigAppJSON  from './files/tsconfig.app.json';
   providedIn: 'root'
 })
 export class OnlineIdeService {
-  template = 'angular-cli' as const;
-  dependencies = {
-    '@angular/animations': '^18.0.0',
-    '@angular/cdk': '^18.0.0',
-    '@angular/common': '^18.0.0',
-    '@angular/compiler': '^18.0.0',
-    '@angular/core': '^18.0.0',
-    '@angular/forms': '^18.0.0',
-    '@angular/platform-browser': '^18.0.0',
-    '@angular/platform-browser-dynamic': '^18.0.0',
-    '@angular/router': '^18.0.0',
-    '@ant-design/icons-angular': '^18.0.0',
-    rxjs: '~7.8.1',
-    'core-js': '~3.6.5',
-    'date-fns': '^2.16.1',
-    tslib: '^2.0.0',
-    'zone.js': '~0.14.2',
-    'ng-zorro-antd': `^${VERSION.full}`,
-    // demo needs
-    d3: '^6.3.1',
-    dagre: '^0.8.5',
-    'dagre-compound': '^0.0.8',
-    'cron-parser': '^4.6.0',
-    'monaco-editor': '^0.33.0'
-  };
-
   openOnStackBlitz(componentName: string, appComponentCode: string, selector: string): void {
     sdk.openProject({
       title: 'NG-ZORRO',
       description: 'Ant Design of Angular',
       template: 'node',
       files: {
-        'package.json': packageJSON(VERSION.full, '^18.0.0'),
-        'angular.json': `${JSON.stringify(angularJSON, null, 2)}`,
-        'tsconfig.json': `${JSON.stringify(tsconfigJSON, null, 2)}`,
-        'tsconfig.app.json': `${JSON.stringify(tsconfigAppJSON, null, 2)}`,
+        'package.json': JSON.stringify(packageJSON(`^${VERSION.full}`, '^18.0.0'), null, 2),
+        'angular.json':  JSON.stringify(angularJSON, null, 2),
+        'tsconfig.json':  JSON.stringify(tsconfigJSON, null, 2),
+        'tsconfig.app.json': JSON.stringify(tsconfigAppJSON, null, 2),
         'src/index.html': `<${selector}>loading</${selector}>`,
         'src/main.ts': mainTS(componentName),
         'src/app/app.component.ts': appComponentCode,
