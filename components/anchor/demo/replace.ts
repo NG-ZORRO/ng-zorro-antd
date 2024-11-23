0a751177cb363225ca12bc3ee5a5991d3c4571e4
Laffery <49607541+Laffery@users.noreply.github.com>
2024-11-23 16:16:42 +0800

    Update replace.ts

--- components/anchor/demo/replace.ts
+++ components/anchor/demo/replace.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 
+import { NzAnchorModule } from 'ng-zorro-antd/anchor';
+
 @Component({
   selector: 'nz-demo-anchor-replace',
+  standalone: true,
+  imports: [NzAnchorModule],
   template: `
     <nz-anchor>
       <nz-link [nzReplace]="true" nzHref="#components-anchor-demo-basic" nzTitle="Basic demo"></nz-link>
