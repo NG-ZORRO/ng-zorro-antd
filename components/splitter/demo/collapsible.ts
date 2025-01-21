1c5f5e47bf3581999b3d8e486c853084abb5d568
Laffery <laffery86@gmail.com>
2025-01-21 11:39:28 +0800

    feat(module:splitter): init showcases

---
+++ components/splitter/demo/collapsible.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+import { NzSplitterModule } from 'ng-zorro-antd/splitter';
+
+@Component({
+  selector: 'nz-demo-splitter-collapsible',
+  imports: [NzSplitterModule],
+  template: ``
+})
+export class NzDemoSplitterCollapsibleComponent {}
---
+++ components/splitter/demo/vertical.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+import { NzSplitterModule } from 'ng-zorro-antd/splitter';
+
+@Component({
+  selector: 'nz-demo-splitter-vertical',
+  imports: [NzSplitterModule],
+  template: ``
+})
+export class NzDemoSplitterVerticalComponent {}
