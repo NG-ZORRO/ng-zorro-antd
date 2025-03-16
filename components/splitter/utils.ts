b35b69f78a1805ff9670ca81b1cdcb655ef5f077
Laffery <laffery86@gmail.com>
2025-03-16 21:17:14 +0800

    feat(module:splitter): add collapse bar

--- components/splitter/utils.ts
+++ components/splitter/utils.ts
@@ -3,6 +3,8 @@
  * found in the LICENSE file at https://github.com/NG-ZORRO/ng-zorro-antd/blob/master/LICENSE
  */
 
+import { NzSplitterCollapseOption, NzSplitterCollapsible } from 'ng-zorro-antd/splitter/typings';
+
 /**
  * Get the percentage value of the string. (e.g. '50%' => 0.5)
  * @param str
@@ -17,4 +19,19 @@ export function getPercentValue(str: string): number {
  */
 export function isPercent(size: string | number | undefined): size is string {
   return typeof size === 'string' && size.endsWith('%');
+}
+
+/**
+ * Coerce the panel collapsible option to the NzSplitterCollapseOption type.
+ */
+export function coerceCollapsible(collapsible: NzSplitterCollapsible): NzSplitterCollapseOption {
+  if (collapsible && typeof collapsible === 'object') {
+    return collapsible;
+  }
+
+  const mergedCollapsible = !!collapsible;
+  return {
+    start: mergedCollapsible,
+    end: mergedCollapsible
+  };
 }
