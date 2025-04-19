1111039d87a7efebb4d9e7e6b5e7049d3d3abc78
HyperLife1119 <hyperlife1119@qq.com>
2025-04-19 12:02:27 +0800

    feat(module:tabs): support for start and end extra content

---
+++ components/tabs/tab-bar-extra-content.directive.ts
@@ -0,0 +1,14 @@
+/**
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/NG-ZORRO/ng-zorro-antd/blob/master/LICENSE
+ */
+
+import { Directive, inject, input, TemplateRef } from '@angular/core';
+
+@Directive({
+  selector: '[nzTabBarExtraContent]:not(nz-tabset)'
+})
+export class NzTabBarExtraContentDirective {
+  readonly position = input<'start' | 'end'>('end', { alias: 'nzTabBarExtraContent' });
+  readonly templateRef = inject(TemplateRef);
+}
