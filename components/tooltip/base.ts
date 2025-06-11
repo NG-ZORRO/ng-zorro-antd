fabd3e4b3777eb65742c73a734da0b813f29c787
Laffery <laffery86@gmail.com>
2025-06-12 00:23:26 +0800

    refactor(module:tooltip,popconfirm,popover): use inject pattern

--- components/tooltip/base.ts
+++ components/tooltip/base.ts
@@ -400,12 +400,6 @@ export abstract class NzTooltipBaseComponent implements OnInit {
 
   _positions: ConnectionPositionPair[] = [...DEFAULT_TOOLTIP_POSITIONS];
 
-  constructor() {
-    this.destroyRef.onDestroy(() => {
-      this.nzVisibleChange.complete();
-    });
-  }
-
   ngOnInit(): void {
     this.directionality.change?.pipe(takeUntilDestroyed(this.destroyRef)).subscribe(direction => {
       this.dir = direction;
