b96d62b01b88e82b7a0ecaed931d59051ec49234
Laffery <49607541+Laffery@users.noreply.github.com>
2025-06-18 18:10:46 +0800

    Update header.component.ts

--- components/layout/header.component.ts
+++ components/layout/header.component.ts
@@ -3,17 +3,16 @@
  * found in the LICENSE file at https://github.com/NG-ZORRO/ng-zorro-antd/blob/master/LICENSE
  */
 
-import { ChangeDetectionStrategy, Component, ElementRef, inject, Renderer2, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'nz-header',
   exportAs: 'nzHeader',
   template: `<ng-content></ng-content>`,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  encapsulation: ViewEncapsulation.None
-})
-export class NzHeaderComponent {
-  constructor() {
-    inject(Renderer2).addClass(inject(ElementRef<HTMLElement>).nativeElement, 'ant-layout-header');
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    'class': 'ant-layout-header'
   }
-}
+})
+export class NzHeaderComponent {}
