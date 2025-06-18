b9dee5d6b1543dd5e67de53dff3446bfad4aa476
Nicolas Frizzarin <nicolas.frizzarin54@gmail.com>
2025-06-18 03:52:55 +0200

    refactor(module:mention): use inject pattern (#9232)

--- components/mention/mention.service.ts
+++ components/mention/mention.service.ts
@@ -3,15 +3,21 @@
  * found in the LICENSE file at https://github.com/NG-ZORRO/ng-zorro-antd/blob/master/LICENSE
  */
 
-import { Injectable, OnDestroy } from '@angular/core';
+import { DestroyRef, inject, Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 
 import { NzMentionTriggerDirective } from './mention-trigger';
 
 @Injectable()
-export class NzMentionService implements OnDestroy {
+export class NzMentionService {
   private trigger?: NzMentionTriggerDirective;
   private triggerChange$ = new Subject<NzMentionTriggerDirective>();
+
+  constructor() {
+    inject(DestroyRef).onDestroy(() => {
+      this.triggerChange$.complete();
+    });
+  }
 
   triggerChanged(): Observable<NzMentionTriggerDirective> {
     return this.triggerChange$.asObservable();
@@ -22,9 +28,5 @@ export class NzMentionService implements OnDestroy {
       this.trigger = trigger;
       this.triggerChange$.next(trigger);
     }
-  }
-
-  ngOnDestroy(): void {
-    this.triggerChange$.complete();
   }
 }
