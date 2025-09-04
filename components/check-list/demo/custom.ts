235b493705d297ccff21969f2d770b7f4eba7fb5
originRing <48777107+OriginRing@users.noreply.github.com>
2025-09-04 10:22:49 +0800

    feat(module:check-list): add badge and default checked (#9343)

--- components/check-list/demo/custom.ts
+++ components/check-list/demo/custom.ts
@@ -107,7 +107,7 @@ export class NzDemoCheckListCustomComponent {
   readonly options = this.nzItems.map((_, index) => index).concat(this.nzItems.length + 1);
   form = this.fb.group({
     nzProgress: true,
-    nzVisible: true,
+    nzVisible: false,
     nzIndex: 0,
     nzTriggerRender: 'Open List',
     nzTitle: 'Customize task lists',
