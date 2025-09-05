8145c1f92dd2e051f67ac215b008a1e66d9721e7
Nicolas Frizzarin <nicolas.frizzarin54@gmail.com>
2025-09-05 18:14:46 +0200

    feat(module:mention): support autosize

---
+++ components/mention/demo/auto-size.ts
@@ -0,0 +1,29 @@
+import { CdkTextareaAutosize } from '@angular/cdk/text-field';
+import { Component, model } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzMentionModule } from 'ng-zorro-antd/mention';
+
+@Component({
+  selector: 'nz-demo-mention-auto-size',
+  imports: [FormsModule, NzMentionModule, NzInputModule, CdkTextareaAutosize],
+  template: ` <nz-mention [nzSuggestions]="suggestions" (nzOnSelect)="onSelect($event)">
+    <textarea
+      rows="1"
+      placeholder="input here"
+      nzMentionTrigger
+      nz-input
+      [(ngModel)]="inputValue"
+      cdkTextareaAutosize
+    ></textarea>
+  </nz-mention>`
+})
+export class NzDemoMentionAutoSizeComponent {
+  inputValue = model('@afc163');
+  suggestions = ['afc163', 'benjycui', 'yiminghe', 'RaoHai', '中文', 'にほんご'];
+
+  onSelect(suggestion: string): void {
+    console.log(`onSelect ${suggestion}`);
+  }
+}
