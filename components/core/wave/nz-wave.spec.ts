18e8f4a6115595245cc593eccf43760ab568a520
Laffery <laffery86@gmail.com>
2025-11-23 08:43:01 +0800

    test(module:*): add provideZoneChangeDetection temporarily

--- components/core/wave/nz-wave.spec.ts
+++ components/core/wave/nz-wave.spec.ts
@@ -27,88 +27,86 @@ describe('nz-wave basic', () => {
     });
   });
 
-  describe('basic wave', () => {
-    beforeEach(() => {
-      fixture = TestBed.createComponent(WaveContainerWithButtonComponent);
-      fixture.detectChanges();
-      waveTarget = fixture.componentInstance.trigger.nativeElement;
-    });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WaveContainerWithButtonComponent);
+    fixture.detectChanges();
+    waveTarget = fixture.componentInstance.trigger.nativeElement;
+  });
 
-    it('should create wave on click', () => {
-      dispatchMouseEvent(waveTarget, 'click');
-      expect(waveTarget.hasAttribute(WAVE_ATTRIBUTE_NAME)).toBe(true);
-      expect(document.body.querySelector('style') !== null).toBe(true);
-    });
+  it('should create wave on click', () => {
+    dispatchMouseEvent(waveTarget, 'click');
+    expect(waveTarget.hasAttribute(WAVE_ATTRIBUTE_NAME)).toBe(true);
+    expect(document.body.querySelector('style') !== null).toBe(true);
+  });
 
-    it('should remove wave when transition end', fakeAsync(() => {
-      dispatchMouseEvent(waveTarget, 'click');
-      expect(waveTarget.hasAttribute(WAVE_ATTRIBUTE_NAME)).toBe(true);
-      expect(document.body.querySelector('style') !== null).toBe(true);
-      tick(500);
-      expect(waveTarget.hasAttribute(WAVE_ATTRIBUTE_NAME)).toBe(false);
-      expect(document.body.querySelector('style') !== null).toBe(false);
-    }));
+  it('should remove wave when transition end', fakeAsync(() => {
+    dispatchMouseEvent(waveTarget, 'click');
+    expect(waveTarget.hasAttribute(WAVE_ATTRIBUTE_NAME)).toBe(true);
+    expect(document.body.querySelector('style') !== null).toBe(true);
+    tick(500);
+    expect(waveTarget.hasAttribute(WAVE_ATTRIBUTE_NAME)).toBe(false);
+    expect(document.body.querySelector('style') !== null).toBe(false);
+  }));
 
-    it('should throttling when clicks', () => {
-      dispatchMouseEvent(waveTarget, 'click');
-      fixture.detectChanges();
-      dispatchMouseEvent(waveTarget, 'click');
-      fixture.detectChanges();
-      dispatchMouseEvent(waveTarget, 'click');
-      fixture.detectChanges();
-      expect(document.body.querySelectorAll('style').length).toBe(1);
-    });
+  it('should throttling when clicks', () => {
+    dispatchMouseEvent(waveTarget, 'click');
+    fixture.detectChanges();
+    dispatchMouseEvent(waveTarget, 'click');
+    fixture.detectChanges();
+    dispatchMouseEvent(waveTarget, 'click');
+    fixture.detectChanges();
+    expect(document.body.querySelectorAll('style').length).toBe(1);
+  });
 
-    it('should not create wave on click when disabled', () => {
-      fixture.componentInstance.disabled = true;
-      fixture.detectChanges();
-      dispatchMouseEvent(waveTarget, 'click');
-      fixture.detectChanges();
-      expect(waveTarget.hasAttribute(WAVE_ATTRIBUTE_NAME)).toBe(false);
-      expect(document.body.querySelector('style') !== null).toBe(false);
-    });
+  it('should not create wave on click when disabled', () => {
+    fixture.componentInstance.disabled = true;
+    fixture.detectChanges();
+    dispatchMouseEvent(waveTarget, 'click');
+    fixture.detectChanges();
+    expect(waveTarget.hasAttribute(WAVE_ATTRIBUTE_NAME)).toBe(false);
+    expect(document.body.querySelector('style') !== null).toBe(false);
+  });
 
-    it('should not create wave on click when has disabled class', () => {
-      fixture.componentInstance.disabledClass = true;
-      fixture.detectChanges();
-      dispatchMouseEvent(waveTarget, 'click');
-      expect(waveTarget.hasAttribute(WAVE_ATTRIBUTE_NAME)).toBe(false);
-      expect(document.body.querySelector('style') !== null).toBe(false);
-    });
+  it('should not create wave on click when has disabled class', () => {
+    fixture.componentInstance.disabledClass = true;
+    fixture.detectChanges();
+    dispatchMouseEvent(waveTarget, 'click');
+    expect(waveTarget.hasAttribute(WAVE_ATTRIBUTE_NAME)).toBe(false);
+    expect(document.body.querySelector('style') !== null).toBe(false);
+  });
 
-    it('should not create wave style on click when invalid color', () => {
-      fixture.componentInstance.borderColor = '#ffffff';
-      fixture.detectChanges();
-      dispatchMouseEvent(waveTarget, 'click');
-      expect(document.body.querySelector('style') !== null).toBe(false);
-    });
+  it('should not create wave style on click when invalid color', () => {
+    fixture.componentInstance.borderColor = '#ffffff';
+    fixture.detectChanges();
+    dispatchMouseEvent(waveTarget, 'click');
+    expect(document.body.querySelector('style') !== null).toBe(false);
+  });
 
-    it('should priority use border-color color', () => {
-      fixture.componentInstance.borderColor = 'rgb(255, 255, 0)';
-      fixture.componentInstance.backgroundColor = 'rgb(255, 0, 0)';
-      fixture.detectChanges();
-      dispatchMouseEvent(waveTarget, 'click');
-      const style: string = document.body.querySelector('style')!.innerText;
-      expect(style.includes(fixture.componentInstance.borderColor)).toBe(true);
-    });
+  it('should priority use border-color color', () => {
+    fixture.componentInstance.borderColor = 'rgb(255, 255, 0)';
+    fixture.componentInstance.backgroundColor = 'rgb(255, 0, 0)';
+    fixture.detectChanges();
+    dispatchMouseEvent(waveTarget, 'click');
+    const style: string = document.body.querySelector('style')!.innerText;
+    expect(style.includes(fixture.componentInstance.borderColor)).toBe(true);
+  });
 
-    it('should not create wave style on click when grey color', () => {
-      fixture.componentInstance.borderColor = '#eee';
-      fixture.detectChanges();
-      dispatchMouseEvent(waveTarget, 'click');
-      expect(document.body.querySelector('style') !== null).toBe(false);
-    });
+  it('should not create wave style on click when grey color', () => {
+    fixture.componentInstance.borderColor = '#eee';
+    fixture.detectChanges();
+    dispatchMouseEvent(waveTarget, 'click');
+    expect(document.body.querySelector('style') !== null).toBe(false);
+  });
 
-    it('should destroy', () => {
-      dispatchMouseEvent(waveTarget, 'click');
-      expect(waveTarget.hasAttribute(WAVE_ATTRIBUTE_NAME)).toBe(true);
-      expect(document.body.querySelector('style') !== null).toBe(true);
+  it('should destroy', () => {
+    dispatchMouseEvent(waveTarget, 'click');
+    expect(waveTarget.hasAttribute(WAVE_ATTRIBUTE_NAME)).toBe(true);
+    expect(document.body.querySelector('style') !== null).toBe(true);
 
-      fixture.componentInstance.isDestroyed = true;
-      fixture.detectChanges();
+    fixture.componentInstance.isDestroyed = true;
+    fixture.detectChanges();
 
-      expect(document.body.querySelector('style') !== null).toBe(false);
-    });
+    expect(document.body.querySelector('style') !== null).toBe(false);
   });
 });
 
@@ -123,77 +121,75 @@ describe('nz-wave extra', () => {
     });
   });
 
-  describe('extra node wave', () => {
-    beforeEach(() => {
-      fixture = TestBed.createComponent(WaveContainerWithExtraNodeComponent);
-      fixture.detectChanges();
-      waveTarget = fixture.componentInstance.trigger.nativeElement;
-    });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WaveContainerWithExtraNodeComponent);
+    fixture.detectChanges();
+    waveTarget = fixture.componentInstance.trigger.nativeElement;
+  });
 
-    it('should create wave on click', () => {
-      dispatchMouseEvent(waveTarget, 'click');
-      expect(waveTarget.hasAttribute(WAVE_ATTRIBUTE_NAME_EXTRA_NODE)).toBe(true);
-      expect(document.body.querySelector('style') !== null).toBe(true);
-      expect(waveTarget.querySelector(EXTRA_NODE_CLASS_NAME) !== null).toBe(true);
-    });
+  it('should create wave on click', () => {
+    dispatchMouseEvent(waveTarget, 'click');
+    expect(waveTarget.hasAttribute(WAVE_ATTRIBUTE_NAME_EXTRA_NODE)).toBe(true);
+    expect(document.body.querySelector('style') !== null).toBe(true);
+    expect(waveTarget.querySelector(EXTRA_NODE_CLASS_NAME) !== null).toBe(true);
+  });
 
-    it('should remove wave when transition end', fakeAsync(() => {
-      dispatchMouseEvent(waveTarget, 'click');
-      expect(document.body.querySelector('style') !== null).toBe(true);
-      expect(waveTarget.querySelector(EXTRA_NODE_CLASS_NAME) !== null).toBe(true);
-      tick(500);
-      expect(document.body.querySelector('style') !== null).toBe(false);
-      expect(waveTarget.querySelector(EXTRA_NODE_CLASS_NAME) !== null).toBe(false);
-    }));
+  it('should remove wave when transition end', fakeAsync(() => {
+    dispatchMouseEvent(waveTarget, 'click');
+    expect(document.body.querySelector('style') !== null).toBe(true);
+    expect(waveTarget.querySelector(EXTRA_NODE_CLASS_NAME) !== null).toBe(true);
+    tick(500);
+    expect(document.body.querySelector('style') !== null).toBe(false);
+    expect(waveTarget.querySelector(EXTRA_NODE_CLASS_NAME) !== null).toBe(false);
+  }));
 
-    it('should not create wave on click when has disabled class', () => {
-      fixture.componentInstance.disabledClass = true;
-      fixture.detectChanges();
-      dispatchMouseEvent(waveTarget, 'click');
-      expect(waveTarget.hasAttribute(WAVE_ATTRIBUTE_NAME_EXTRA_NODE)).toBe(false);
-      expect(document.body.querySelector('style') !== null).toBe(false);
-      expect(waveTarget.querySelector(EXTRA_NODE_CLASS_NAME) !== null).toBe(false);
-    });
+  it('should not create wave on click when has disabled class', () => {
+    fixture.componentInstance.disabledClass = true;
+    fixture.detectChanges();
+    dispatchMouseEvent(waveTarget, 'click');
+    expect(waveTarget.hasAttribute(WAVE_ATTRIBUTE_NAME_EXTRA_NODE)).toBe(false);
+    expect(document.body.querySelector('style') !== null).toBe(false);
+    expect(waveTarget.querySelector(EXTRA_NODE_CLASS_NAME) !== null).toBe(false);
+  });
 
-    it('should not create wave style on click when invalid color', () => {
-      fixture.componentInstance.borderColor = '#ffffff';
-      fixture.detectChanges();
-      dispatchMouseEvent(waveTarget, 'click');
-      expect(document.body.querySelector('style') !== null).toBe(false);
-    });
+  it('should not create wave style on click when invalid color', () => {
+    fixture.componentInstance.borderColor = '#ffffff';
+    fixture.detectChanges();
+    dispatchMouseEvent(waveTarget, 'click');
+    expect(document.body.querySelector('style') !== null).toBe(false);
+  });
 
-    it('should priority use border-color color', () => {
-      fixture.componentInstance.borderColor = 'rgb(255, 255, 0)';
-      fixture.componentInstance.backgroundColor = 'rgb(255, 0, 0)';
-      fixture.detectChanges();
-      dispatchMouseEvent(waveTarget, 'click');
-      const style: string = document.body.querySelector('style')!.innerText;
-      expect(style.includes(fixture.componentInstance.borderColor)).toBe(true);
-    });
+  it('should priority use border-color color', () => {
+    fixture.componentInstance.borderColor = 'rgb(255, 255, 0)';
+    fixture.componentInstance.backgroundColor = 'rgb(255, 0, 0)';
+    fixture.detectChanges();
+    dispatchMouseEvent(waveTarget, 'click');
+    const style: string = document.body.querySelector('style')!.innerText;
+    expect(style.includes(fixture.componentInstance.borderColor)).toBe(true);
+  });
 
-    it('should not create wave style on click when grey color', () => {
-      fixture.componentInstance.borderColor = '#eee';
-      fixture.detectChanges();
-      dispatchMouseEvent(waveTarget, 'click');
-      expect(document.body.querySelector('style') !== null).toBe(false);
-    });
+  it('should not create wave style on click when grey color', () => {
+    fixture.componentInstance.borderColor = '#eee';
+    fixture.detectChanges();
+    dispatchMouseEvent(waveTarget, 'click');
+    expect(document.body.querySelector('style') !== null).toBe(false);
+  });
 
-    it('should destroy', () => {
-      dispatchMouseEvent(waveTarget, 'click');
-      expect(waveTarget.hasAttribute(WAVE_ATTRIBUTE_NAME_EXTRA_NODE)).toBe(true);
-      expect(document.body.querySelector('style') !== null).toBe(true);
-      expect(waveTarget.querySelector(EXTRA_NODE_CLASS_NAME) !== null).toBe(true);
+  it('should destroy', () => {
+    dispatchMouseEvent(waveTarget, 'click');
+    expect(waveTarget.hasAttribute(WAVE_ATTRIBUTE_NAME_EXTRA_NODE)).toBe(true);
+    expect(document.body.querySelector('style') !== null).toBe(true);
+    expect(waveTarget.querySelector(EXTRA_NODE_CLASS_NAME) !== null).toBe(true);
 
-      fixture.componentInstance.isDestroyed = true;
-      fixture.detectChanges();
+    fixture.componentInstance.isDestroyed = true;
+    fixture.detectChanges();
 
-      expect(document.body.querySelector('style') !== null).toBe(false);
-      expect(waveTarget.querySelector(EXTRA_NODE_CLASS_NAME) !== null).toBe(false);
-    });
+    expect(document.body.querySelector('style') !== null).toBe(false);
+    expect(waveTarget.querySelector(EXTRA_NODE_CLASS_NAME) !== null).toBe(false);
   });
 });
 
-describe('nz-wave NoopAnimationsModule', () => {
+describe('nz-wave noAnimation', () => {
   let fixture: ComponentFixture<WaveContainerWithButtonComponent>;
   let waveRef: NzWaveDirective;
 
@@ -203,23 +199,21 @@ describe('nz-wave NoopAnimationsModule', () => {
     });
   });
 
-  describe('NoopAnimationsModule', () => {
-    beforeEach(() => {
-      fixture = TestBed.createComponent(WaveContainerWithButtonComponent);
-      fixture.detectChanges();
-      waveRef = fixture.componentInstance.wave;
-    });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WaveContainerWithButtonComponent);
+    fixture.detectChanges();
+    waveRef = fixture.componentInstance.wave;
+  });
 
-    it('should disable by NoopAnimationsModule ', () => {
-      expect(waveRef.disabled).toBe(true);
-      expect(waveRef.rendererRef).toBeFalsy();
-    });
+  it('should disable by NoopAnimationsModule ', () => {
+    expect(waveRef.disabled).toBe(true);
+    expect(waveRef.rendererRef).toBeFalsy();
+  });
 
-    it('should config priority', () => {
-      waveRef.enable();
-      expect(waveRef.disabled).toBe(true);
-      expect(waveRef.rendererRef).toBeFalsy();
-    });
+  it('should config priority', () => {
+    waveRef.enable();
+    expect(waveRef.disabled).toBe(true);
+    expect(waveRef.rendererRef).toBeFalsy();
   });
 });
 
@@ -228,30 +222,28 @@ describe('nz-wave disable/enable', () => {
   let waveTarget: HTMLElement;
   let waveRef: NzWaveDirective;
 
-  describe('disable/enable', () => {
-    beforeEach(() => {
-      fixture = TestBed.createComponent(WaveContainerWithButtonComponent);
-      fixture.detectChanges();
-      waveTarget = fixture.componentInstance.trigger.nativeElement;
-      waveRef = fixture.componentInstance.wave;
-    });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WaveContainerWithButtonComponent);
+    fixture.detectChanges();
+    waveTarget = fixture.componentInstance.trigger.nativeElement;
+    waveRef = fixture.componentInstance.wave;
+  });
 
-    it('should enable work', () => {
-      waveRef.enable();
-      expect(waveRef.disabled).toBe(false);
-      expect(waveRef.rendererRef).toBeTruthy();
-      dispatchMouseEvent(waveTarget, 'click');
-      expect(waveTarget.hasAttribute(WAVE_ATTRIBUTE_NAME)).toBe(true);
-      expect(document.body.querySelector('style') !== null).toBe(true);
-    });
+  it('should enable work', () => {
+    waveRef.enable();
+    expect(waveRef.disabled).toBe(false);
+    expect(waveRef.rendererRef).toBeTruthy();
+    dispatchMouseEvent(waveTarget, 'click');
+    expect(waveTarget.hasAttribute(WAVE_ATTRIBUTE_NAME)).toBe(true);
+    expect(document.body.querySelector('style') !== null).toBe(true);
+  });
 
-    it('should disable work', () => {
-      waveRef.disable();
-      expect(waveRef.disabled).toBe(true);
-      dispatchMouseEvent(waveTarget, 'click');
-      expect(waveTarget.hasAttribute(WAVE_ATTRIBUTE_NAME)).toBe(false);
-      expect(document.body.querySelector('style') === null).toBe(true);
-    });
+  it('should disable work', () => {
+    waveRef.disable();
+    expect(waveRef.disabled).toBe(true);
+    dispatchMouseEvent(waveTarget, 'click');
+    expect(waveTarget.hasAttribute(WAVE_ATTRIBUTE_NAME)).toBe(false);
+    expect(document.body.querySelector('style') === null).toBe(true);
   });
 });
 
--- components/form/form-text.spec.ts
+++ components/form/form-text.spec.ts
@@ -3,35 +3,20 @@
  * found in the LICENSE file at https://github.com/NG-ZORRO/ng-zorro-antd/blob/master/LICENSE
  */
 
-import { Component, DebugElement } from '@angular/core';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { TestBed } from '@angular/core/testing';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
 
-import { NzFormModule } from 'ng-zorro-antd/form/form.module';
-
-import { NzFormTextComponent } from './form-text.component';
-
-const testBedOptions = { imports: [NoopAnimationsModule] };
+import { NzFormTextComponent } from 'ng-zorro-antd/form/form-text.component';
 
 describe('nz-form-text', () => {
-  describe('default', () => {
-    let fixture: ComponentFixture<NzTestFormTextComponent>;
-    let text: DebugElement;
-    beforeEach(() => {
-      TestBed.configureTestingModule(testBedOptions);
-      fixture = TestBed.createComponent(NzTestFormTextComponent);
-      fixture.detectChanges();
-      text = fixture.debugElement.query(By.directive(NzFormTextComponent));
-    });
-    it('should className correct', () => {
-      expect(text.nativeElement.classList).toContain('ant-form-text');
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [provideNoopAnimations()]
     });
   });
-});
 
-@Component({
-  imports: [NzFormModule],
-  template: `<nz-form-text></nz-form-text>`
-})
-export class NzTestFormTextComponent {}
+  it('should className correct', () => {
+    const fixture = TestBed.createComponent(NzFormTextComponent);
+    expect(fixture.nativeElement.classList).toContain('ant-form-text');
+  });
+});
