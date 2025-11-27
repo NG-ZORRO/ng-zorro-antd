acc421e2323afddf922f64a73ff91d9916f92666
Nicolas Frizzarin <nicolas.frizzarin54@gmail.com>
2025-11-27 16:24:37 +0100

    test(module:avatar): update tests to use zoneless mode

--- components/core/testing/public-api.ts
+++ components/core/testing/public-api.ts
@@ -5,5 +5,6 @@
 
 export * from './dispatch-events';
 export * from './event-objects';
-export * from './type-in-element';
 export * from './mock-ng-zone';
+export * from './type-in-element';
+export * from './zoneless-helpers';
