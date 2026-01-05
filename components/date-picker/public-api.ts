5aa11e2c0f709c3b496bceae57ce59d5a7a3ea56
Nicolas Frizzarin <nicolas.frizzarin54@gmail.com>
2026-01-05 14:53:27 +0100

    feat(module:time-picker): support nzPlacement

--- components/date-picker/public-api.ts
+++ components/date-picker/public-api.ts
@@ -6,17 +6,17 @@
 export * from './standard-types';
 export * from './util';
 
-export { NzDatePickerModule } from './date-picker.module';
+export { CalendarFooterComponent as ɵCalendarFooterComponent } from './calendar-footer.component';
 export { NzDatePickerComponent } from './date-picker.component';
-export type { NzDatePickerSizeType, NzPlacement } from './date-picker.component';
-export { NzRangePickerComponent } from './range-picker.component';
-export { NzMonthPickerComponent } from './month-picker.component';
-export { NzQuarterPickerComponent } from './quarter-picker.component';
-export { NzWeekPickerComponent } from './week-picker.component';
-export { NzYearPickerComponent } from './year-picker.component';
+export type { NzDatePickerSizeType } from './date-picker.component';
+export { NzDatePickerModule } from './date-picker.module';
 export { DatePickerService as ɵDatePickerService } from './date-picker.service';
 export { DateRangePopupComponent as ɵDateRangePopupComponent } from './date-range-popup.component';
 export { InnerPopupComponent as ɵInnerPopupComponent } from './inner-popup.component';
-export { CalendarFooterComponent as ɵCalendarFooterComponent } from './calendar-footer.component';
+export { NzMonthPickerComponent } from './month-picker.component';
+export { NzQuarterPickerComponent } from './quarter-picker.component';
+export { NzRangePickerComponent } from './range-picker.component';
+export { NzWeekPickerComponent } from './week-picker.component';
+export { NzYearPickerComponent } from './year-picker.component';
 
 export * from './lib/public-api';
