4b6ea5c2c3b3444575093764e167f086962d69b5
Nicolas Frizzarin <nicolas.frizzarin54@gmail.com>
2026-01-11 00:26:19 +0100

    fix(module:core): build failed

--- components/core/form/nz-form-size.token.ts
+++ components/core/form/nz-form-size.token.ts
@@ -5,7 +5,7 @@
 
 import { InjectionToken, type Signal } from '@angular/core';
 
-import type { NzSizeLDSType } from '../types';
+import type { NzSizeLDSType } from 'ng-zorro-antd/core/types';
 
 export const NZ_FORM_SIZE = new InjectionToken<Signal<NzSizeLDSType | undefined>>(
   typeof ngDevMode !== 'undefined' && ngDevMode ? 'nz-form-size' : ''
