3068e114c7972618d14056945faf737e041c5799
Maxim <100690852+maxonbigbon@users.noreply.github.com>
2026-01-31 08:01:14 +0000

    test(module:date-picker): add unit tests for week mode persistence

--- components/date-picker/lib/lib-packer.spec.ts
+++ components/date-picker/lib/lib-packer.spec.ts
@@ -7,17 +7,19 @@ import { fakeAsync, TestBed } from '@angular/core/testing';
 
 import { CandyDate } from 'ng-zorro-antd/core/time';
 import { NzSafeAny } from 'ng-zorro-antd/core/types';
+import { en_US, NzI18nModule } from 'ng-zorro-antd/i18n';
 
 import { DateTableComponent } from './date-table.component';
-import { LibPackerModule } from './lib-packer.module';
 import { MonthTableComponent } from './month-table.component';
+import { DateHeaderComponent } from './date-header.component';
+import { LibPackerModule } from './lib-packer.module';
 
 describe('Coverage supplements', () => {
   let componentInstance: NzSafeAny;
 
   beforeEach(fakeAsync(() => {
     TestBed.configureTestingModule({
-      imports: [LibPackerModule]
+      imports: [LibPackerModule, NzI18nModule]
     });
   }));
 
@@ -93,4 +95,46 @@ describe('Coverage supplements', () => {
       expect(componentInstance.render).toHaveBeenCalled();
     });
   }); // /MonthTable
+
+  // Tests for issue #9650
+  describe('DateHeader', () => {
+    beforeEach(() => {
+      componentInstance = TestBed.createComponent(DateHeaderComponent).componentInstance;
+      componentInstance.value = new CandyDate('2020-02-25');
+      componentInstance.locale = en_US.DatePicker.lang;
+    });
+
+    it('should accept mode as an input and use default value of "date"', () => {
+      componentInstance.ngOnInit();
+      expect(componentInstance.mode).toBe('date');
+    });
+
+    it('should accept mode input and maintain it as "week"', () => {
+      componentInstance.mode = 'week';
+      componentInstance.ngOnInit();
+      expect(componentInstance.mode).toBe('week');
+    });
+
+    it('should preserve mode when calling changeValue', () => {
+      componentInstance.mode = 'week';
+      componentInstance.ngOnInit();
+      const initialMode = componentInstance.mode;
+      const newValue = new CandyDate('2020-03-15');
+      componentInstance.changeValue(newValue);
+      expect(componentInstance.mode).toBe(initialMode);
+      expect(componentInstance.mode).toBe('week');
+    });
+
+    it('should emit panelChange with correct mode when value changes', () => {
+      componentInstance.mode = 'week';
+      componentInstance.ngOnInit();
+      spyOn(componentInstance.panelChange, 'emit');
+      const newValue = new CandyDate('2020-03-15');
+      componentInstance.changeValue(newValue);
+      expect(componentInstance.panelChange.emit).toHaveBeenCalledWith({
+        mode: 'week',
+        date: newValue.nativeDate
+      });
+    });
+  }); // /DateHeader
 });
