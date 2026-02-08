aa5546448ae4a0ef9930a81dc9f91e93d47ab1c3
Clayton <claytonlin1110@gmail.com>
2026-02-08 05:17:50 -0600

    fix: lint

--- components/steps/demo/description-only.ts
+++ components/steps/demo/description-only.ts
@@ -9,9 +9,9 @@ import { NzStepsModule } from 'ng-zorro-antd/steps';
     <nz-steps [nzCurrent]="1">
       <nz-step
         nzDescription="This is a description only. The title row is reserved so the description stays below the horizontal line."
-      ></nz-step>
-      <nz-step nzTitle="In Progress" nzDescription="This step has both title and description."></nz-step>
-      <nz-step nzDescription="Another step with description only. No title provided."></nz-step>
+      />
+      <nz-step nzTitle="In Progress" nzDescription="This step has both title and description." />
+      <nz-step nzDescription="Another step with description only. No title provided." />
     </nz-steps>
   `
 })
