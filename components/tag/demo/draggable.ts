d1acf3e3248037069374fcfe8fb7c393fe9f4ad6
laffery <laffery86@gmail.com>
2026-05-23 13:25:19 +0800

    docs: add draggable tag demo

---
+++ components/tag/demo/draggable.ts
@@ -0,0 +1,43 @@
+import { CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Component } from '@angular/core';
+
+import { NzTagModule } from 'ng-zorro-antd/tag';
+
+@Component({
+  selector: 'nz-demo-tag-draggable',
+  imports: [CdkDropList, CdkDrag, NzTagModule],
+  template: `
+    <div class="tag-list" cdkDropList cdkDropListOrientation="horizontal" (cdkDropListDropped)="drop($event)">
+      @for (tag of tags; track tag) {
+        <nz-tag cdkDrag>{{ tag }}</nz-tag>
+      }
+    </div>
+  `,
+  styles: `
+    .tag-list {
+      display: flex;
+      flex-wrap: wrap;
+      gap: 8px;
+    }
+
+    .tag-list nz-tag {
+      margin-inline-end: 0;
+      cursor: move;
+    }
+
+    .tag-list.cdk-drop-list-dragging nz-tag:not(.cdk-drag-placeholder) {
+      transition: transform 250ms cubic-bezier(0, 0, 0.2, 1);
+    }
+
+    .tag-list .cdk-drag-placeholder {
+      opacity: 0;
+    }
+  `
+})
+export class NzDemoTagDraggableComponent {
+  tags = ['Tag 1', 'Tag 2', 'Tag 3', 'Tag 4', 'Tag 5'];
+
+  drop(event: CdkDragDrop<string[]>): void {
+    moveItemInArray(this.tags, event.previousIndex, event.currentIndex);
+  }
+}
