ffc81e4c1c9a3404db6102df26c95e363c237dfa
laffery86 <laffery86@gmail.com>
2026-08-25 10:25:51 +0800

    test(module:cdk): cover resize observer disabled changes

--- components/cdk/resize-observer/resize-observer.spec.ts
+++ components/cdk/resize-observer/resize-observer.spec.ts
@@ -3,107 +3,65 @@
  * found in the LICENSE file at https://github.com/NG-ZORRO/ng-zorro-antd/blob/master/LICENSE
  */
 
-import { Component, EventEmitter, SimpleChanges } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { Subscription } from 'rxjs';
-
-import { vi } from 'vitest';
+import { Subject } from 'rxjs';
 
 import { NzResizeObserverDirective, NzResizeObserver } from 'ng-zorro-antd/cdk/resize-observer';
-import { NzSafeAny } from 'ng-zorro-antd/core/types';
 
 describe('resize observer', () => {
   let fixture: ComponentFixture<TestHostComponent>;
-  let directive: NzResizeObserverDirective;
-
-  function getResizeObserver(): NzResizeObserver {
-    return directive['resizeObserver'];
-  }
+  let component: TestHostComponent;
+  let resizeEntries$: Subject<ResizeObserverEntry[]>;
 
   beforeEach(() => {
+    resizeEntries$ = new Subject<ResizeObserverEntry[]>();
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: NzResizeObserver,
+          useValue: { observe: () => resizeEntries$ }
+        }
+      ]
+    });
     fixture = TestBed.createComponent(TestHostComponent);
-    directive = fixture.debugElement.children[0].injector.get(NzResizeObserverDirective);
+    component = fixture.componentInstance;
+    fixture.autoDetectChanges();
   });
 
-  it('should have correct initial values', () => {
-    expect(directive.nzResizeObserve).toBeDefined();
-    expect(directive.nzResizeObserve).toBeInstanceOf(EventEmitter);
+  it('should stop and resume resize events when disabled changes', async () => {
+    await fixture.whenStable();
 
-    expect(directive.nzResizeObserverDisabled).toEqual(false);
+    const initialEntries = [{} as ResizeObserverEntry];
+    resizeEntries$.next(initialEntries);
+    expect(component.resizeEntries()).toEqual([initialEntries]);
 
-    expect(directive['currentSubscription']).toEqual(null);
-  });
+    component.disabled.set(true);
+    await fixture.whenStable();
 
-  it('should call subscribe when all the conditions are met', () => {
-    directive['currentSubscription'] = null;
-    directive.nzResizeObserverDisabled = false;
-    vi.spyOn(directive as NzSafeAny, 'subscribe');
-    directive.ngAfterContentInit();
-    expect(directive['subscribe']).toHaveBeenCalled();
-  });
+    resizeEntries$.next([{} as ResizeObserverEntry]);
+    expect(component.resizeEntries()).toEqual([initialEntries]);
 
-  it('should not call subscribe when nzResizeObserverDisabled is true', () => {
-    directive['currentSubscription'] = null;
-    directive.nzResizeObserverDisabled = true;
-    vi.spyOn(directive as NzSafeAny, 'subscribe');
-    directive.ngAfterContentInit();
-    expect(directive['subscribe']).not.toHaveBeenCalled();
-  });
+    component.disabled.set(false);
+    await fixture.whenStable();
 
-  it('should not call subscribe when currentSubscription is truthy', () => {
-    directive['currentSubscription'] = new Subscription();
-    directive.nzResizeObserverDisabled = false;
-    vi.spyOn(directive as NzSafeAny, 'subscribe');
-    directive.ngAfterContentInit();
-    expect(directive['subscribe']).not.toHaveBeenCalled();
-  });
-
-  it('should call unsubscribe when nzResizeObserverDisabled changes to true', () => {
-    const change = {
-      nzResizeObserverDisabled: {}
-    };
-    vi.spyOn(directive as NzSafeAny, 'unsubscribe');
-    directive.nzResizeObserverDisabled = true;
-    directive.ngOnChanges(change as unknown as SimpleChanges);
-    expect(directive['unsubscribe']).toHaveBeenCalled();
-  });
-
-  it('should call subscribe when nzResizeObserverDisabled changes to false', () => {
-    const change = {
-      nzResizeObserverDisabled: {}
-    };
-    vi.spyOn(directive as NzSafeAny, 'subscribe');
-    directive.nzResizeObserverDisabled = false;
-    directive.ngOnChanges(change as unknown as SimpleChanges);
-    expect(directive['subscribe']).toHaveBeenCalled();
-  });
-
-  it('should call correct methods when calling subscribe', () => {
-    vi.spyOn(directive as NzSafeAny, 'unsubscribe');
-    directive['subscribe']();
-    expect(directive['unsubscribe']).toHaveBeenCalled();
-  });
-
-  it('should destroy the observedElements', () => {
-    const element = document.createElement('div');
-    const resizeObserver = getResizeObserver();
-    resizeObserver.observe(element);
-    fixture.detectChanges();
-    vi.spyOn(resizeObserver as NzSafeAny, 'cleanupObserver');
-    fixture.destroy();
-    expect(resizeObserver['cleanupObserver']).toHaveBeenCalled();
-  });
-
-  it('should return correct resizeObserver if it is supported', () => {
-    // eslint-disable-next-line no-global-assign
-    ResizeObserver = undefined as NzSafeAny;
-    const result = directive['resizeObserver']['nzResizeObserverFactory'].create(vi.fn());
-    expect(result).toEqual(null);
+    const resumedEntries = [{} as ResizeObserverEntry];
+    resizeEntries$.next(resumedEntries);
+    expect(component.resizeEntries()).toEqual([initialEntries, resumedEntries]);
   });
 });
 
 @Component({
-  template: `<div nzResizeObserver></div>`,
+  template: `
+    <div nzResizeObserver [nzResizeObserverDisabled]="disabled()" (nzResizeObserve)="onResize($event)"></div>
+  `,
   imports: [NzResizeObserverDirective]
 })
-class TestHostComponent {}
+class TestHostComponent {
+  readonly disabled = signal(false);
+  readonly resizeEntries = signal<ResizeObserverEntry[][]>([]);
+
+  onResize(entries: ResizeObserverEntry[]): void {
+    this.resizeEntries.update(currentEntries => [...currentEntries, entries]);
+  }
+}
